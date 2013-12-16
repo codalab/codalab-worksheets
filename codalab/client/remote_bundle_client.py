96074ed91f5ce8646f190d10f7d3886e40c43a95
Shaunak Kishore <skishore@mit.edu>
2013-12-15 18:04:07 -0800

    Trap server-side UsageErrors cleanly

--- codalab/client/remote_bundle_client.py
+++ codalab/client/remote_bundle_client.py
@@ -1,7 +1,10 @@
 import xmlrpclib
 
 from codalab.client.bundle_client import BundleClient
-from codalab.common import BUNDLE_RPC_PORT
+from codalab.common import (
+  BUNDLE_RPC_PORT,
+  UsageError,
+)
 
 
 class RemoteBundleClient(BundleClient):
@@ -24,7 +27,15 @@ class RemoteBundleClient(BundleClient):
     self.proxy = xmlrpclib.ServerProxy(address)
     def do_command(command):
       def inner(*args, **kwargs):
-        return getattr(self.proxy, command)(*args, **kwargs)
+        try:
+          return getattr(self.proxy, command)(*args, **kwargs)
+        except xmlrpclib.Fault, e:
+          # Transform server-side UsageErrors into client-side UsageErrors.
+          if 'codalab.common.UsageError' in e.faultString:
+            index = e.faultString.find(':')
+            raise UsageError(e.faultString[index + 1:])
+          else:
+            raise
       return inner
     for command in self.PROXY_COMMANDS:
       setattr(self, command, do_command(command))
