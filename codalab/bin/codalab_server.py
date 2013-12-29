e5572ca8001845fbc2e5201a3fab3ab79260e3ee
Shaunak Kishore <skishore@mit.edu>
2013-12-29 18:29:22 -0500

    Drop file server

--- codalab/bin/codalab_server.py
+++ codalab/bin/codalab_server.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python
-import argparse
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument('server_type', help='[file|rpc]', default='rpc', nargs='?')
-  args = parser.parse_args()
-  if args.server_type == 'file':
-    from codalab.server.bundle_file_server import BundleFileServer
-    server = BundleFileServer()
-  elif args.server_type == 'rpc':
-    from codalab.server.bundle_rpc_server import BundleRPCServer
-    server = BundleRPCServer()
-  else:
-    raise ValueError('Unexpected server type: %s' % (args.server_type,))
+  from codalab.server.bundle_rpc_server import BundleRPCServer
+  server = BundleRPCServer()
   server.serve_forever()
