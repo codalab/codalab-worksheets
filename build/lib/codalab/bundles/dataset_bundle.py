f0db7311c990c27b198a7705d283af5d2de37aff
cpoulain <cxpoulain@live.com>
2014-04-04 11:57:01 -0700

    Merge pull request #7 Remove build foder and add entries to .gitignore

--- build/lib/codalab/bundles/dataset_bundle.py
+++
@@ -1,8 +0,0 @@
-'''
-DatasetBundle is a Bundle type that simply inherits from UploadedBundle.
-'''
-from codalab.bundles.uploaded_bundle import UploadedBundle
-
-
-class DatasetBundle(UploadedBundle):
-    BUNDLE_TYPE = 'dataset'
--- build/lib/codalab/model/util.py
+++
@@ -1,8 +0,0 @@
-'''
-Some utility classes and methods used with the CodaLab bundle model.
-'''
-class LikeQuery(str):
-    '''
-    Used for a string that should be used to construct a LIKE clause instead of
-    an equality clause in make_bundle_clause.
-    '''
