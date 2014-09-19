b07b2ed16b05ad2fd9897102c8905c74ee420fe1
Francis <francis@tivix.com>
2014-09-18 17:08:02 -0700

    force string on result user ids

--- codalab/server/auth.py
+++ codalab/server/auth.py
@@ -203,7 +203,7 @@ class OAuthHandler(object):
         result = json.load(response)
         status_code = result['code'] if 'code' in result else 500
         if status_code == 200:
-            self._user = User(result['user']['name'], result['user']['id'])
+            self._user = User(result['user']['name'], str(result['user']['id']))
             return True
         elif status_code == 403 or status_code == 404:
             return False # 'User credentials are not valid'
