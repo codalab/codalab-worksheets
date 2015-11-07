37f0c1c91617d3dda5ca7dea6c545ed7d2605334
Stephen Koo <stephen.theking@gmail.com>
2015-11-06 17:20:51 -0800

    Add bundles through cl add again
     - New syntax for cl add: `cl add item_type item [item ...]
       dest_worksheet`
     - Update test-cli
     - Include aliases in `cl help` output

--- codalab/lib/bundle_cli.py
+++ codalab/lib/bundle_cli.py
@@ -19,6 +19,7 @@ import shlex
 import sys
 import time
 import tempfile
+import textwrap
 
 import argcomplete
 from argcomplete.completers import FilesCompleter, ChoicesCompleter
@@ -59,10 +60,10 @@ from codalab.lib.completers import (
   BundlesCompleter,
   AddressesCompleter,
   GroupsCompleter,
+  UnionCompleter,
   NullCompleter,
   require_not_headless,
 )
-
 
 ## Formatting Constants
 GLOBAL_SPEC_FORMAT = "[<alias>::|<address>::]|(<uuid>|<name>)"
@@ -73,6 +74,58 @@ WORKSHEET_SPEC_FORMAT = GLOBAL_SPEC_FORMAT
 GROUP_SPEC_FORMAT = '(<uuid>|<name>|public)'
 PERMISSION_SPEC_FORMAT = '((n)one|(r)ead|(a)ll)'
 UUID_POST_FUNC = '[0:8]'  # Only keep first 8 characters
+
+## Command groupings
+BUNDLE_COMMANDS = (
+    'upload',
+    'make',
+    'run',
+    'edit',
+    'detach',
+    'rm',
+    'search',
+    'ls',
+    'info',
+    'cat',
+    'wait',
+    'download',
+    'cp',
+    'mimic',
+    'macro',
+    'kill',
+)
+
+WORKSHEET_COMMANDS = (
+    'new',
+    'add',
+    'work',
+    'print',
+    'wedit',
+    'wrm',
+    'wls',
+    'wcp',
+)
+
+GROUP_AND_PERMISSION_COMMANDS = (
+    'gls',
+    'gnew',
+    'grm',
+    'ginfo',
+    'uadd',
+    'urm',
+    'perm',
+    'wperm',
+    'chown',
+)
+
+OTHER_COMMANDS = (
+    'help',
+    'status',
+    'alias',
+    'work-manager',
+    'server',
+    'logout',
+)
 
 class Commands(object):
     '''
@@ -113,8 +166,6 @@ class Commands(object):
         class _AliasedPseudoAction(argparse.Action):
             def __init__(self, name, aliases=None, help=None):
                 dest = name
-                if aliases:
-                    dest += ' (%s)' % ','.join(aliases)
                 sup = super(Commands.AliasedSubParsersAction._AliasedPseudoAction, self)
                 sup.__init__(option_strings=[], dest=dest, help=help)
 
@@ -153,13 +204,55 @@ class Commands(object):
         return register_command
 
     @classmethod
-    def get_help(cls, name):
-        '''
-        Returns the help string for the subcommand of the given `name`, or
-        return None if the command doesn't exist or if the command doesn't have
-        a help string.
-        '''
-        return getattr(cls.commands.get(name, None), 'help', None)
+    def help_text(cls):
+        def command_name(command):
+            name = command
+            aliases = cls.commands[command].aliases
+            if aliases:
+                name += ' (%s)' % ', '.join(list(aliases))
+            return name
+
+        indent = 2
+        max_length = max(
+          len(command_name(command)) for command in itertools.chain(
+              BUNDLE_COMMANDS,
+              WORKSHEET_COMMANDS,
+              GROUP_AND_PERMISSION_COMMANDS,
+              OTHER_COMMANDS)
+        )
+
+        def command_help_text(command):
+            name = command_name(command)
+            return '%s%s%s%s' % (
+              ' ' * indent,
+              name,
+              ' ' * (indent + max_length - len(name)),
+              cls.commands[command].help,
+            )
+
+        def command_group_help_text(commands):
+            return '\n'.join([command_help_text(command) for command in commands])
+
+        return textwrap.dedent("""
+        Usage: cl <command> <arguments>
+
+        Commands for bundles:
+        {bundle_commands}
+
+        Commands for worksheets:
+        {worksheet_commands}
+
+        Commands for groups and permissions:
+        {group_and_permission_commands}
+
+        Other commands:
+        {other_commands}
+        """).format(
+            bundle_commands=command_group_help_text(BUNDLE_COMMANDS),
+            worksheet_commands=command_group_help_text(WORKSHEET_COMMANDS),
+            group_and_permission_commands=command_group_help_text(GROUP_AND_PERMISSION_COMMANDS),
+            other_commands=command_group_help_text(OTHER_COMMANDS),
+        ).strip()
 
     @classmethod
     def build_parser(cls, cli):
@@ -167,14 +260,14 @@ class Commands(object):
         Builds an `ArgumentParser` for the cl program, with all the subcommands registered
         through the `Commands.command` decorator.
         '''
-        parser = argparse.ArgumentParser(prog='cl', add_help=False)
+        parser = argparse.ArgumentParser(prog='cl', add_help=False, formatter_class=argparse.RawTextHelpFormatter)
         parser.register('action', 'parsers', cls.AliasedSubParsersAction)
         cls.hack_formatter(parser)
         subparsers = parser.add_subparsers(dest='command', metavar='command')
 
         # Build subparser for each subcommand
         for command in cls.commands.itervalues():
-            subparser = subparsers.add_parser(command.name, help=command.help, description=command.help, aliases=command.aliases, add_help=True)
+            subparser = subparsers.add_parser(command.name, help=command.help, description=command.help, aliases=command.aliases, add_help=True, formatter_class=argparse.RawTextHelpFormatter)
 
             # Register arguments for the subcommand
             for argument in command.arguments:
@@ -244,56 +337,6 @@ class Commands(object):
 
 
 class BundleCLI(object):
-    BUNDLE_COMMANDS = (
-        'upload',
-        'make',
-        'run',
-        'edit',
-        'detach',
-        'rm',
-        'search',
-        'ls',
-        'info',
-        'cat',
-        'wait',
-        'download',
-        'cp',
-        'mimic',
-        'macro',
-        'kill',
-    )
-
-    WORKSHEET_COMMANDS = (
-        'new',
-        'add',
-        'work',
-        'print',
-        'wedit',
-        'wrm',
-        'wls',
-        'wcp',
-    )
-
-    GROUP_AND_PERMISSION_COMMANDS = (
-        'gls',
-        'gnew',
-        'grm',
-        'ginfo',
-        'uadd',
-        'urm',
-        'perm',
-        'wperm',
-        'chown',
-    )
-
-    OTHER_COMMANDS = (
-        'help',
-        'status',
-        'alias',
-        'work-manager',
-        'server',
-        'logout',
-    )
 
     def __init__(self, manager, headless=False):
         self.manager = manager
@@ -501,6 +544,10 @@ class BundleCLI(object):
         argv = self.collapse_bare_command(argv)
         args = parser.parse_args(argv)
 
+        # print args.help
+        # print args.command
+        # sys.exit()
+
         # Bind self (BundleCLI instance) and args to command function
         command_fn = lambda: args.function(self, args)
 
@@ -541,34 +588,7 @@ class BundleCLI(object):
             self.do_command([args.command, '--help'])
             return
 
-        print 'Usage: cl <command> <arguments>'
-        max_length = max(
-          len(command) for command in
-          itertools.chain(self.BUNDLE_COMMANDS,
-                          self.WORKSHEET_COMMANDS,
-                          self.GROUP_AND_PERMISSION_COMMANDS,
-                          self.OTHER_COMMANDS)
-        )
-        indent = 2
-        def print_command(command):
-            print '%s%s%s%s' % (
-              indent*' ',
-              command,
-              (indent + max_length - len(command))*' ',
-              Commands.get_help(command),
-            )
-        print '\nCommands for bundles:'
-        for command in self.BUNDLE_COMMANDS:
-            print_command(command)
-        print '\nCommands for worksheets:'
-        for command in self.WORKSHEET_COMMANDS:
-            print_command(command)
-        print '\nCommands for groups and permissions:'
-        for command in self.GROUP_AND_PERMISSION_COMMANDS:
-            print_command(command)
-        print '\nOther commands:'
-        for command in self.OTHER_COMMANDS:
-            print_command(command)
+        print Commands.help_text()
 
     @Commands.command(
         'status',
@@ -1451,28 +1471,40 @@ class BundleCLI(object):
                 ui_actions.OpenWorksheet(uuid)
             ])
 
+    ITEM_DESCRIPTION = textwrap.dedent("""
+    item specifications, with the format depending on the specified item_type.
+        text:      (<text>|%%<directive>)
+        bundle:    {0}
+        worksheet: {1}""").format(BUNDLE_SPEC_FORMAT, WORKSHEET_SPEC_FORMAT).strip()
+
     @Commands.command(
         'add',
-        help='Append worksheet links and/or text items to a worksheet.',
+        help='Append text items, bundles, or subworksheets to a worksheet.',
         arguments=(
-            Commands.Argument('subworksheet_spec', help='worksheets to add links to (%s)' % WORKSHEET_SPEC_FORMAT, nargs='*', completer=WorksheetsCompleter),
-            Commands.Argument('-m', '--message', help='add a text element (must appear after subworksheet_specs)', nargs='+'),
-            Commands.Argument('-w', '--worksheet_spec', help='operate on this worksheet (%s)' % WORKSHEET_SPEC_FORMAT, completer=WorksheetsCompleter),
+            Commands.Argument('item_type', help='{text, bundle, worksheet}', choices=('text', 'bundle', 'worksheet'), metavar='item_type'),
+            Commands.Argument('item_spec', help=ITEM_DESCRIPTION, nargs='+', completer=UnionCompleter(WorksheetsCompleter, BundlesCompleter)),
+            Commands.Argument('dest_worksheet', help='worksheet to which to add items %s' % WORKSHEET_SPEC_FORMAT, completer=WorksheetsCompleter),
         ),
     )
     def do_add_command(self, args):
-        client, worksheet_uuid = self.parse_client_worksheet_uuid(args.worksheet_spec)
+        client, dest_worksheet_uuid = self.parse_client_worksheet_uuid(args.dest_worksheet)
 
-        for spec in args.subworksheet_spec:
-            subworksheet_uuid = worksheet_util.get_worksheet_uuid(client, worksheet_uuid, spec)
-            client.add_worksheet_item(worksheet_uuid, worksheet_util.subworksheet_item(subworksheet_uuid))
-
-        if args.message is not None:
-            for message in args.message:
-                if message.startswith('%'):
-                    client.add_worksheet_item(worksheet_uuid, worksheet_util.directive_item(message[1:].strip()))
+        if args.item_type == 'text':
+            for item_spec in args.item_spec:
+                if item_spec.startswith('%'):
+                    client.add_worksheet_item(dest_worksheet_uuid, worksheet_util.directive_item(item_spec[1:].strip()))
                 else:
-                    client.add_worksheet_item(worksheet_uuid, worksheet_util.markup_item(message))
+                    client.add_worksheet_item(dest_worksheet_uuid, worksheet_util.markup_item(item_spec))
+
+        elif args.item_type == 'bundle':
+            bundle_uuids = worksheet_util.get_bundle_uuids(client, dest_worksheet_uuid, args.item_spec)
+            for bundle_uuid in bundle_uuids:
+                client.add_worksheet_item(dest_worksheet_uuid, worksheet_util.bundle_item(bundle_uuid))
+
+        elif args.item_type == 'worksheet':
+            for item_spec in args.item_spec:
+                subworksheet_uuid = worksheet_util.get_worksheet_uuid(client, dest_worksheet_uuid, item_spec)
+                client.add_worksheet_item(dest_worksheet_uuid, worksheet_util.subworksheet_item(subworksheet_uuid))
 
     @Commands.command(
         'work',
