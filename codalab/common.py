c0f39f46860c45eedca3369f6587e23e4fa63d9d
Andrew Duffy <root@aduffy.org>
2016-03-04 02:47:41 -0800

    Increment version to v0.1.5

--- codalab/common.py
+++ codalab/common.py
@@ -8,7 +8,7 @@ import httplib
 
 # Increment this on the develop branch when develop is merged into master.
 # http://semver.org/
-CODALAB_VERSION = '0.1.4'
+CODALAB_VERSION = '0.1.5'
 
 class IntegrityError(ValueError):
     """
