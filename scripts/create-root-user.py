529bbdcd99d239ce478da5d7937d0d5331fca6c9
Konstantin Lopyrev <klopyrev@gmail.com>
2016-03-05 12:03:40 -0800

    Read password from command line.

--- scripts/create-root-user.py
+++ scripts/create-root-user.py
@@ -14,12 +14,16 @@ model = manager.model()
 
 username = manager.root_user_name()
 user_id = manager.root_user_id()
-while True:
-    password = getpass.getpass()
-    if getpass.getpass('Config password: ') == password:
-        break
 
-    print 'Passwords don\'t match. Try again.'
-    print
+if len(sys.argv) == 2:
+    password = sys.argv[1]
+else:
+    while True:
+        password = getpass.getpass()
+        if getpass.getpass('Config password: ') == password:
+            break
+    
+        print 'Passwords don\'t match. Try again.'
+        print
 
 model.add_user(username, '', password, user_id, is_verified=True)
