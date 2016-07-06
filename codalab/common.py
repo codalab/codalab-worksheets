3fd8b88960e58f02957185e027f0cf278f48e51b
Percy Liang <percyliang@gmail.com>
2016-07-05 22:22:16 -0700

    increment version

--- codalab/common.py
+++ codalab/common.py
@@ -8,7 +8,7 @@ import httplib
 
 # Increment this on the develop branch when develop is merged into master.
 # http://semver.org/
-CODALAB_VERSION = '0.1.8'
+CODALAB_VERSION = '0.1.9'
 
 class IntegrityError(ValueError):
     """
