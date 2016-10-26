c5db54d45586eca7be3ba411dec3fca854deb27b
Stephen Koo <stephen.theking@gmail.com>
2016-10-25 21:37:13 -0700

    Fix typo in do_cat_command

--- codalab/lib/bundle_cli.py
+++ codalab/lib/bundle_cli.py
@@ -1568,7 +1568,7 @@ class BundleCLI(object):
         ),
     )
     def do_cat_command(self, args):
-        self._fail_if_headless('cat')  # Files might be too big
+        self._fail_if_headless(args)  # Files might be too big
 
         client, worksheet_uuid = self.parse_client_worksheet_uuid(args.worksheet_spec)
         target = self.parse_target(client, worksheet_uuid, args.target_spec)
