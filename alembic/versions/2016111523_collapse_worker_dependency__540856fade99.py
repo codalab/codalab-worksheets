2ced17a7fd3fe9cf5494cad47c6bbcfbecee4c8d
Stephen Koo <stephen.theking@gmail.com>
2016-11-16 12:39:22 -0800

    Migrate dependencies properly

--- alembic/versions/2016111523_collapse_worker_dependency__540856fade99.py
+++ alembic/versions/2016111523_collapse_worker_dependency__540856fade99.py
@@ -12,11 +12,26 @@ down_revision = '309cf9c796b9'
 
 from alembic import op
 import sqlalchemy as sa
+from codalab.model.worker_model import WorkerModel
+from codalab.model.tables import worker_dependency
 
 
 def upgrade():
     # Add active flag to worker
     op.add_column('worker', sa.Column('is_active', sa.Boolean(), nullable=False))
+
+    # Convert table to new format
+    conn = op.get_bind()
+    results = conn.execute("SELECT * FROM worker_dependency").fetchall()
+    dependencies = {}
+    for row in results:
+        dependencies[(row.user_id, row.worker_id)]['dependencies'].append((row.dependency_uuid, row.dependency_path))
+    op.execute("DELETE FROM `worker_dependency`;")
+    op.execute(worker_dependency.insert().values([{
+        'user_id': user_id,
+        'worker_id': worker_id,
+        'dependencies': WorkerModel._serialize_dependencies(deps),
+    } for (user_id, worker_id), deps in dependencies.iteritems()]))
 
     # Use worker_dependency as a key-value store, with serialized dependencies in the MEDIUMBLOB
     op.add_column('worker_dependency', sa.Column('dependencies', sa.LargeBinary(), nullable=False))
@@ -28,6 +43,9 @@ def upgrade():
 
 
 def downgrade():
+    # Clear table, which will be refilled by worker checkin
+    op.execute("DELETE FROM worker_dependency")
+
     # Need to temporarily drop foreign keys to drop the primary key constraint
     op.drop_constraint('worker_dependency_ibfk_1', 'worker_dependency', type_='foreignkey')
     op.drop_constraint('worker_dependency_ibfk_2', 'worker_dependency', type_='foreignkey')
