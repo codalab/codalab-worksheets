6dff5705835bd45b4a450577b4b2441f3e9e9720
Fabian Chan <fabianc@stanford.edu>
2017-02-03 14:16:12 -0800

    Add balloon stress-test

--- stress-test.py
+++ stress-test.py
@@ -1,5 +1,3 @@
-#import altf4
-
 import logging
 import os, sys
 import uuid
@@ -10,120 +8,138 @@ from cStringIO import StringIO
 
 from codalab.lib.bundle_cli import BundleCLI, Commands
 from codalab.lib.codalab_manager import CodaLabManager
+from codalab.common import UsageError
 
 from multiprocessing.dummy import Pool as ThreadPool
 
 logger = logging.getLogger(__file__)
 
-class Timer:
+class Timer(object):
     def __enter__(self):
         self.start = time.clock()
         return self
 
-    def __exit__(self, *args):
+    def __exit__(self, eType, eValue, eTrace):
         self.end = time.clock()
         self.interval = self.end - self.start
 
-def run_cl_command(args, timeout=30):
-    '''
-    Run command with timeout
+        if eType:
+            return False
 
-    Returns: exit_code, time_elapsed, output
+class CreateWorksheets(object):
+    def __init__(self, cli, n, multithread=True):
+        self.cli = cli
+        self.n = n
 
-    '''
+    def __enter__(self):
+        def create_worksheet(i):
+            ws = 'gregwasher91-test-worksheet-{}'.format(i)
+            try:
+                cli.do_command(['new', ws], cli=False)
+            except:
+                return None
+            return ws
 
-    start_time = time.time()
-    try:
-        args = ['timeout', '{}s'.format(timeout)] + args
-        output = subprocess.check_output(args)
-        exit_code = 0
-    except subprocess.CalledProcessError, e:
-        output = e.output
-        exit_code = e.returncode
-    end_time = time.time()
-    time_elapsed = end_time - start_time
-    print('Code {}, Time {}s, Ran: {}'.format(exit_code, int(time_elapsed), args))
+        if self.multithread:
+            array = [ i for i in range(n) ]
+            pool = ThreadPool(10)
+            self.ws_names = pool.map(create_worksheet, array)
+        else:
+            self.ws_names = [ create_worksheet(i) for i in range(n) ]
 
-    return exit_code, time_elapsed, output.rstrip()
+    def __exit__(self, eType, eValue, eTrace):
+        def delete_worksheet(ws):
+            return cli.do_command(['wrm', ws], cli=False)
 
-def run_stuff(i):
-    exit_code, time_elapsed, output = run_command(['cl', 'search', '.count'])
-    if exit_code != 0:
-        raise Exception(output)
-    #run_command(['cl', 'search', 'size=.sum'])
-    #run_command(['cl', 'search', 'size=.sort-', '.limit=5'])
-    #run_command(['cl', 'search', 'size=.sort-', '.limit=50'])
-    #run_command(['cl', 'search', 'size=.sort-', '.limit=500'])
-    #run_command(['cl', 'search', '.last', '.limit=5'])
-    #run_command(['cl', 'search', '.last', '.limit=500'])
+        ws_names = [ ws for ws in self.ws_names if ws ]
+        if self.multithread:
+            pool = ThreadPool(10)
+            ws_names = pool.map(delete_worksheet, ws_names)
+        else:
+            ws_names = [ delete_worksheet(w) for w in ws_names ]
 
-def create_worksheet(i):
-    ws = 'gregwasher91-test-worksheet-{}'.format(i)
-    try:
-        cli.do_command(['new', ws])
-    except:
-        return None
-    return ws
+        if eType:
+            return False
 
-def delete_worksheet(ws):
-    return cli.do_command(['wrm', ws])
+class UploadBalloons(object):
 
-def test_number_of_worksheets(cli, n, multithread=False):
+    def upload_balloon(self):
+        ''' tell codalab to download a balloon, aka a zip file that becomes big when unzipped '''
 
-    if multithread:
-        array = [ i for i in range(n) ]
-        pool = ThreadPool(50)
-        ws_names = pool.map(create_worksheet, array)
-    else:
-        ws_names = [ create_worksheet(i) for i in range(n) ]
+        self.cli.do_command([
+            'upload',
+            'http://cs246h.stanford.edu/map_only_data.zip' # 250MB per
+        ], cli=False)
 
-    with Timer() as t:
-        output = cli.do_command(['wsearch', '.mine'])
-    print 'Searching through ~{} worksheets took {}s'.format(n, t.interval)
+    def __init__(self, cli):
+        self.cli = cli
 
-    ws_names = [ ws for ws in ws_names if ws ]
-    if multithread:
-        pool = ThreadPool(50)
-        ws_names = pool.map(delete_worksheet, ws_names)
-    else:
-        ws_names = [ delete_worksheet(w) for w in ws_names ]
+    def __enter__(self):
+        try:
+            c = 0
+            while True:
+                self.upload_balloon()
+                c += 1
+        except UsageError:
+            print "Reached disk quota. Uploaded {} balloons".format(c)
 
-def upload_big_bundle(i):
-    return cli.do_command([
-        'upload',
-        'http://cs246h.stanford.edu/map_only_data.zip'
-    ])
 
-def test_upload_bundles(cli, n, multithread=False):
-    if multithread:
-        array = [ i for i in range(n) ]
-        pool = ThreadPool(50)
-        pool.map(upload_big_bundle, array)
-    else:
-        [ upload_big_bundle(i) for i in range(n) ]
+    def __exit__(self, eType, eValue, eTrace):
+        while 1: # remove uploaded bundles. Loop because only 10 results are returned at a time
+            bundles = [ r['uuid'] for r in self.cli.do_command(['search', '.mine', 'map_only_data'], cli=False)['refs'].values() ]
+            if not bundles:
+                return
+            self.cli.do_command(['rm'] + bundles, cli=False)
+        if eType:
+            return False
 
-    with Timer() as t:
-        output = cli.do_command(['ls'])
-    print 'Listing ~{} bundles took {}s'.format(n, t.interval)
+class UploadMemoryHog(object):
+    def __init__(self, cli):
+        self.cli = cli
 
-    while 1:
-        bundles = [ r['uuid'] for r in cli.do_command(['search', '.mine', 'map_only_data'])['refs'].values() ]
-        if not bundles:
-            return
-        cli.do_command(['rm'] + bundles)
+    def __enter__(self):
+        self.bundle = self.cli.do_command([
+            'upload',
+            'stress-test-data/memory_hog.py',
+        ], cli=False)
 
+    def __exit__(self, eType, eValue, eTrace):
+        self.cli.do_command([
+            'rm',
+            self.bundle['uuid']
+        ], cli=False)
+
+        if eType:
+            return False
 
 if __name__ == '__main__':
     logging.basicConfig(level='INFO')
-    logger.info('hi')
 
     cli = BundleCLI(CodaLabManager(), stdout=StringIO(), stderr=StringIO())
     # Make sure we can connect (might prompt for username/password)
     try:
-        cli.do_command(['work', 'https://worksheets-test.codalab.org/::gregwasher91-worksheetname'])
+        cli.do_command(['work', 'https://worksheets-test.codalab.org/::gregwasher91-worksheetname'], cli=False)
     except Exception, e:
         logger.error(e)
         sys.exit(1)
 
-    #test_number_of_worksheets(cli, 300, True)
-    test_upload_bundles(cli, 10, True)
+    with CreateWorksheets(cli, n):
+        with Timer() as t:
+            output = cli.do_command(['wsearch', '.mine'], cli=False)
+        print 'Searching through ~{} worksheets took {}s'.format(n, t.interval)
+
+    with UploadBalloons(cli):
+        with Timer() as t:
+            output = cli.do_command(['ls'], cli=False)
+        print 'Listing ~{} bundles took {}s'.format(len(output['refs']), t.interval)
+
+    with UploadMemoryHog(cli):
+        bundle = cli.do_command([
+            'run',
+            ':memory_hog.py',
+            '---',
+            'timeout 300s python memory_hog.py 500 30'
+        ], cli=False)
+
+        cli.do_command(['wait', bundle['uuid']], cli=False)
+        cli.do_command(['rm', bundle['uuid']], cli=False)
