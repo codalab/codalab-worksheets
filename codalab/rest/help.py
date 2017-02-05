779d0d464837260a83bed6ad6c34ca0f052b7d35
Max Wang <maxwang7@gmail.com>
2017-02-05 21:57:06 +0000

    begins help REST API endpoint implementation

---
+++ codalab/rest/help.py
@@ -0,0 +1,26 @@
+import httplib
+
+from bottle import abort, get, request, local, post
+
+from codalab.lib.spec_util import NAME_REGEX
+from codalab.lib.server_util import bottle_patch as patch
+from codalab.rest.schemas import (
+    AuthenticatedUserSchema,
+    USER_READ_ONLY_FIELDS,
+    UserSchema,
+)
+from codalab.server.authenticated_plugin import (
+    AuthenticatedPlugin,
+    UserVerifiedPlugin,
+)
+
+@post('/help/', apply=AuthenticatedPlugin(), skip=UserVerifiedPlugin)
+def fetch_help():
+    print 'inside of @get /help'
+    print request.user
+    print request.query
+    for param in request.params: print param
+    for query in request.query: print query
+    print ''
+    print '*' * 100
+    import pdb; pdb.set_trace()
