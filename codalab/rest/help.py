e6c459254d98877a880955b98d8a96a252e89499
Max Wang <maxwang7@gmail.com>
2017-02-06 01:26:01 +0000

    more edits, they may be breaking changes

--- codalab/rest/help.py
+++ codalab/rest/help.py
@@ -5,7 +5,7 @@ from bottle import abort, get, request, local, post
 from codalab.lib.spec_util import NAME_REGEX
 from codalab.lib.server_util import bottle_patch as patch
 from codalab.rest.schemas import (
-    AuthenticatedUserSchema,
+#    AuthenticatedUserSchema,
     USER_READ_ONLY_FIELDS,
     UserSchema,
 )
@@ -16,9 +16,11 @@ from codalab.server.authenticated_plugin import (
 
 @post('/help/', apply=AuthenticatedPlugin(), skip=UserVerifiedPlugin)
 def fetch_help():
-    print 'inside of @get /help'
     print request.user
     print request.query
+    # user = AuthenticatedUserSchema().dump(request.user).data
+    # print user
+
     for param in request.params: print param
     for query in request.query: print query
     print ''
