61a3bf97933724a6a28617108e8f8734201f4e88
Max Wang <maxwang7@gmail.com>
2017-02-06 04:57:24 +0000

    implements email REST API endpoint

--- codalab/rest/help.py
+++ codalab/rest/help.py
@@ -1,11 +1,11 @@
 import httplib
 
-from bottle import abort, get, request, local, post
+from bottle import abort, get, request, local, post, template
 
 from codalab.lib.spec_util import NAME_REGEX
 from codalab.lib.server_util import bottle_patch as patch
 from codalab.rest.schemas import (
-#    AuthenticatedUserSchema,
+    AuthenticatedUserSchema,
     USER_READ_ONLY_FIELDS,
     UserSchema,
 )
@@ -16,13 +16,15 @@ from codalab.server.authenticated_plugin import (
 
 @post('/help/', apply=AuthenticatedPlugin(), skip=UserVerifiedPlugin)
 def fetch_help():
-    print request.user
-    print request.query
-    # user = AuthenticatedUserSchema().dump(request.user).data
-    # print user
+    # import pdb; pdb.set_trace()
+    user = AuthenticatedUserSchema().dump(request.user).data
+    email = user['data']['attributes']['email']
+    username = user['data']['attributes']['user_name']
+    message = request.json['message']
 
-    for param in request.params: print param
-    for query in request.query: print query
-    print ''
-    print '*' * 100
-    import pdb; pdb.set_trace()
+    local.emailer.send_email(
+        subject="",
+        body=template('help_message_to_codalab_body', username=username, email=email, message=message),
+        recipient=email,
+    )
+    return '{}'
