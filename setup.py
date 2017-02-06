4352c577666b93ccd0be01092b27ab00d072442d
Max Wang <maxwang7@gmail.com>
2017-02-06 21:53:39 +0000

    drafts setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup
 setup(name='codalab-cli',
     version='0.0.1',
     description='CLI for CodaLab, a platform for reproducible computation',
-    long_description='See https://worksheets.codalab.org/ or setup your own instance',
+    long_description='Visit https://worksheets.codalab.org/ or setup your own instance',
     url='https://github.com/codalab/codalab-cli',
-    author='Percy Liang',
-    author_email='', # Percy's email?
+    author='CodaLab',
+    author_email='codalab.worksheets@gmail.com', # Percy's email?
     license='Apache License 2.0',
-    packages=['funniest'],
+    keywords='codalab reproducible computation worksheets competitions',
+    packages=['codalab'],
     install_requires=[
         'argcomplete==1.1.0',
         'PyYAML==3.11',
@@ -17,4 +18,7 @@ setup(name='codalab-cli',
         'SQLAlchemy==1.0.8',
         'watchdog==0.8.3',
     ],
-    zip_safe=False)
+    scripts=[
+        'codalab/bin/cl'
+    ],
+    zip_safe=False),
