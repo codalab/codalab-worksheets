b061a562f93548a619623f0a05bd970bc52e8ec8
maxwang7 <maxwang7@gmail.com>
2017-02-07 17:13:37 -0800

    pip install (#677)
    Packages codalab-cli into a pip package. Allows for `cl` statements.
    
    
    Individual commit messages:
    
    * creates basic setup.py file
    
    * drafts setup.py
    
    * packages codalab-cli
    
    * adds rest documentation - merge conflict?
    
    * upgrades version number
    
    * removes dead code
    
    * uses codalab/common.py's CODALB_VERSION as the version in setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+from codalab.common import CODALAB_VERSION
+
+
+setup(name='codalab-cli',
+    version=CODALAB_VERSION,
+    description='CLI for CodaLab, a platform for reproducible computation',
+    long_description='Visit https://worksheets.codalab.org/ or setup your own server by following the instructions in the Wiki.',
+    url='https://github.com/codalab/codalab-cli',
+    author='CodaLab',
+    author_email='codalab.worksheets@gmail.com', # Percy's email?
+    license='Apache License 2.0',
+    keywords='codalab reproducible computation worksheets competitions',
+    packages=find_packages(include=['codalab*', 'worker*']),
+    include_package_data=True,
+    install_requires=[
+        'argcomplete==1.1.0',
+        'PyYAML==3.11',
+        'psutil==3.3.0',
+        'six==1.10.0',
+        'SQLAlchemy==1.0.8',
+        'watchdog==0.8.3',
+    ],
+    entry_points={
+        'console_scripts': [
+            'cl=codalab.bin.cl:main',
+        ],
+    },
+    zip_safe=False),
