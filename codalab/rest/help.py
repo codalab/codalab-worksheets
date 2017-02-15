386d4ac829b1ad9329d62751f5bfdbd629bd023f
Max Wang <maxwang7@gmail.com>
2017-02-15 20:37:40 +0000

    adds user's name to email

--- codalab/rest/help.py
+++ codalab/rest/help.py
@@ -13,18 +13,20 @@ from codalab.server.authenticated_plugin import (
 @post('/help/', apply=AuthenticatedPlugin(), skip=UserVerifiedPlugin)
 def fetch_help():
     message = request.json['message']
-    if 'support_email' not in local.config:
+
+    if local.config and 'support_email' not in local.config['server']:
         print >>sys.stderr, 'Warning: No support_email configured, so no email sent.'
         print >>sys.stderr, 'User\'s message: %s' % message
         return
 
-    support_email = local.config['support_email']
+    support_email = local.config['server']['support_email']
     username = request.user.user_name
     user_email = request.user.email
+    real_name = ("%s %s" % (request.user.first_name, request.user.last_name))
 
     local.emailer.send_email(
         subject="Message from %s" % user_email,
-        body=template('help_message_to_codalab_body', username=username, email=user_email, message=message),
+        body=template('help_message_to_codalab_body', real_name=real_name, username=username, email=user_email, message=message),
         recipient=support_email,
         sender=user_email
     )
