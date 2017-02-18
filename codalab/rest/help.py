355ff3dcbbff08e3e116d9b918e1150f14e508f2
maxwang7 <maxwang7@gmail.com>
2017-02-17 19:44:20 -0800

    Adds unicode support in support emails (#689)
    * fixes escaping problem and uses the correct email as the user email
    
    * adds support for unicode support emails
    
    * adds docstring for mimetype and charset keyword arguments

--- codalab/rest/help.py
+++ codalab/rest/help.py
@@ -26,9 +26,15 @@ def fetch_help():
     last_name = request.user.last_name if request.user.last_name else ''
     real_name = ("%s %s" % (first_name, last_name))
 
+    first_name = request.user.first_name if request.user.first_name else ''
+    last_name = request.user.last_name if request.user.last_name else ''
+    real_name = ("%s %s" % (first_name, last_name))
+    message = message.encode('utf-8')
+
     local.emailer.send_email(
         subject="Message from %s" % user_email,
         body=template('help_message_to_codalab_body', real_name=real_name, username=username, email=user_email, message=message),
         recipient=support_email,
-        sender=user_email
+        sender=user_email,
+        charset='utf-8',
     )
