bf0f8a3ab413545b2fa41fe4969c0541151c5f61
Fabian Chan <fabianc@stanford.edu>
2017-05-16 03:32:12 -0700

    Fix dependency_manager_test

--- worker/dependency_manager.py
+++ worker/dependency_manager.py
@@ -18,7 +18,8 @@ class DependencyManager(object):
     def __init__(self, work_dir, max_work_dir_size_bytes, prevous_runs=[]):
         self._max_work_dir_size_bytes = max_work_dir_size_bytes
         self._state_file = os.path.join(work_dir, self.STATE_FILENAME)
-        self._work_dir = os.path.join(work_dir, 'bundles')
+        self._work_dir = work_dir
+        self._bundles_dir = os.path.join(work_dir, 'bundles')
         self._lock = threading.Lock()
         self._stop_cleanup = False
         self._cleanup_thread = None
@@ -31,6 +32,7 @@ class DependencyManager(object):
         else:
             remove_path(self._work_dir)
             os.makedirs(self._work_dir, 0770)
+            os.makedirs(self._bundles_dir, 0770)
             self._save_state()
 
     def _load_state(self, prevous_runs):
@@ -47,8 +49,8 @@ class DependencyManager(object):
             self._paths.add(uuid)
 
         # Remove paths that aren't complete (e.g. interrupted downloads and runs).
-        for path in set(os.listdir(self._work_dir)) - self._paths:
-            remove_path(os.path.join(self._work_dir, path))
+        for path in set(os.listdir(self._bundles_dir)) - self._paths:
+            remove_path(os.path.join(self._bundles_dir, path))
 
     def _save_state(self):
         """
@@ -86,7 +88,7 @@ class DependencyManager(object):
                     # that adds new bundles to the dependency manager simpler.
                     if dependency.size_bytes is None:
                         self._lock.release()
-                        size_bytes = get_path_size(os.path.join(self._work_dir,
+                        size_bytes = get_path_size(os.path.join(self._bundles_dir,
                                                                 dependency.path))
                         self._lock.acquire()
                         dependency.size_bytes = size_bytes
@@ -110,7 +112,7 @@ class DependencyManager(object):
                         del self._dependencies[first_used_target]
                         self._paths.remove(dependency.path)
                         self._save_state()
-                        remove_path(os.path.join(self._work_dir, dependency.path))
+                        remove_path(os.path.join(self._bundles_dir, dependency.path))
                 else:
                     break
 
@@ -167,7 +169,7 @@ class DependencyManager(object):
                     else:
                         # Already downloaded.
                         dependency.add_child(uuid)
-                        return os.path.join(self._work_dir, dependency.path), False
+                        return os.path.join(self._bundles_dir, dependency.path), False
                 else:
                     if parent_path:
                         path = os.path.join(parent_uuid, parent_path)
@@ -184,7 +186,7 @@ class DependencyManager(object):
                         Dependency(path, True, self._lock))
                     self._paths.add(path)
                     dependency.add_child(uuid)
-                    return os.path.join(self._work_dir, path), True
+                    return os.path.join(self._bundles_dir, path), True
 
     def finish_download(self, uuid, path, success):
         """
@@ -219,7 +221,7 @@ class DependencyManager(object):
                 self._save_state()
 
     def get_run_path(self, uuid):
-        return os.path.join(self._work_dir, uuid)
+        return os.path.join(self._bundles_dir, uuid)
 
     def finish_run(self, uuid):
         """
