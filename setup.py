9c9b827358a18e29511f030cfd8cbb877385aefd
Max Wang <maxwang7@gmail.com>
2017-05-18 08:27:54 +0000

    adds cl-worker script

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(name='codalab-cli',
     entry_points={
         'console_scripts': [
             'cl=codalab.bin.cl:main',
+            'cl-worker=worker.main:main',
         ],
     },
     zip_safe=False),
