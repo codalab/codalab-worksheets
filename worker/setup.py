a8d2bfabbe5a567a52a2f82cdee72649c495e11e
Max Wang <maxwang7@gmail.com>
2017-06-02 22:26:56 +0000

    updates version number for codalabworker

--- worker/setup.py
+++ worker/setup.py
@@ -12,9 +12,9 @@ def get_requirements(*requirements_file_paths):
     return requirements
 
 setup(name='codalabworker',
-    version='0.0.4',
+    version='0.2.8',
     description='Worker for CodaLab, a platform for reproducible computation',
-    long_description='To use your own hardware in CodaLab Worksheets, visit https://github.com/codalab/codalab-worksheets/wiki/Execution#running-your-own-workervisit. You can find the code at https://github.com/codalab/codalab-cli.',
+    long_description='To use your own hardware in CodaLab Worksheets, visit https://github.com/codalab/codalab-worksheets/wiki/Execution#running-your-own-worker. You can find the code at https://github.com/codalab/codalab-cli.',
     url='https://github.com/codalab/codalab-cli',
     author='CodaLab',
     author_email='codalab.worksheets@gmail.com',
