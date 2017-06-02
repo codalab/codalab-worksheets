66e7ade9ce7bcb3ad91034ba0ff1423fdc2e70e4
Max Wang <maxwang7@gmail.com>
2017-06-02 08:02:09 +0000

    moves upgrading setuptools to before installing requirements

--- setup.sh
+++ setup.sh
@@ -42,6 +42,8 @@ if [ ! -e $env ]; then
   echo
 fi
 
+$env/bin/pip install -U setuptools
+
 echo -e "${info}[*] Installing Python packages into $env...${reset}"
 if [ "$1" == "server" ]; then
   $env/bin/pip install -r $codalabdir/requirements-server.txt
@@ -50,8 +52,6 @@ elif [ "$1" == "dev" ]; then
 else
   $env/bin/pip install -r $codalabdir/requirements.txt
 fi
-
-$env/bin/pip install -U setuptools
 
 ( # try
     $env/bin/pip install psutil || exit 1
