06f5bf12502c324b914fc117155de1026fc4884d
Max Wang <maxwang7@gmail.com>
2017-11-01 08:32:52 +0000

    increments version to 0.2.19

--- codalab/common.py
+++ codalab/common.py
@@ -8,7 +8,7 @@ import httplib
 
 # Increment this on the develop branch when develop is merged into master.
 # http://semver.org/
-CODALAB_VERSION = '0.2.18'
+CODALAB_VERSION = '0.2.19'
 
 class IntegrityError(ValueError):
     """
