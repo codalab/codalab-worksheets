234f3d927abb42c39113c9b88008e1894cd5efc6
Fabian Chan <fabianc@stanford.edu>
2018-01-19 17:53:03 -0800

    Update version

--- codalab/common.py
+++ codalab/common.py
@@ -8,7 +8,7 @@ import httplib
 
 # Increment this on the develop branch when develop is merged into master.
 # http://semver.org/
-CODALAB_VERSION = '0.2.21'
+CODALAB_VERSION = '0.2.22'
 
 class IntegrityError(ValueError):
     """
