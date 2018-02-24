997a6d11c0cb2263731654f3ad62a7a501ece21b
Kerem Goksel <bkgoksel@gmail.com>
2018-02-23 16:36:27 -0800

    parse_size in worker formatting now returns an int (number of bytes) instead of float

--- codalab/worker/default_bundle_manager.py
+++ codalab/worker/default_bundle_manager.py
@@ -34,7 +34,7 @@ class DefaultBundleManager(BundleManager):
         self._schedule_run_bundles_on_workers(workers, user_owned=True)
         self._schedule_run_bundles_on_workers(workers, user_owned=False)
 
-    def _fail_on_excess_resource(self, value, user_fail_string=None, global_fail_string=None, user_max=None, global_max=None):
+    def _check_resource_failure(self, value, user_fail_string=None, global_fail_string=None, user_max=None, global_max=None):
         if value:
             if user_max and value > user_max:
                 return user_fail_string % (value, user_max)
@@ -55,24 +55,24 @@ class DefaultBundleManager(BundleManager):
 
             failures = []
 
-            failures.append(self._fail_on_excess_resource(
+            failures.append(self._check_resource_failure(
                     self._compute_request_cpus(bundle),
                     global_fail_string='No workers available with %d CPUs, max available: %d',
                     global_max=max(map(lambda worker: worker['cpus'], workers_list))))
 
-            failures.append(self._fail_on_excess_resource(
+            failures.append(self._check_resource_failure(
                     self._compute_request_gpus(bundle),
                     global_fail_string='No workers available with %d GPUs, max available: %d',
                     global_max=max(map(lambda worker: worker['gpus'], workers_list))))
 
-            failures.append(self._fail_on_excess_resource(
+            failures.append(self._check_resource_failure(
                     self._compute_request_disk(bundle),
                     user_fail_string='Requested more disk (%s) than user disk quota left (%s)',
                     user_max=self._model.get_user_disk_quota_left(bundle.owner_id),
                     global_fail_string='Maximum job disk size (%s) exceeded (%s)',
                     global_max=self._max_request_disk))
 
-            failures.append(self._fail_on_excess_resource(
+            failures.append(self._check_resource_failure(
                     self._compute_request_time(bundle),
                     user_fail_string='Requested more time (%s) than user time quota left (%s)',
                     user_max=self._model.get_user_time_quota_left(bundle.owner_id),
