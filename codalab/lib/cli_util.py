da60488cb90ec0cbf35a9c29082bfd1bf9e4731e
Kerem Goksel <bkgoksel@stanford.edu>
2018-03-08 11:18:57 -0800

    Add support for referring to bundles from other worksheets using `worksheet//bundle` syntax (#870)
    * Initial commit with the first proposal to support //
    
    * Added tests for new double slash syntax
    
    * Refactors around bundle spec parsing
    
    * Remove if False wrapped profiling
    
    * Add documentation
    
    * Trim worksheet spec when parsing target
    
    * Fixed desugar_command documentation

--- codalab/lib/cli_util.py
+++ codalab/lib/cli_util.py
@@ -3,7 +3,7 @@ import re
 from codalab.common import precondition, UsageError
 
 
-def nested_dict_get(o, *args, **kwargs):
+def nested_dict_get(obj, *args, **kwargs):
     """
     Get a value from a nested dictionary.
 
@@ -24,11 +24,38 @@ def nested_dict_get(o, *args, **kwargs):
     precondition(not kwargs, 'unsupported kwargs %s' % kwargs.keys())
     try:
         for arg in args:
-            o = o[arg]
-        return o
+            obj = obj[arg]
+        return obj
     except (KeyError, TypeError):
         return default
 
+def parse_target_spec(spec):
+    """
+    Helper: takes in a target spec in the form of
+        [[<key>]:][<instance>::][<worksheet_spec>//]<bundle_spec>[/<subpath>]
+    where <bundle_spec> is required and the rest are optional.
+    Returns a (<key>, <value_spec>) tuple where <value_spec> is everything after
+        the key and <key> is one of the following:
+        - if <spec> starts with '<key>:', <key> is returned as the key
+        - if <spec> starts with ':' (without a key before the ':'),
+            <bundle_spec>[/subpath] is returned as the key
+        - if <spec> doesn't include a ':', empty string is returned as the key
+    """
+    key = ''
+    value_spec = ''
+    if '::' in spec:
+        prefix, suffix = spec.split('::')
+    else:
+        prefix, suffix = spec, None
+    if ':' in prefix:  # :<value_spec> or <key>:<value_spec>
+        key, bundle_prefix = prefix.split(':', 1)
+        value_spec = bundle_prefix if suffix is None else bundle_prefix + "::" + suffix
+        if key == '':
+            bundle_spec = value_spec.split('//', 1)[1] if '//' in value_spec else value_spec
+            key = bundle_spec
+    else:  # <value_spec>
+        value_spec = spec
+    return key, value_spec
 
 def desugar_command(orig_target_spec, command):
     """
@@ -36,7 +63,10 @@ def desugar_command(orig_target_spec, command):
     Examples:
     - %a.txt% => [b1:a.txt], b1
     - %:a.txt% => [:a.txt], a.txt (implicit key is a.txt)
+    - %instance::ws//a.txt% => [b1:instance::ws//a.txt], b1
     - %corenlp%/run %a.txt% => [b1:corenlp, b2:a.txt], b1/run b2
+    - %:word-vectors//glove.6B%/vector.txt =>
+        [glove.6B/vector.txt:word-vectors//glove.6B/vector.txt], glove.6B/vector.txt
     """
     # If key is not specified, use b1, b2, b3 by default.
     pattern = re.compile('^([^%]*)%([^%]+)%(.*)$')
@@ -46,11 +76,8 @@ def desugar_command(orig_target_spec, command):
     val2key = {}  # e.g., a.txt => b1 (use first key)
 
     def get(dep):  # Return the key
-        if ':' in dep:  # :<bundle_spec> or <key>:<bundle_spec>
-            key, val = dep.split(':', 1)
-            if key == '':
-                key = val
-        else:  # <bundle_spec>
+        key, val = parse_target_spec(dep)
+        if key == '':
             val = dep
             if val in val2key:
                 key = val2key[val]
@@ -68,15 +95,15 @@ def desugar_command(orig_target_spec, command):
         return key
 
     target_spec = []
-    for x in orig_target_spec:
-        get(x)
+    for spec in orig_target_spec:
+        get(spec)
 
     while True:
-        m = pattern.match(command)
-        if not m:
+        match = pattern.match(command)
+        if not match:
             break
 
-        buf += m.group(1) + get(m.group(2))
-        command = m.group(3)
+        buf += match.group(1) + get(match.group(2))
+        command = match.group(3)
 
     return (target_spec, buf + command)
