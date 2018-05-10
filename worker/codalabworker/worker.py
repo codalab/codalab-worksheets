230934fdce8d1d636c8c6c1c2d9d5a761c015afa
Kerem Goksel <bkgoksel@gmail.com>
2018-05-10 16:58:16 -0700

    Got it to run locally

--- worker/codalabworker/worker.py
+++ worker/codalabworker/worker.py
@@ -52,7 +52,6 @@ class Worker(object):
                 self._last_checkin_successful = False
                 traceback.print_exc()
                 time.sleep(1)
-
         self._run_manager.stop()
         self._image_manager.stop()
         self._dependency_manager.stop()
