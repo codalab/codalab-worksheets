f78efb5404893b878be64d4796778623e54f901d
Dennis Jeong <wonjeo@gmail.com>
2018-05-19 01:36:49 -0700

    Added ability to search for worksheets shared with you through groups (#914)
    * Added ability to search for worksheets shared with you through groups
    
    * fixing errors, also adding condition that permission must be greater than or equal to GROUP_OBJECT_PERMISSION_READ (there are not any permission objects with permission value GROUP_OBJECT_PERMISSION_NONE, since they are just removed instead, but just making sure.)

--- codalab/lib/bundle_cli.py
+++ codalab/lib/bundle_cli.py
@@ -2378,8 +2378,10 @@ class BundleCLI(object):
         aliases=('wsearch', 'ws'),
         help=[
             'List worksheets on the current instance matching the given keywords.',
-            '  wls tag=paper : List worksheets tagged as "paper".',
-            '  wls .mine     : List my worksheets.',
+            '  wls tag=paper           : List worksheets tagged as "paper".',
+            '  wls group=<group_spec>  : List worksheets shared with the group identfied by group_spec.',
+            '  wls .mine               : List my worksheets.',
+            '  wls .shared             : List worksheets that have been shared with any of the groups I am in.',
         ],
         arguments=(
             Commands.Argument('keywords', help='Keywords to search for.', nargs='*'),
