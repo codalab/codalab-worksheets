4c38e985ae0d487332a3357e6dc6286925a7c469
Kerem Goksel <bkgoksel@gmail.com>
2018-06-19 11:05:29 -0700

    Remove pyfakefs dependency as it breaks on Travis

--- tests/worker/file_util_test.py
+++ tests/worker/file_util_test.py
@@ -2,7 +2,6 @@ import os
 import tempfile
 import unittest
 import bz2
-import fake_filesystem_unittest
 
 from codalabworker.file_util import (gzip_file,
                                      gzip_string,
@@ -14,10 +13,7 @@ from codalabworker.file_util import (gzip_file,
                                      un_tar_directory)
 
 
-class FileUtilTest(fake_filesystem_unittest.TestCase):
-    def setUp(self):
-        self.setUpPyfakefs()
-
+class FileUtilTest(unittest.TestCase):
     def test_tar_has_files(self):
         dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'files')
         temp_dir = tempfile.mkdtemp()
