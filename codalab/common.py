206b298b3b4b188a4f31ba0d21a69a33326bcbba
Kerem Goksel <bkgoksel@gmail.com>
2018-07-24 17:53:12 -0700

    Last component of adding preparing state

--- codalab/common.py
+++ codalab/common.py
@@ -100,14 +100,15 @@ class State(object):
     MAKING = 'making'  # Creating a make bundle.
     WAITING_FOR_WORKER_STARTUP = 'waiting_for_worker_startup'  # Waiting for the worker to start up.
     STARTING = 'starting'  # Wait for the worker to start running the bundle.
+    PREPARING = 'preparing' # Wait for worker to download dependencies and docker images
     RUNNING = 'running'   # Actually running
     READY = 'ready'       # Done running and succeeded
     FAILED = 'failed'     # Done running and failed
     KILLED = 'killed'     # Killed by user
     WORKER_OFFLINE = 'worker_offline'  # Assigned worker has gone offline
 
-    OPTIONS = {CREATED, STAGED, MAKING, WAITING_FOR_WORKER_STARTUP, STARTING, RUNNING, READY, FAILED}
-    ACTIVE_STATES = {MAKING, WAITING_FOR_WORKER_STARTUP, STARTING, RUNNING}
+    OPTIONS = {CREATED, STAGED, MAKING, WAITING_FOR_WORKER_STARTUP, STARTING, RUNNING, READY, FAILED, PREPARING}
+    ACTIVE_STATES = {MAKING, WAITING_FOR_WORKER_STARTUP, STARTING, RUNNING, PREPARING}
     FINAL_STATES = {READY, FAILED, KILLED}
 
 
