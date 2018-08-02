b489e25a9bbcc8c950066ee356c23e8473bac84c
Kerem Goksel <bkgoksel@gmail.com>
2018-08-02 16:36:09 -0700

    Version 0.2.30, fix pip dependencies after new changes

--- codalab/common.py
+++ codalab/common.py
@@ -8,7 +8,7 @@ import httplib
 
 # Increment this on the develop branch when develop is merged into master.
 # http://semver.org/
-CODALAB_VERSION = '0.2.28'
+CODALAB_VERSION = '0.2.30'
 
 
 class IntegrityError(ValueError):
