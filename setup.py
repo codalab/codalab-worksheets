564de79dced1d72039723e88c7e92ca97311b6f8
Kerem Goksel <bkgoksel@gmail.com>
2018-08-02 16:36:09 -0700

    Version 0.2.30, fix pip dependencies after new changes

--- setup.py
+++ setup.py
@@ -5,22 +5,29 @@ import setuptools
 from codalab.common import CODALAB_VERSION
 
 if int(setuptools.__version__.split('.')[0]) < 25:
-    print ("WARNING: Please upgrade setuptools to a newer version, otherwise installation may break. "
+    print(
+        "WARNING: Please upgrade setuptools to a newer version, otherwise installation may break. "
         "Recommended command: `pip install -U setuptools`")
 
+
 def get_requirements(*requirements_file_paths):
-    requirements = []
+    requirements = ['codalabworker']
     for requirements_file_path in requirements_file_paths:
         with open(requirements_file_path) as requirements_file:
             for line in requirements_file:
                 if line[0:2] != '-r' and line.find('git') == -1:
                     requirements.append(line.strip())
     return requirements
-setup(name='codalab',
+
+
+setup(
+    name='codalab',
     version=CODALAB_VERSION,
     description='CLI for CodaLab, a platform for reproducible computation',
-    long_description=('Visit https://worksheets.codalab.org/ or setup your own server by following the '
-                      'instructions in the Wiki (https://github.com/codalab/codalab-worksheets/wiki/Server-Setup).'),
+    long_description=
+    ('Visit https://worksheets.codalab.org/ or setup your own server by following the '
+     'instructions in the Wiki (https://github.com/codalab/codalab-worksheets/wiki/Server-Setup).'
+     ),
     url='https://github.com/codalab/codalab-cli',
     author='CodaLab',
     author_email='codalab.worksheets@gmail.com',
