dfffcbf4cf1b8d4c13a34f2f378ea11a50a0c729
Kerem Goksel <bkgoksel@gmail.com>
2018-08-01 18:44:28 -0700

    Update version, lint some files with YAPF

--- worker/setup.py
+++ worker/setup.py
@@ -2,19 +2,24 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 import os
 
+
 def get_requirements(*requirements_file_paths):
     requirements = []
     for requirements_file_path in requirements_file_paths:
         with open(requirements_file_path) as requirements_file:
             for line in requirements_file:
-                if line[0:2] != '-r' and line.find('git') == -1 and line.find('codalabworker') == -1:
+                if line[0:2] != '-r' and line.find('git') == -1 and line.find(
+                        'codalabworker') == -1:
                     requirements.append(line)
     return requirements
 
-setup(name='codalabworker',
-    version='0.2.23',
+
+setup(
+    name='codalabworker',
+    version='0.2.27',
     description='Worker for CodaLab, a platform for reproducible computation',
-    long_description='To use your own hardware in CodaLab Worksheets, visit https://github.com/codalab/codalab-worksheets/wiki/Execution#running-your-own-worker. You can find the code at https://github.com/codalab/codalab-cli.',
+    long_description=
+    'To use your own hardware in CodaLab Worksheets, visit https://github.com/codalab/codalab-worksheets/wiki/Execution#running-your-own-worker. You can find the code at https://github.com/codalab/codalab-cli.',
     url='https://github.com/codalab/codalab-cli',
     author='CodaLab',
     author_email='codalab.worksheets@gmail.com',
