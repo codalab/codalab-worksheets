595ba9102f0363e0cde8730414dc180ee420a43a
Kerem Goksel <bkgoksel@gmail.com>
2018-08-13 16:03:55 -0700

    make worker verbose

--- scripts/slurm-worker.py
+++ scripts/slurm-worker.py
@@ -7,7 +7,7 @@ from codalab.lib.formatting import parse_duration
 SERVER_INSTANCE = 'https://worksheets-dev.codalab.org'
 BASE_NLPRUN_INVOCATION = '/u/nlp/bin/nlprun'
 DEFAULT_PASSWORD_FILE_LOCATION = '~/codalab.password'
-BASE_WORKER_INVOCATION = 'cl-worker --server {} --password {}'.format(SERVER_INSTANCE, DEFAULT_PASSWORD_FILE_LOCATION)
+BASE_WORKER_INVOCATION = 'cl-worker --verbose --server {} --password {}'.format(SERVER_INSTANCE, DEFAULT_PASSWORD_FILE_LOCATION)
 SLEEP_INTERVAL = 30
 FIELDS = ['uuid', 'request_cpus', 'request_gpus', 'request_memory', 'request_time', 'tag']
 
