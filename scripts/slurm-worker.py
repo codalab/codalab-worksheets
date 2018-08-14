3fc4cc4b0e1f8ad76453ac9c1c91df99144c879f
Kerem Goksel <bkgoksel@gmail.com>
2018-08-13 22:05:54 -0700

    Refactor

--- scripts/slurm-worker.py
+++ scripts/slurm-worker.py
@@ -5,17 +5,72 @@ import time
 from codalab.lib.formatting import parse_duration
 
 SERVER_INSTANCE = 'https://worksheets-dev.codalab.org'
-BASE_NLPRUN_INVOCATION = '/u/nlp/bin/nlprun'
+NLPRUN_BINARY = '/u/nlp/bin/nlprun'
+CL_WORKER_BINARY = 'u/nlp/bin/cl-worker'
 DEFAULT_PASSWORD_FILE_LOCATION = '~/codalab.password'
-BASE_WORKER_INVOCATION = 'cl-worker --verbose --server {} --password {}'.format(SERVER_INSTANCE, DEFAULT_PASSWORD_FILE_LOCATION)
 SLEEP_INTERVAL = 30
 FIELDS = ['uuid', 'request_cpus', 'request_gpus', 'request_memory', 'request_time', 'tag']
+
+
+def get_worker_invocation(
+    server=SERVER_INSTANCE,
+    password=DEFAULT_PASSWORD_FILE_LOCATION,
+    work_dir=None,
+    tag=None,
+    verbose=False,
+):
+    flags = []
+    flags.append('--server {}'.format(server))
+    flags.append('--password {}'.format(password))
+    if work_dir is not None:
+        flags.append('--work-dir {}'.format(work_dir))
+    if tag is not None:
+        flags.append('--tag {}'.format(tag))
+    if verbose:
+        flags.append('--verbose')
+    return '{} {}'.format(CL_WORKER_BINARY, ' '.join(flags))
+
+
+def start_worker_for(run_number, run_fields, nlprun_binary=NLPRUN_BINARY):
+    if 'jag_hi' in run_fields['tags']:
+        queue = 'jag'
+        priority = 'high'
+        tag = 'jag-hi'
+    elif run_fields['request_gpus']:
+        queue = 'jag'
+        priority = 'low'
+    else:
+        queue = 'john'
+        priority = 'low'
+
+    nlprun_flags = [
+        nlprun_binary,
+        '-q {}'.format(queue),
+        '-c {}'.format(run_fields['num_cpus']),
+        '-r {}'.format(run_fields['request_memory']),
+        '-g {}'.format(run_fields['request_gpus']),
+        '-p {}'.format(priority),
+    ]
+
+    run_command = ' '.join(nlprun_flags)
+    worker_home = '~/worker-{}'.format(run_number)
+    prepare_command = 'mkdir {}'.format(worker_home)
+    cleanup_command = 'rm -rf {}'.format(worker_home)
+    worker_invocation = get_worker_invocation(work_dir=worker_home, tag=tag, verbose=True)
+    final_command = '{} \'{}; {}; {}\''.format(
+        run_command, prepare_command, worker_invocation, cleanup_command
+    )
+    print(final_command)
+    subprocess.check_call(final_command, shell=True)
+    print('Started worker for run {}'.format(run_fields['uuid']))
 
 
 def parse_field(field, val):
     if field == 'request_time':
         duration_in_secs = parse_duration(val)
-        return str(int(math.ceil(duration_in_secs)) * 60)
+        return int(math.ceil(duration_in_secs)) * 60
+    elif field in ['request_cpus', 'request_gpus']:
+        return int(val)
     else:
         return val
 
@@ -28,11 +83,10 @@ def main():
     # Infinite loop until user kills us
     num_runs = 0
     while True:
-        run_lines = subprocess.check_output('cl search .mine state=staged', shell=True)
-        run_lines = run_lines.splitlines()[2:]
+        run_lines = subprocess.check_output('cl search .mine state=staged -u', shell=True)
+        uuids = run_lines.splitlines()
         runs = []
-        for run in run_lines:
-            uuid = run.split()[0]
+        for uuid in uuids:
             info_cmd = 'cl info {} -f {}'.format(uuid, ','.join(FIELDS))
             field_values = subprocess.check_output(info_cmd, shell=True)
             run_info = {
@@ -40,52 +94,9 @@ def main():
             }
             runs.append(run_info)
         if runs:
-            cpu_runs = [run for run in runs if run['request_gpus'] == '0']
-            jag_hi_runs = [
-                run for run in runs if run['request_gpus'] != '0' and 'jag-hi' in run['tags']
-            ]
-            jag_lo_runs = [
-                run for run in runs if run['request_gpus'] != '0' and 'jag-hi' not in run['tags']
-            ]
-            for run in cpu_runs:
-                run_command = '{0} -q john -r {request_memory} -c {request_cpus}'.format(
-                    BASE_NLPRUN_INVOCATION, **run
-                )
-                worker_home = '~/worker-{}'.format(num_runs)
+            for run in runs:
+                start_worker_for(num_runs, run)
                 num_runs += 1
-                prepare_command = 'mkdir {}'.format(worker_home)
-                cleanup_command = 'rm -rf {}'.format(worker_home)
-                worker_invocation = '{} --work-dir {}'.format(BASE_WORKER_INVOCATION, worker_home)
-                final_command = '{} \'{}; {}; {}\''.format(run_command, prepare_command, worker_invocation, cleanup_command)
-                print(final_command)
-                subprocess.check_call(final_command, shell=True)
-                print('Started worker for run {uuid}'.format(**run))
-            for run in jag_hi_runs:
-                run_command = '{0} -q jag -p high -r {request_memory} -c {request_cpus} -g {request_gpus}'.format(
-                    BASE_NLPRUN_INVOCATION, **run
-                )
-                worker_home = '~/worker-{}'.format(num_runs)
-                num_runs += 1
-                worker_invocation = '{} --work-dir {} --tag jag-hi'.format(BASE_WORKER_INVOCATION, worker_home)
-                prepare_command = 'mkdir {}'.format(worker_home)
-                cleanup_command = 'rm -rf {}'.format(worker_home)
-                final_command = '{} \'{}; {}; {}\''.format(run_command, prepare_command, worker_invocation, cleanup_command)
-                print(final_command)
-                subprocess.check_call(final_command, shell=True)
-                print('Started worker for run {uuid}'.format(**run))
-            for run in jag_lo_runs:
-                run_command = '{0} -q jag -p low -r {request_memory} -c {request_cpus} -g {request_gpus}'.format(
-                    BASE_NLPRUN_INVOCATION, **run
-                )
-                worker_home = '~/worker-{}'.format(num_runs)
-                num_runs += 1
-                worker_invocation = '{} --work-dir {}'.format(BASE_WORKER_INVOCATION, worker_home)
-                prepare_command = 'mkdir {}'.format(worker_home)
-                cleanup_command = 'rm -rf {}'.format(worker_home)
-                final_command = '{} \'{}; {}; {}\''.format(run_command, prepare_command, worker_invocation, cleanup_command)
-                print(final_command)
-                subprocess.check_call(final_command, shell=True)
-                print('Started worker for run {uuid}'.format(**run))
 
         time.sleep(SLEEP_INTERVAL)
 
