ee7399d998da8b775c67be4abc443c1d25e144ed
Kerem Goksel <bkgoksel@gmail.com>
2018-09-05 10:12:33 -0700

    Return 1 less than quota left to prevent failures

--- codalab/worker/bundle_manager.py
+++ codalab/worker/bundle_manager.py
@@ -463,7 +463,7 @@ class BundleManager(object):
         The default is however much disk quota the user has left.
         """
         if not bundle.metadata.request_disk:
-            return (self._model.get_user_disk_quota_left(bundle.owner_id),)
+            return self._model.get_user_disk_quota_left(bundle.owner_id) - 1
         return formatting.parse_size(bundle.metadata.request_disk)
 
     def _compute_request_time(self, bundle):
@@ -472,7 +472,7 @@ class BundleManager(object):
         The default is however much time quota the user has left.
         """
         if not bundle.metadata.request_time:
-            return (self._model.get_user_time_quota_left(bundle.owner_id),)
+            return self._model.get_user_time_quota_left(bundle.owner_id) - 1
         return formatting.parse_duration(bundle.metadata.request_time)
 
     def _get_docker_image(self, bundle):
