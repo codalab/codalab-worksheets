63ac49fecf6ca037d99bec391c53e1aec671d01f
Kerem Goksel <bkgoksel@gmail.com>
2018-09-20 23:37:19 +0300

    Bump version to 0.2.35

--- codalab/common.py
+++ codalab/common.py
@@ -8,7 +8,7 @@ import httplib
 
 # Increment this on the develop branch when develop is merged into master.
 # http://semver.org/
-CODALAB_VERSION = '0.2.34'
+CODALAB_VERSION = '0.2.35'
 
 
 class IntegrityError(ValueError):
