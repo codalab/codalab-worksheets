2cbcb3d601698d1df64f0d227dd5dd2d8c80a1c4
Kerem Goksel <bkgoksel@gmail.com>
2018-10-01 07:24:44 +0000

    pip install worker in venv on server-side setup and get rid of worker.sh

--- setup.sh
+++ setup.sh
@@ -34,6 +34,7 @@ fi
 echo
 
 codalabdir=`dirname $0`
+workerdir=$codalabdir/worker
 env=$codalabdir/venv
 
 if [ ! -e $env ]; then
@@ -47,6 +48,7 @@ $env/bin/pip install -U setuptools
 echo -e "${info}[*] Installing Python packages into $env...${reset}"
 if [ "$1" == "server" ]; then
   $env/bin/pip install -r $codalabdir/requirements-server.txt
+  $env/bin/pip install -e $workerdir
 elif [ "$1" == "client" ]; then
   $env/bin/pip install -r $codalabdir/requirements.txt
 fi
