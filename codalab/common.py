cc3686af2719a3fab3be61891614c14fdf46cae7
Nikhil Bhattasali <nikhilxb@stanford.edu>
2018-10-10 17:18:20 -0700

    Update version to 0.2.36

--- codalab/common.py
+++ codalab/common.py
@@ -8,7 +8,7 @@ import httplib
 
 # Increment this on the develop branch when develop is merged into master.
 # http://semver.org/
-CODALAB_VERSION = '0.2.35'
+CODALAB_VERSION = '0.2.36'
 
 
 class IntegrityError(ValueError):
