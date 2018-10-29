89818de84478eb2fdf2faa0c29e5ee6aae283e39
Kerem Goksel <bkgoksel@gmail.com>
2018-10-29 12:02:58 -0700

    use travis deploy to build and push docker images to dockerhub on master, staging and successful releases

--- travis_build_images.sh
+++ travis_build_images.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+IMAGE_TAG=$1
 
-if [ "$BRANCH" == "master" || "$BRANCH" == "staging" ]; then
-  docker build . -f docker/Dockerfile.server -t codalab/bundleserver:$BRANCH
-  docker build . -f docker/Dockerfile.worker -t codalab/worker:$BRANCH
-  docker build . -f docker/Dockerfile.cpu -t codalab/default-cpu:$BRANCH
-  docker build . -f docker/Dockerfile.gpu -t codalab/default-gpu:$BRANCH
-  docker login -u $DOCKER_USER -p $DOCKER_PWD
-  docker push codalab/bundleserver:$BRANCH
-  docker push codalab/worker:$BRANCH
-  docker push codalab/default-cpu:$BRANCH
-  docker push codalab/default-gpu:$BRANCH
+docker build . -f docker/Dockerfile.server -t codalab/bundleserver:$IMAGE_TAG
+docker build . -f docker/Dockerfile.worker -t codalab/worker:$IMAGE_TAG
+docker build . -f docker/Dockerfile.cpu -t codalab/default-cpu:$IMAGE_TAG
+docker build . -f docker/Dockerfile.gpu -t codalab/default-gpu:$IMAGE_TAG
+docker login -u $DOCKER_USER -p $DOCKER_PWD
+docker push codalab/bundleserver:$IMAGE_TAG
+docker push codalab/worker:$IMAGE_TAG
+docker push codalab/default-cpu:$IMAGE_TAG
+docker push codalab/default-gpu:$IMAGE_TAG
+if [ "$TRAVIS_BRANCH" == 'release' ]; then
+docker build . -f docker/Dockerfile.cpu -t codalab/default-cpu:latest
+docker build . -f docker/Dockerfile.gpu -t codalab/default-gpu:latest
+  docker push codalab/default-cpu:latest
+  docker push codalab/default-gpu:latest
 fi
