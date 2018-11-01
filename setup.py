a4d4d9805ad77c1295d064a5244badbad58acf23
Kerem Goksel <bkgoksel@gmail.com>
2018-11-01 10:57:22 -0700

    Remove broken OS reference from setup py files

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
         "Programming Language :: Python :: 2 :: Only",
         "Programming Language :: Python :: 2.7",
         "License :: OSI Approved :: Apache Software License",
-        "Operating System :: UNIX",
     ],
     python_requires='~=2.7',
     include_package_data=True,
