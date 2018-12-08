e629b15fd22018b348eb9bcfc12499559fc1d1af
Kerem Goksel <bkgoksel@gmail.com>
2018-12-08 01:36:35 +0000

    Got local setup working

--- docker/service/initialize-db.sh
+++ docker/service/initialize-db.sh
@@ -11,13 +11,14 @@ done
 
 >&2 echo "Mysql server responding - initializing database"
 
-mysql -h mysql -u root -pROOTPWD -e "GRANT ALL PRIVILEGES ON codalab_bundles.* TO 'codalab'@'%' IDENTIFIED BY 'TESTPASSWORD'"
 
->&2 echo "Created user codalab"
-
-mysql -h mysql -u root -pROOTPWD -e "CREATE DATABASE codalab_bundles"
+mysql -h mysql -u root -pROOTPWD -e "CREATE DATABASE IF NOT EXISTS codalab_bundles"
 
 >&2 echo "Created database codalab_bundles"
+
+mysql -h mysql -u root -pROOTPWD -e "GRANT ALL PRIVILEGES ON codalab_bundles.* TO 'codalab' IDENTIFIED BY 'TESTPASSWORD'"
+
+>&2 echo "Created user codalab"
 
 . /opt/codalab-cli/venv/bin/activate
 pip install /opt/codalab-cli/worker
@@ -26,6 +27,11 @@ pip install /opt/codalab-cli
 python /opt/codalab-cli/scripts/create-root-user.py TESTPASSWORD
 
 >&2 echo "Created CodaLab root user codalab"
+
+until cl work http://rest-server:2900::; do
+  >&2 echo "Codalab server not available - waiting"
+  sleep 1
+done
 
 cl new home
 
