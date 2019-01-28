414ff79427a84a29abdcc4e9f0c49e91b0754305
Dennis Jeong <wonjeo@gmail.com>
2019-01-28 15:21:51 -0800

    adding some more worksheetItem components

--- frontend/src/components/worksheets/items/ImageItem.js
+++ frontend/src/components/worksheets/items/ImageItem.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Immutable from 'seamless-immutable';
+import { worksheetItemPropsChanged } from '../../../util/worksheet_utils';
 
 class ImageItem extends React.Component {
     /** Constructor. */
@@ -8,9 +9,32 @@ class ImageItem extends React.Component {
         this.state = Immutable({});
     }
 
-    /** Renderer. */
+    handleClick = (event) => {
+        this.props.setFocus(this.props.focusIndex, 0);
+    };
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return worksheetItemPropsChanged(this.props, nextProps);
+    }
+
     render() {
-        return <div>ImageItem</div>;
+        var className = 'type-image' + (this.props.focused ? ' focused' : '');
+        var src = 'data:image/png;base64,' + this.props.item.image_data;
+        var styles = {};
+        if (this.props.item.hasOwnProperty('height')) {
+            styles['height'] = this.props.item.height + 'px;';
+        }
+        if (this.props.item.hasOwnProperty('width')) {
+            styles['width'] = this.props.item.width + 'px;';
+        }
+
+        return (
+            <div className='ws-item' onClick={this.handleClick}>
+                <div className={className} ref={this.props.item.ref}>
+                    <img style={styles} src={src} />
+                </div>
+            </div>
+        );
     }
 }
 
--- frontend/src/components/worksheets/items/RecordItem.js
+++ frontend/src/components/worksheets/items/RecordItem.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Immutable from 'seamless-immutable';
+import { worksheetItemPropsChanged } from '../../../util/worksheet_utils';
 
 class RecordItem extends React.Component {
     /** Constructor. */
@@ -8,9 +9,49 @@ class RecordItem extends React.Component {
         this.state = Immutable({});
     }
 
-    /** Renderer. */
+    handleClick = (event) => {
+        this.props.setFocus(this.props.focusIndex, 0);
+    };
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return worksheetItemPropsChanged(this.props, nextProps);
+    }
+
     render() {
-        return <div>RecordItem</div>;
+        var item = this.props.item;
+        var className = 'table table-record' + (this.props.focused ? ' focused' : '');
+        var bundleInfo = this.props.item.bundles_spec.bundle_infos[0];
+        var header = item.header;
+        var k = header[0];
+        var v = header[1];
+        var items = item.rows.map(function(item, index) {
+            var ref = 'row' + index;
+            return (
+                <tr ref={ref} key={index}>
+                    <th>{item[k]}</th>
+                    <td>{item[v]}</td>
+                </tr>
+            );
+        });
+        return (
+            <div
+                className='ws-item'
+                onClick={this.handleClick}
+                onContextMenu={this.props.handleContextMenu.bind(
+                    null,
+                    bundleInfo.uuid,
+                    this.props.focusIndex,
+                    0,
+                    bundleInfo.bundle_type === 'run',
+                )}
+            >
+                <div className='type-record'>
+                    <table className={className}>
+                        <tbody>{items}</tbody>
+                    </table>
+                </div>
+            </div>
+        );
     }
 }
 
