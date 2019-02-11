fb7d0abce494a7fbe676654bc2f4294c91e2578a
Nikhil Bhattasali <nikhilxb@stanford.edu>
2019-02-11 13:25:27 -0800

    Undo bad rebase merge [kerem]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if int(setuptools.__version__.split('.')[0]) < 25:
 
 
 def get_requirements(*requirements_file_paths):
-    requirements = ['codalabworker>=0.2.41']
+    requirements = ['codalabworker>={}'.format(CODALAB_VERSION)]
     for requirements_file_path in requirements_file_paths:
         with open(requirements_file_path) as requirements_file:
             for line in requirements_file:
