0f2e32c09bbcb8c166ccbc63dc57f28fc4f94dc4
Codalab Worksheets Team <codalab.worksheets@gmail.com>
2019-02-23 15:47:16 -0800

    Make the docker service use the new frontend

--- docker/service/build-and-start.sh
+++ docker/service/build-and-start.sh
@@ -4,18 +4,18 @@
 
 usage()
 {
-  echo "Build docker images from current codebase and run a full Codalab service on docker. [[-t --test : runs integration tests against the service] [-w --webserver : also builds and runs docker containers for the webserver portion, assumes webserver repo can be found in the same directory as the CLI repo]]"
+  echo "Build docker images from current codebase and run a full Codalab service on docker. [[-t --test : runs integration tests against the service] [-f --frontend : also builds and runs docker containers for the frontend portion]]"
 }
 
 TEST=0
-WEBSERVER=0
+FRONTEND=0
 
 while [ "$1" != "" ]; do
     case $1 in
         -t | --test )           shift
                                 TEST=1
                                 ;;
-        -w | --webserver )      WEBSERVER=1
+        -w | --frontend )      FRONTEND=1
                                 ;;
         -h | --help )           usage
                                 exit
@@ -23,13 +23,10 @@ while [ "$1" != "" ]; do
     shift
 done
 
-PARENT_DIR=$(cd "$(dirname "$0")/../../.."; pwd)
-if [ -z "$CODALAB_DIR" ]; then CODALAB_DIR="$PARENT_DIR"; fi
-
 echo "==> Bringing down service"
 docker-compose down --remove-orphans
 
-cd $CODALAB_DIR/codalab-cli
+cd ../..
 echo "==> Building the bundleserver Docker image"
 docker build -t codalab/bundleserver:local-dev -f docker/Dockerfile.server .
 echo "==> Building the worker Docker image"
@@ -38,10 +35,9 @@ docker build -t codalab/worker:local-dev -f docker/Dockerfile.worker .
 COMPOSE_FILES="-f docker-compose.yml"
 COMPOSE_FLAGS="-d"
 
-if [ "$WEBSERVER" = "1" ]; then
-  cd $CODALAB_DIR/codalab-worksheets
-  docker build -t codalab/webserver:local-dev -f Dockerfile .
-  COMPOSE_FILES="$COMPOSE_FILES -f docker-compose.webserver.yml"
+if [ "$FRONTEND" = "1" ]; then
+  docker build -t codalab/frontend:local-dev -f docker/Dockerfile.frontend .
+  COMPOSE_FILES="$COMPOSE_FILES -f docker-compose.frontend.yml"
 fi
 
 if [ "$TEST" = "1" ]; then
@@ -49,6 +45,6 @@ if [ "$TEST" = "1" ]; then
   COMPOSE_FLAGS="--exit-code-from tests"
 fi
 
-cd $CODALAB_DIR/codalab-cli/docker/service
+cd docker/service
 echo "==> Bringing service up with 'docker-compose $COMPOSE_FILES up $COMPOSE_FLAGS'"
 docker-compose $COMPOSE_FILES up $COMPOSE_FLAGS
