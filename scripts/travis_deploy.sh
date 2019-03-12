1e6e9d48ce2bba26274a9d13f2f3a75c65f29252
Kerem Goksel <bkgoksel@gmail.com>
2019-03-12 13:56:16 -0700

    Automatically build and upload PyPi packages on Travis

---
+++ scripts/travis_deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# travis_deploy.sh
+# Called by Travis CI at the end of a successful build to do necessary
+# deployment actions like building and pushing docker images and PyPI
+# packages
+
+TAG=$1
+RELEASE=0
+if [ "$2" = "release" ]; then
+  RELEASE=1
+fi
+
+if [ "$RELEASE" = "1" ]; then
+  ./docker/build_images.sh $TAG -p
+  ./docker/build_images.sh latest -p
+  ./upload_to_pypi.sh $TAG
+else
+  ./docker/build_images.sh $TAG -p
+fi
