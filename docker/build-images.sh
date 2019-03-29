9e8c5516d931e6f109661ead067e0fa930ae2f2f
Kerem Goksel <bkgoksel@gmail.com>
2019-03-29 16:12:03 -0700

    Use previous docker images for cache on travis

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -20,15 +20,20 @@ else
 fi
 
 echo "==> Building the bundleserver Docker image"
-docker build -t codalab/bundleserver:$TAG -f docker/Dockerfile.server .
+docker pull codalab/bundleserver:$TAG
+docker build --cache-from codalab/bundleserver:$TAG -t codalab/bundleserver:$TAG -f docker/Dockerfile.server .
 echo "==> Building the frontend Docker image"
-docker build -t codalab/frontend:$TAG -f docker/Dockerfile.frontend .
+docker pull codalab/frontend:$TAG
+docker build --cache-from codalab/frontend:$TAG -t codalab/frontend:$TAG -f docker/Dockerfile.frontend .
 echo "==> Building the worker Docker image"
-docker build -t codalab/worker:$TAG -f docker/Dockerfile.worker .
+docker pull codalab/worker:$TAG
+docker build --cache-from codalab/worker:$TAG -t codalab/worker:$TAG -f docker/Dockerfile.worker .
 echo "==> Building the default-cpu Docker image"
-docker build -t codalab/default-cpu:$TAG -f docker/Dockerfile.cpu .
+docker pull codalab/default-cpu:$TAG
+docker build --cache-from codalab/default-cpu:$TAG -t codalab/default-cpu:$TAG -f docker/Dockerfile.cpu .
 echo "==> Building the default-gpu Docker image"
-docker build -t codalab/default-gpu:$TAG -f docker/Dockerfile.gpu .
+docker pull codalab/default-gpu:$TAG
+docker build --cache-from codalab/default-gpu:$TAG -t codalab/default-gpu:$TAG -f docker/Dockerfile.gpu .
 
 if [ "$PUSH" = "1" ]; then
   docker login -u $DOCKER_USER -p $DOCKER_PWD
