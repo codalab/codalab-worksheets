fa251502b4651b62fe62d03f6eb78436ed3d5a8d
Matt-F-Wu <wuhao20@stanford.edu>
2019-04-21 17:34:46 -0700

    Bundle Detail Design wip

---
+++ frontend/src/components/worksheets/BundleDetail/MainContent.jsx
@@ -0,0 +1,102 @@
+// @flow
+import * as React from 'react';
+import Typography from '@material-ui/core/Typography';
+import CopyIcon from '@material-ui/icons/FileCopy';
+import Tooltip from '@material-ui/core/Tooltip';
+import { withStyles } from '@material-ui/core';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+import { FileBrowser } from '../FileBrowser';
+
+class MainContent extends React.Component<
+    {
+      bundleInfo: {},
+      stdout: string | null,
+      strerr: string | null,
+      fileContents: string | null,
+      classes: {},
+    }
+> {  
+  
+  render() {
+    const {
+      classes, bundleInfo, stdout, stderr, fileContents } = this.props;
+    const bundleState = (bundleInfo.state == 'running' &&
+              bundleInfo.metadata.run_status != 'Running')
+          ? bundleInfo.metadata.run_status
+          : bundleInfo.state;
+
+    const bundleStateClass = `bundle-state state-${
+        bundleInfo.state || 'ready' }`;
+    // State of the bundle, IMPORTANT
+    
+    return (
+      <div>
+        <div className={ bundleStateClass }>{ bundleState }</div>
+        { bundleInfo.bundle_type === 'run' &&
+          <div className={ classes.section }>
+            <div
+              className={ `${ classes.row } ${ classes.command }` }
+            >
+              <CopyToClipboard
+                text={ bundleInfo.command }
+              >
+                <Tooltip title="Copy to clipboard">
+                  <CopyIcon
+                    style={ { color: 'white', marginLeft: 8 } }
+                  />
+                </Tooltip>
+              </CopyToClipboard>
+            </div>
+            <Typography variant="body1">
+              run time: { bundleInfo.metadata.time }
+            </Typography>
+          </div>
+        }
+        { stdout &&
+          <div className={ classes.snippet }>
+            <b>stdout</b>
+            { stdout }
+          </div>
+        }
+        { stderr &&
+          <div className={ classes.snippet }>
+            <b>stderr</b>
+            { stderr }
+          </div>
+        }
+        { fileContents
+          ? <div className={ classes.snippet }>
+            { fileContents }
+          </div>
+          : <FileBrowser uuid={ bundleInfo.uuid } />
+        }
+      </div>
+    );
+  }
+}
+
+const styles = (theme) => ({
+  section: {
+    marginTop: theme.spacing.large,
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  command: {
+    backgroundColor: '#333',
+    color: 'white',
+    fontFamily: 'monospace',
+  },
+  snippet: {
+    fontFamily: 'monospace',
+    backgroundColor: theme.color.grey.lightest,
+    height: 160,
+    marginTop: theme.spacing.large,
+  },
+});
+
+export default withStyles(styles)(MainContent);
---
+++ frontend/src/components/worksheets/BundleDetail/SideBar.jsx
@@ -0,0 +1,140 @@
+// @flow
+import * as React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core';
+
+import './PermissionDialog';
+import Editable from '../Editable';
+import { shorten_uuid } from '../util/worksheet_utils';
+
+const metadataFields = [
+  'name',
+  'description',
+  'created',
+  'data_size',
+];
+
+class Dependency extends React.PureComponent<
+  {
+    bundleInfo: {},
+    classes: {},
+  }
+> {
+
+  render() {
+    const { bundleInfo } = this.props;
+    let dependencies_table = [];
+    if (!bundleInfo.dependencies || bundleInfo.dependencies.length == 0) return <div />;
+
+    bundleInfo.dependencies.forEach((dep, i) => {
+        let dep_bundle_url = '/bundles/' + dep.parent_uuid;
+        dependencies_table.push(
+            <tr key={ dep.parent_uuid + i }>
+                <td>{ dep.child_path }</td>
+                <td>
+                    &rarr; { dep.parent_name }(
+                    <a href={ dep_bundle_url }>
+                      { shorten_uuid(dep.parent_uuid) }
+                    </a>)
+                    { dep.parent_path ? '/' + dep.parent_path : '' }
+                </td>
+            </tr>
+        );
+    });
+
+    return (
+        <div>
+            <h4>dependencies</h4>
+            <table className='bundle-meta table'>
+                <tbody>{ dependencies_table }</tbody>
+            </table>
+        </div>
+    );
+  }
+}
+
+// Main class definition.
+class SideBar extends React.Component<
+    {
+      bundleInfo: {},
+    }
+> {
+  
+  render() {
+    const { bundleInfo } = this.props;
+    const { metadata, editableMetadataFields } = bundleInfo;
+
+    const bundleDownloadUrl = '/rest/bundles/' + bundleInfo.uuid + '/contents/blob/';
+    const bundleStateClass = 'bundle-state state-' + (bundleInfo.state || 'ready');
+
+    return <div>
+      {/* Clickable UUID */}
+      <a
+        href={ `/bundles/${ bundleInfo.uuid }` }
+        className={ classes.uuidLink }>
+        { bundleInfo.uuid }
+      </a>
+      {/* General Metadata */}
+      {
+        metadata.failure_message && <Editable
+          label="Failure Message"
+          value={ metadata.failure_message }
+        />
+      }
+      {
+        metadataFields.map((field) => <Editable
+          key={ field }
+          label={ field }
+          value={ metadata[field] }
+          canEdit={ editableMetadataFields.includes(field) }
+        />)
+      }
+      <div className{ classes.section }>
+        <Dependency
+          bundleInfo={ bundleInfo }
+        />
+      </div>
+      <div
+        className={ classes.section }
+      >
+        <Typography variant="body1">
+          Owned by { bundleInfo.owner.user_name }, hosted within:
+        </Typography>
+        {
+          bundleInfo.host_worksheets.map((worksheet) => <div
+            key={ worksheet.uuid }
+          >
+            <a href={ `/worksheets/${ worksheet.uuid }` }>
+              { worksheet.name }
+            </a>
+          </div>)
+        }
+      </div>
+      <div className={ classes.section }>
+        <PermissionDialog
+          permission_spec={ bundleInfo.permission_spec }
+          group_permissions={ bundleInfo.group_permissions }
+        />
+      </div>
+    </div>;
+  }
+}
+
+const styles = (theme) => ({
+  section: {
+    marginTop: 8,
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  uuidLink: {
+    textDecoration: 'none',
+    '&:hover': {
+      opacity: 0.5,
+    }
+  },
+});
+
+export default withStyles(styles)(SideBar);
