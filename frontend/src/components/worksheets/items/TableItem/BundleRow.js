d04db941b61d248a8f0b22e27d3b19b73deae579
matt-f-wu <wuhao20@stanford.edu>
2019-05-11 21:10:13 -0700

    Final Table UI

--- frontend/src/components/worksheets/items/TableItem/BundleRow.js
+++ frontend/src/components/worksheets/items/TableItem/BundleRow.js
@@ -1,27 +1,21 @@
-import * as React from 'react';
-import { withStyles } from '@material-ui/core';
+import React, { Component } from 'react';
+import $ from 'jquery';
+import { withStyles } from '@material-ui/core/styles';
+import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
-import TableBody from '@material-ui/core/TableBody';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreIcon from '@material-ui/icons/MoreVert';
+
 import BundleDetail from '../../BundleDetail';
+import InsertButtons from './InsertButtons';
+import { buildTerminalCommand } from '../../../../util/worksheet_utils';
 
-class BundleRow extends React.Component<
-    {
-        onEnter: () => void,
-        onLeave: () => void,
-    }
+class BundleRow extends Component {
 
->{
-    static defaultProps = {
-        onEnter: () => {},
-        onLeave: () => {},
-    }
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            showDetail: false,
-        };
+    state = {
+        showInsertButtons: 0,
     }
 
     handleClick = () => {
@@ -30,11 +24,46 @@ class BundleRow extends React.Component<
         this.setState({
             showDetail: !showDetail,
         });
-    };
+    }
+
+    showButtons = (ev) => {
+        const row = ev.currentTarget;
+        const {
+            top,
+            height,
+        } = row.getBoundingClientRect();
+        const { clientY } = ev;
+        const onTop = (clientY >= top
+                && clientY <= top + 0.25 * height);
+        const onBotttom = (clientY >= top + 0.75 * height
+                && clientY <= top + height);
+        if (onTop) {
+            this.setState({
+                showInsertButtons: -1,
+            });
+        }
+        if (onBotttom) {
+            this.setState({
+                showInsertButtons: 1,
+            });
+        }
+    }
+
+    deleteItem = (ev) => {
+        ev.stopPropagation();
+        const { uuid } = this.props.bundleInfo;
+        $('#command_line')
+            .terminal()
+            .exec(buildTerminalCommand(['rm', uuid]));
+    }
+
+    showMore = (ev) => {
+        ev.stopPropagation();
+    }
 
     render() {
+        const { showInsertButtons, showDetail } = this.state;
         const { bundleInfo, classes, onMouseMove } = this.props;
-        const { showDetail } = this.state;
         var rowItems = this.props.item;
         var baseUrl = this.props.url;
         var uuid = this.props.uuid;
@@ -68,7 +97,7 @@ class BundleRow extends React.Component<
                 <TableCell
                     key={col}
                     classes={ {
-                        root: showDetail ? classes.noBottomBorder : classes.root
+                        root: classes.root
                     } }
                 >
                     {rowContent}
@@ -76,54 +105,138 @@ class BundleRow extends React.Component<
             );
         });
 
-        return (
-            <TableBody onMouseMove={ onMouseMove }>
-                <TableRow
-                    onClick={this.handleClick}
-                    onContextMenu={this.props.handleContextMenu.bind(
-                        null,
-                        bundleInfo.uuid,
-                        this.props.focusIndex,
-                        this.props.rowIndex,
-                        bundleInfo.bundle_type === 'run',
-                    )}
+        return <TableBody
+            classes={ { root: classes.tableBody } }
+            onMouseMove={ this.showButtons }
+            onMouseLeave={ () => {
+                this.setState({
+                    showInsertButtons: 0,
+                });
+            } }
+        >
+            <TableRow classes={ { root: classes.panelContainer } }>
+                <TableCell
+                    colSpan="100%"
+                    classes={ { root: classes.panelCellContainer } }
                 >
-                    {rowCells}
+                    {
+                        (showInsertButtons < 0) &&
+                        <InsertButtons />
+                    }
+                </TableCell>
+            </TableRow>
+            <TableRow
+                onClick={this.handleClick}
+                onContextMenu={this.props.handleContextMenu.bind(
+                    null,
+                    bundleInfo.uuid,
+                    this.props.focusIndex,
+                    this.props.rowIndex,
+                    bundleInfo.bundle_type === 'run',
+                )}
+            >
+                { rowCells }
+            </TableRow>
+            <TableRow classes={ { root: classes.panelContainer } }>
+                <TableCell
+                    colSpan="100%"
+                    classes={ { root: classes.panelCellContainer } }
+                >
+                    <div
+                        className={ classes.rightButtonStripe }
+                    >
+                        <IconButton
+                            onClick={ this.showMore }
+                        >
+                            <MoreIcon />
+                        </IconButton>
+                        &nbsp;&nbsp;
+                        <IconButton
+                            onClick={ this.deleteItem }
+                        >
+                            <DeleteIcon />
+                        </IconButton>
+                    </div>
+                </TableCell>
+            </TableRow>
+            {
+                showDetail &&
+                <TableRow>
+                    <TableCell colspan="100%" classes={ { root: classes.rootNoPad  } } >
+                        <BundleDetail
+                            uuid={ bundleInfo.uuid }
+                            bundleMetadataChanged={ this.props.reloadWorksheet }
+                            ref='bundleDetail'
+                            onClose={ () => {
+                                this.setState({
+                                    showDetail: false,
+                                });
+                            } }
+                        />
+                    </TableCell>
                 </TableRow>
-                {
-                    showDetail &&
-                    <TableRow>
-                        <TableCell colspan="100%" classes={ { root: classes.noTopBorder  } } >
-                            <BundleDetail
-                                uuid={ bundleInfo.uuid }
-                                bundleMetadataChanged={ this.props.reloadWorksheet }
-                                ref='bundleDetail'
-                                onClose={ () => {
-                                    this.setState({
-                                        showDetail: false,
-                                    });
-                                } }
-                            />
-                        </TableCell>
-                    </TableRow>
-                }
-            </TableBody>
-        );
+            }
+            <TableRow classes={ { root: classes.panelContainer } }>
+                <TableCell
+                    colSpan="100%"
+                    classes={ { root: classes.panelCellContainer } }
+                >
+                    {
+                        (showInsertButtons > 0) &&
+                        <InsertButtons />
+                    }
+                </TableCell>
+            </TableRow>
+        </TableBody>        
     }
 }
 
 const styles = (theme) => ({
+    tableBody: {
+        '&:hover $rightButtonStripe': {
+            display: 'flex',
+        },
+    },
+    panelContainer: {
+        display: 'block',
+        height: '0px !important',
+        padding: 0,
+        margin: 0,
+        border: 'none !important',
+        overflow: 'visible',
+    },
+    panelCellContainer: {
+        padding: 0,
+        padding: 0,
+        margin: 0,
+        border: 'none !important',
+        overflow: 'visible',
+    },
+    buttonsPanel: {
+        display: 'flex',
+        flexDirection: 'row',
+        position: 'absolute',
+        justifyContent: 'center',
+        width: '100%',
+        transform: 'translateY(-50%)',
+    },
+    rightButtonStripe: {
+        display: 'none',
+        flexDirection: 'row',
+        position: 'absolute',
+        justifyContent: 'center',
+        left: '100%',
+        transform: 'translateY(-100%) translateX(-100%)',
+    },
     root: {
         verticalAlign: 'middle !important',
+        border: 'none !important',
     },
-    noTopBorder: {
-        borderTop: 'none !important',
-        padding: 0,
-    },
-    noBottomBorder: {
+    rootNoPad: {
         verticalAlign: 'middle !important',
-        borderBottom: 'none',
-    },
+        border: 'none !important',
+        padding: '0px !important',
+    }
 });
 
 export default withStyles(styles)(BundleRow);
