62c24c9fe056ca29a54108913105af4646a93829
Dennis Jeong <wonjeo@gmail.com>
2019-05-22 10:23:07 -0700

    Implementing custom editable field (#1138)

--- frontend/src/components/EditableField.js
+++ frontend/src/components/EditableField.js
@@ -4,6 +4,9 @@ import * as $ from 'jquery';
 import classNames from 'classnames';
 import Editable from 'react-x-editable';
 import { renderFormat, serializeFormat } from '../util/worksheet_utils';
+import ReactDOM from 'react-dom';
+
+const KEYCODE_ESC = 27;
 
 function isAscii(str) {
     return /^[\x00-\x7F]*$/.test(str);
@@ -23,47 +26,102 @@ export class EditableField extends React.Component<{
         canEdit: true,
     };
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            editing: false,
+            value: this.props.value,
+            initValue: this.props.value,
+            isValid: true,
+        };
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        if (nextProps.value !== prevState.initValue) {
+            return { value: nextProps.value, initValue: nextProps.value };
+        } else return null;
+    }
+
+    onClick = () => {
+        this.setState({ editing: true });
+    };
+
+    onBlur = (event) => {
+        if (!this.state.isValid) {
+            event.preventDefault();
+            return false;
+        }
+
+        this.setState({ editing: false });
+        event.preventDefault();
+
+        $.ajax({
+            type: this.props.method,
+            url: this.props.url,
+            data: JSON.stringify(this.props.buildPayload(this.state.value)),
+            contentType: 'application/json; charset=UTF-8',
+            dataType: 'json',
+            cache: false,
+            context: this, // automatically bind `this` in all callbacks
+            xhr: function() {
+                // Hack for IE < 9 to use PATCH method
+                return window.XMLHttpRequest == null ||
+                    new window.XMLHttpRequest().addEventListener == null
+                    ? new window.ActiveXObject('Microsoft.XMLHTTP')
+                    : $.ajaxSettings.xhr();
+            },
+        })
+            .done(function(response) {
+                if (this.props.onChange) this.props.onChange();
+            })
+            .fail(function(response, status, err) {
+                // TODO: this doesn't stop the value from updating in the frontend
+                console.log('Invalid value entered: ', response.responseText);
+            });
+    };
+
+    handleKeyPress = (event) => {
+        if (event.keyCode === KEYCODE_ESC) {
+            this.setState({ editing: false, value: this.state.initValue });
+        }
+    };
+
+    handleChange = (event) => {
+        this.setState({ value: event.target.value, isValid: isAscii(event.target.value) });
+    };
+
     shouldComponentUpdate(nextProps, nextState) {
-        return (nextProps.value !== this.props.value || nextProps.canEdit !== this.props.canEdit);
+        return (
+            nextProps.value !== this.props.value ||
+            nextState.value !== this.state.value ||
+            nextProps.canEdit !== this.props.canEdit ||
+            this.state.editing !== nextState.editing
+        );
     }
 
     render() {
-        return (
-            <Editable
-                dataType='text'
-                mode='inline'
-                value={this.props.value}
-                disabled={!this.props.canEdit}
-                emptyValueText='<none>'
-                showButtons={false}
-                validate={(value) => (isAscii(value) ? null : 'Only ASCII characters allowed.')}
-                handleSubmit={({ value }) =>
-                    $.ajax({
-                        type: this.props.method,
-                        url: this.props.url,
-                        data: JSON.stringify(this.props.buildPayload(value)),
-                        contentType: 'application/json; charset=UTF-8',
-                        dataType: 'json',
-                        cache: false,
-                        context: this, // automatically bind `this` in all callbacks
-                        xhr: function() {
-                            // Hack for IE < 9 to use PATCH method
-                            return window.XMLHttpRequest == null ||
-                                new window.XMLHttpRequest().addEventListener == null
-                                ? new window.ActiveXObject('Microsoft.XMLHTTP')
-                                : $.ajaxSettings.xhr();
-                        },
-                    })
-                        .done(function(response) {
-                            if (this.props.onChange) this.props.onChange();
-                        })
-                        .fail(function(response, status, err) {
-                            // TODO: this doesn't stop the value from updating in the frontend
-                            console.log('Invalid value entered: ', response.responseText);
-                        })
-                }
-            />
-        );
+        if (!this.state.editing) {
+            return (
+                <a className='editable editable-click' onClick={this.onClick}>
+                    {this.state.value === '' ? '<none>' : this.state.value}
+                </a>
+            );
+        } else {
+            return (
+                <form onSubmit={this.onBlur}>
+                    <input
+                        autoFocus
+                        value={this.state.value}
+                        onBlur={this.onBlur}
+                        onChange={this.handleChange}
+                        onKeyDown={this.handleKeyPress}
+                    />
+                    {!this.state.isValid && (
+                        <div style={{ color: '#a94442' }}>Only ASCII characters allowed.</div>
+                    )}
+                </form>
+            );
+        }
     }
 }
 
