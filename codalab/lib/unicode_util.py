32db75443f5b835e5aa36687a54fcc161aeab4df
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-07-05 00:37:19 +0000

    fix: fix contains_unicode

--- codalab/lib/unicode_util.py
+++ codalab/lib/unicode_util.py
@@ -5,5 +5,6 @@ containing unicode characters.
 
 
 def contains_unicode(s):
+    # This returns false if everything in s is ASCII, and true otherwise.
     # TODO: use .isascii() for Python 3.7
-    return len(s) == len(s.encode())
+    return len(s) != len(s.encode())
