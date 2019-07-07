e46a0dececab59281c61a7fcfbcd914cca3c383b
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-07-07 14:18:55 +0000

    fix: load json string

--- worker/codalabworker/bundle_service_client.py
+++ worker/codalabworker/bundle_service_client.py
@@ -23,7 +23,7 @@ def wrap_exception(message):
             except RestClientException as e:
                 raise BundleServiceException(message + ': ' + str(e), e.client_error)
             except urllib.error.HTTPError as e:
-                client_error = json.load(e)
+                client_error = json.loads(e.read().decode())
 
                 if client_error['error'] == 'invalid_grant':
                     raise BundleAuthException(
