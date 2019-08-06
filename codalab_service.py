dacd1fca87aebcd266fa3780f13c811cb1374b70
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-08-06 01:43:25 +0000

    fix: black reformat

--- codalab_service.py
+++ codalab_service.py
@@ -590,9 +590,16 @@ class CodalabServiceManager(object):
                     stderr=subprocess.PIPE,
                 )
                 for stdout_line in popen.stdout:
-                    print("process: " + stdout_line.decode('utf-8').encode('ascii', errors='replace'), end="")
+                    print(
+                        "process: " + stdout_line.decode('utf-8').encode('ascii', errors='replace'),
+                        end="",
+                    )
             except subprocess.CalledProcessError as e:
-                print("CalledProcessError: {}, {}".format(str(e), e.output.decode('utf-8').encode('ascii', errors='replace')))
+                print(
+                    "CalledProcessError: {}, {}".format(
+                        str(e), e.output.decode('utf-8').encode('ascii', errors='replace')
+                    )
+                )
                 raise e
         print('')
 
