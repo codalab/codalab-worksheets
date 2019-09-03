481727137639c66bbe1181384863937709579c17
Percy Liang <percyliang@gmail.com>
2019-09-02 20:43:38 -0700

    don't throw exception if talking to old worker (which doesn't send free_disk_bytes)

--- codalab/rest/workers.py
+++ codalab/rest/workers.py
@@ -27,11 +27,11 @@ def checkin(worker_id):
     socket_id = local.worker_model.worker_checkin(
         request.user.user_id,
         worker_id,
-        request.json["tag"],
-        request.json["cpus"],
-        request.json["gpus"],
-        request.json["memory_bytes"],
-        request.json["free_disk_bytes"],
+        request.json.get("tag"),
+        request.json.get("cpus"),
+        request.json.get("gpus"),
+        request.json.get("memory_bytes"),
+        request.json.get("free_disk_bytes"),
         request.json["dependencies"],
     )
 
