fd9a9a27d9c75d6c21523b1aae7fdfc9ce427222
Percy Liang <percyliang@gmail.com>
2019-09-13 06:13:38 +0000

    cleanup pip install

--- pre-commit.sh
+++ pre-commit.sh
@@ -6,12 +6,9 @@ if ! [ -e venv ]; then
   virtualenv -p python3.6 venv || exit 1
   venv/bin/pip install -r requirements-server.txt || exit 1
   venv/bin/pip install -r requirements.docs.txt || exit 1
-  rm -rf worker/codalabworker.egg-info  # Need to clear because of different Python versions
-  # Install for generating docs.
-  venv/bin/pip install black==18.9b0 || exit 1
 fi
 
-venv/bin/pip install -e worker  # Not sure why this is necessary
+venv/bin/pip install -e worker  # Not sure why this is necessary (TODO: get rid of this when we remove the codalabworker package)
 venv/bin/pip install -e .
 
 # Generate docs
