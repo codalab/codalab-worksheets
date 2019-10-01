06a7d6069878e1894e594a70f548c037de082b21
Levi Lian <levi_lian007@163.com>
2019-10-01 00:27:45 -0700

    title name now defaults to untitled

--- frontend/src/components/worksheets/Worksheet.js
+++ frontend/src/components/worksheets/Worksheet.js
@@ -872,7 +872,7 @@ class Worksheet extends React.Component {
                                                             key={'title' + this.canEdit()}
                                                             canEdit={this.canEdit()}
                                                             fieldName='title'
-                                                            value={info && info.title}
+                                                            value={info && info.title || "untitled"}
                                                             uuid={info && info.uuid}
                                                             onChange={() => this.reloadWorksheet()}
                                                         />
