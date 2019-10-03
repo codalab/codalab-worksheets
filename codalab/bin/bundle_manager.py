78371fbd90f22e4e86239d663c22d7103dbbcd2c
Jing Ge <jingge2@illinois.edu>
2019-10-02 20:11:12 -0700

    create cl-server and cl-bundle-manager entry_points (#1445)
    Closed via #1380

---
+++ codalab/bin/bundle_manager.py
@@ -0,0 +1,27 @@
+# Main entry point for CodaLab cl-bundle-manager.
+import signal
+import argparse
+from codalab.lib.codalab_manager import CodaLabManager
+from codalab.server.bundle_manager import BundleManager
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--sleep-time',
+        help='Number of seconds to wait between successive actions.',
+        type=int,
+        default=0.5,
+    )
+    args = parser.parse_args()
+
+    manager = BundleManager.create(CodaLabManager())
+    # Register a signal handler to ensure safe shutdown.
+    for sig in [signal.SIGTERM, signal.SIGINT, signal.SIGHUP]:
+        signal.signal(sig, lambda signup, frame: manager.signal())
+
+    manager.run(args.sleep_time)
+
+
+if __name__ == '__main__':
+    main()
