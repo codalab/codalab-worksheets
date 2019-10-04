032fab70ce72c3f5d83e49848817273efbf55cf1
Kerem Goksel <bkgoksel@microsoft.com>
2019-10-04 14:59:05 -0700

    Format

--- codalab/model/bundle_model.py
+++ codalab/model/bundle_model.py
@@ -754,7 +754,7 @@ class BundleModel(object):
         bundle_update = {
             'state': worker_bundle_update['state'],
             'metadata': {
-                'run_status': worker_bundle_update['run_status'],         
+                'run_status': worker_bundle_update['run_status'],
                 'time': worker_bundle_update['container_time_total'],
                 'time_user': worker_bundle_update['container_time_user'],
                 'time_system': worker_bundle_update['container_time_system'],
