215d37a9d6e52ac59665bb69129e1923d76209c4
Kerem Goksel <bkgoksel@microsoft.com>
2019-10-14 18:10:58 -0700

    Add the bundle state objects file

--- codalab/worker/bundle_state.py
+++ codalab/worker/bundle_state.py
@@ -33,6 +33,124 @@ class State(object):
     FINAL_STATES = {READY, FAILED, KILLED}
 
 
+class DependencyKey(object):
+    """
+    Defines the uniquely identifying properties of a dependency that can be used as a key
+    for caching dependencies
+    """
+
+    def __init__(self, parent_uuid, parent_path):
+        self.parent_uuid = parent_uuid
+        self.parent_path = parent_path
+
+    def __eq__(self, other):
+        return (
+            self.parent_uuid == other.parent_uuid
+            and self.parent_path == other.parent_path
+        )
+
+    def __hash__(self):
+        return hash((self.parent_uuid, self.parent_path))
+
+    def to_dict(self):
+        return generic_to_dict(self)
+
+
+class Dependency(object):
+    """
+    Defines a RunBundle dependency passed from server to worker.
+    Refer to codalab/lib/bundle_util.py#bundle_to_bundle_info() for how the
+    dict to construct this object is created on server side.
+    """
+
+    def __init__(
+        self,
+        parent_name,
+        parent_path,
+        parent_uuid,
+        child_path,
+        child_uuid,
+        location=None,
+    ):
+        self.parent_name = parent_name
+        self.parent_path = parent_path
+        self.parent_uuid = parent_uuid
+        self.child_path = child_path
+        self.child_uuid = child_uuid
+        self.location = location  # Set if local filesystem dependency
+
+    def to_dict(self):
+        return generic_to_dict(self)
+
+
+class BundleInfo(object):
+    """
+    Defines the bundle info passed to the worker by the server.
+    Refer to codalab/lib/bundle_util.py#bundle_to_bundle_info() for how the
+    dict to construct this object is created on server side.
+    """
+
+    def __init__(
+        self,
+        uuid,
+        bundle_type,
+        owner_id,
+        command,
+        data_hash,
+        state,
+        is_anonymous,
+        metadata,
+        dependencies,  # List[str, Dict[str, str]]
+        args,
+        location=None,
+    ):
+        self.uuid = uuid  # type: str
+        self.bundle_type = bundle_type  # type: str
+        self.owner_id = owner_id  # type: str
+        self.command = command  # type: str
+        self.data_hash = data_hash  # type: str
+        self.state = state  # type: State
+        self.is_anonymous = is_anonymous  # type: bool
+        self.metadata = metadata  # type: Dict[Any, Any]
+        self.args = args  # type: Any
+        self.dependencies = {
+            DependencyKey(dep["parent_uuid"], dep["parent_path"]): Dependency(
+                parent_name=dep["parent_name"],
+                parent_path=dep["parent_path"],
+                parent_uuid=dep["parent_uuid"],
+                child_path=dep["child_path"],
+                child_uuid=dep["child_uuid"],
+                location=dep.get("location", None),
+            )
+            for dep in dependencies
+        }  # type: Dict[DependencyKey, Dependency]
+        self.location = location  # type: Optional[str] set if local filesystem
+
+    def to_dict(self):
+        dct = generic_to_dict(self)
+        dct['dependencies'] = [v for k,v  in dct['dependencies'].items()]
+        return dct
+
+    def __str__(self):
+        return str(self.to_dict())
+
+    @classmethod
+    def from_dict(cls, dct):
+        return cls(
+            uuid=dct["uuid"],
+            bundle_type=dct["bundle_type"],
+            owner_id=dct["owner_id"],
+            command=dct["command"],
+            data_hash=dct["data_hash"],
+            state=dct["state"],
+            is_anonymous=dct["is_anonymous"],
+            metadata=dct["metadata"],
+            dependencies=dct["dependencies"],
+            args=dct["args"],
+            location=dct.get("location", None),
+        )
+
+
 class RunResources(object):
     """
     Defines all the resource fields the server propagates to the worker for its runs
@@ -62,6 +180,41 @@ class RunResources(object):
             network=bool(dct["network"]),
         )
 
+
+class WorkerRun(object):
+    """
+    Defines all the field the worker needs to check in with the server for its runs
+    """
+
+    def __init__(self, run_status, bundle_start_time, container_start_time, container_time_total, container_time_user, container_time_system, docker_image, info, state, remote):
+        self.run_status = run_status
+        self.bundle_start_time = bundle_start_time
+        self.container_start_time = container_start_time
+        self.container_time_total = container_time_total
+        self.container_time_user = container_time_user
+        self.container_time_system = container_time_system
+        self.docker_image = docker_image
+        self.info = info
+        self.state = state
+        self.remote = remote
+
+    @classmethod
+    def from_dict(cls, dct):
+        return cls(
+            run_status=dct['run_status'],
+            bundle_start_time=dct['bundle_start_time'],
+            container_start_time=dct['container_start_time'],
+            container_time_total=dct['container_time_total'],
+            container_time_user=dct['container_time_user'],
+            container_time_system=dct['container_time_system'],
+            docker_image=dct['docker_image'],
+            info=dct['info'],
+            state=dct['state'],
+            remote=dct['remote'],
+        )
+
+    def to_dict(self):
+        return generic_to_dict(self)
 
 def generic_to_dict(obj):
     dct = {}
