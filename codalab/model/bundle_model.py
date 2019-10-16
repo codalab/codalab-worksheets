127d6273e78e7a358d18525bdbd8979f7577acae
Kerem Goksel <bkgoksel@microsoft.com>
2019-10-16 13:29:10 -0700

    Format

--- codalab/model/bundle_model.py
+++ codalab/model/bundle_model.py
@@ -869,12 +869,7 @@ class BundleModel(object):
                 self.transition_bundle_running(
                     bundle, worker_run, row, user_id, worker_id, connection
                 )
-                return self.transition_bundle_finalizing(
-                    bundle,
-                    user_id,
-                    worker_run,
-                    connection,
-                )
+                return self.transition_bundle_finalizing(bundle, user_id, worker_run, connection)
 
             if state in [State.PREPARING, State.RUNNING]:
                 return self.transition_bundle_running(
