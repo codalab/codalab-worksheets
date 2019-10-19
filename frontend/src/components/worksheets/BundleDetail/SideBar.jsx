019437e628636f5308aaae0da3fba5d3f8d00a13
yipenghe <yipenghe@stanford.edu>
2019-10-19 16:04:37 -0700

    draft new bundle detail

--- frontend/src/components/worksheets/BundleDetail/SideBar.jsx
+++ frontend/src/components/worksheets/BundleDetail/SideBar.jsx
@@ -7,11 +7,16 @@ import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
-
+import CopyIcon from '@material-ui/icons/FileCopy';
+import Tooltip from '@material-ui/core/Tooltip';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { BundleEditableField } from '../../EditableField';
 import PermissionDialog from '../PermissionDialog';
 import { renderFormat, shorten_uuid } from '../../../util/worksheet_utils';
 import { ConfigLabel } from '../ConfigPanel';
+import { renderPermissions } from '../../../util/worksheet_utils';
+import Popover from '@material-ui/core/Popover';
+
 
 class Dependency extends React.PureComponent<
     {
@@ -21,7 +26,7 @@ class Dependency extends React.PureComponent<
 > {
 
   render() {
-    const { bundleInfo } = this.props;
+    const { classes, bundleInfo } = this.props;
     let dependencies_table = [];
     if (!bundleInfo.dependencies || bundleInfo.dependencies.length == 0) return <div />;
 
@@ -59,22 +64,25 @@ class Dependency extends React.PureComponent<
 /**
  * Content to display in sidebar of a Bundle Detail expansion panel.
  */
-class SideBar extends React.Component<
-    {
-        classes: {},
-        bundleInfo: {},
-        onUpdate: () => any,
+class SideBar extends React.Component{
+    state= {
+        showPermisson: false,
     }
-> {
   
     render() {
         const { bundleInfo, classes, onUpdate } = this.props;
         const { metadata, editableMetadataFields=[], metadataType } = bundleInfo;
         const hasEditPermission = bundleInfo.permission > 1;
-
+        const bundleState = (bundleInfo.state == 'running' &&
+							bundleInfo.metadata.run_status != 'Running')
+					? bundleInfo.metadata.run_status
+					: bundleInfo.state;
         const bundleDownloadUrl = '/rest/bundles/' + bundleInfo.uuid + '/contents/blob/';
         const bundleStateClass = 'bundle-state state-' + (bundleInfo.state || 'ready');
         const isRunBundle = bundleInfo.bundle_type === 'run';
+        const stateSpecClass = bundleInfo.state === 'failed'
+            ? 'failedState'
+            : (bundleInfo.state === 'ready' ? 'readyState' : 'otherState');
 
         return (
             <div>
@@ -91,56 +99,113 @@ class SideBar extends React.Component<
                 />
                 {/** ----------------------------------------------------------------------------------------------- */}
                 <ConfigLabel label="Description" />
-                <BundleEditableField
-                    dataType={ metadataType.description }
-                    fieldName="description"
-                    uuid={ bundleInfo.uuid }
-                    value={ metadata.description }
-                    canEdit={ hasEditPermission && editableMetadataFields.includes("description") }
-                    onChange={ (description) => onUpdate({ description }) }
-                />
-                {/** ----------------------------------------------------------------------------------------------- */}
-                { isRunBundle && <Dependency bundleInfo={ bundleInfo }/> }
-                { metadata.data_size && <ConfigLabel label="Size" /> }
-                { metadata.data_size && renderFormat(metadata.data_size, metadataType.data_size) }
-                <ConfigLabel label="Created On" />
-                {renderFormat(metadata.created, metadataType.created)}
-                <ConfigLabel label="Owner" />
-                { bundleInfo.owner.user_name }
-                <ConfigLabel label="Attached to Worksheets" />
-                {
-                    bundleInfo.host_worksheets.map((worksheet) =>
-                        <div
-                            key={ worksheet.uuid }
-                        >
-                            <a
-                                href={ `/worksheets/${ worksheet.uuid }`}
-                                className={ classes.uuidLink }
-                                target="_blank"
-                            >
-                                { worksheet.name }
-                            </a>
-                        </div>
-                    )
-                }
-                {/** ----------------------------------------------------------------------------------------------- */}
-                <div className={classes.spacer}/>
-                <div>
-                    <Typography variant="subtitle1">Permissions</Typography>
-                    <PermissionDialog
+                <div style={{ maxWidth: 100, flexWrap: 'wrap', flexShrink: 1}}>
+                    <BundleEditableField
+                        dataType={ metadataType.description }
+                        fieldName="description"
                         uuid={ bundleInfo.uuid }
-                        permission_spec={ bundleInfo.permission_spec }
-                        group_permissions={ bundleInfo.group_permissions }
-                        onChange={ this.props.onMetaDataChange }
+                        value={ metadata.description }
+                        canEdit={ hasEditPermission && editableMetadataFields.includes("description") }
+                        onChange={ (description) => onUpdate({ description }) }
                     />
                 </div>
+                <div>
+                    <ConfigLabel label="Status" inline={true}/>
+                    <div className={ `${ classes.stateBox } ${ classes[stateSpecClass] }`} style={{ display: 'inline' }}>
+                        { bundleState }
+                    </div>
+                </div>
+                <ConfigLabel label="Command" />
+                { isRunBundle &&
+                        <Grid item xs={12}>  
+                            <CopyToClipboard
+                                text={ bundleInfo.command }
+                            >
+                                <div
+                                    className={ `${ classes.row } ${ classes.command }` }
+                                >
+                                    <span>{ bundleInfo.command }</span>
+                                    <Tooltip title="Copy to clipboard">
+                                        <CopyIcon
+                                            style={ { color: 'white', marginLeft: 8 } }
+                                        />
+                                    </Tooltip>
+                                </div>
+                            </CopyToClipboard>
+                        </Grid>
+                    }
+                
+                {/** ----------------------------------------------------------------------------------------------- */}
+                <div>
+                    <ConfigLabel label="Owner:" inline={true}/>
+                    <div className={classes.dataText}>
+                        { bundleInfo.owner.user_name }
+                    </div>
+                </div>
+                <div>
+                    <ConfigLabel label="Created:" inline={true}/>
+                    <div className={classes.dataText}>
+                        {renderFormat(metadata.created, metadataType.created)}
+                    </div>
+                </div>
+                {metadata.data_size 
+                    ?   <div>
+                            <ConfigLabel label="Size:" inline={true}/>
+                            <div className={classes.dataText}>                    
+                                {renderFormat(metadata.data_size, metadataType.data_size) }
+                            </div>
+                        </div>
+                    :null
+                }
+                { isRunBundle && <Dependency bundleInfo={ bundleInfo }/> }
+                <div>
+                    <ConfigLabel label="Permissions:" inline={true}/>
+                    <div
+                        onClick={() => { this.setState({ showPermisson: !this.state.showPermisson}) }}
+                        className={classes.permissions}
+                    >
+                        {renderPermissions(bundleInfo)}
+                    </div>
+                    {this.state.showPermisson
+                    ?   <div>
+                            <PermissionDialog
+                                uuid={bundleInfo.uuid}
+                                permission_spec={bundleInfo.permission_spec}
+                                group_permissions={bundleInfo.group_permissions}
+                                onChange={this.props.onMetaDataChange}
+                                wperm
+                            />
+                        </div>
+                    :   null
+                    }
+                </div>
+                <ConfigLabel label="Attached to Worksheets" />
+                {
+                    bundleInfo.host_worksheets.length > 0
+                    ?   bundleInfo.host_worksheets.map((worksheet) =>
+                            <div
+                                key={ worksheet.uuid }
+                            >
+                                <a
+                                    href={ `/worksheets/${ worksheet.uuid }`}
+                                    className={ classes.uuidLink }
+                                    target="_blank"
+                                >
+                                    { worksheet.name }
+                                </a>
+                            </div>
+                        )
+                    : <div>None</div>
+                }
+                {/** ----------------------------------------------------------------------------------------------- */}
+                
                 <div>
                     <a
                         href={ `/bundles/${ bundleInfo.uuid }` }
                         className={ classes.uuidLink }
                         target="_blank"
                     >
-                        More Information for Bundle { shorten_uuid(bundleInfo.uuid) }
+                        More details
                     </a>
                 </div>
             </div>
@@ -165,6 +230,46 @@ const styles = (theme) => ({
     },
     spacer: {
         marginTop: theme.spacing.larger,
+    },
+    stateBox: {
+        color: 'white',
+        fontSize: '14px',
+        width: `50px`,
+        textAlign: 'center',
+        paddingLeft: 3,
+        paddingRight: 3,
+        marginLeft: 3,
+        border: '1px solid',
+        borderRadius: '5px!important'
+    },
+    readyState: {
+        backgroundColor: theme.color.green.base,
+    },
+    failedState: {
+        backgroundColor: theme.color.red.base,
+    },
+    otherState: {
+        backgroundColor: theme.color.yellow.base,
+    },
+    command: {
+        flex: 1,
+		backgroundColor: '#333',
+		color: 'white',
+		fontFamily: 'monospace',
+		padding: theme.spacing.large,
+        borderRadius: theme.spacing.unit,
+    },
+    permissions: {
+        cursor: 'pointer',
+        '&:hover': {
+            backgroundColor: theme.color.primary,
+        },
+    },
+    dataText: {
+        display:'inline', 
+        fontSize: 14, 
+        verticalAlign: 'middle', 
+        paddingLeft: 2,
     },
 });
 
