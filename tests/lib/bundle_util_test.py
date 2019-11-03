457fd59ae508ef485124989da497b46197865968
tony.lee <tony.lee@workday.com>
2019-11-03 10:14:21 -0800

    TAKEHOME: fixed checkstyles again

--- tests/lib/bundle_util_test.py
+++ tests/lib/bundle_util_test.py
@@ -11,20 +11,16 @@ class BundleUtilTest(unittest.TestCase):
                 return {
                     'uuid': '0x12345',
                     'metadata': {'name': 'bundle1'},
-                    'dependencies': [{'parent_uuid': '0x23456'}]
+                    'dependencies': [{'parent_uuid': '0x23456'}],
                 }
             elif args[1] == '0x23456':
                 return {
                     'uuid': '0x23456',
                     'metadata': {'name': 'bundle2'},
-                    'dependencies': [{'parent_uuid': '0x34567'}]
+                    'dependencies': [{'parent_uuid': '0x34567'}],
                 }
             elif args[1] == '0x34567':
-                return {
-                    'uuid': '0x34567',
-                    'metadata': {'name': 'bundle3'},
-                    'dependencies': []
-                }
+                return {'uuid': '0x34567', 'metadata': {'name': 'bundle3'}, 'dependencies': []}
 
         mock_client = Mock()
         mock_client.fetch.side_effect = side_effect
