ceb9dea93dc81623ff7e088ff593d9844f5b453d
yipenghe <yipenghe@stanford.edu>
2019-11-11 22:51:50 -0800

    fix rerun, newline for stdout & stderr in bundle detail

--- frontend/src/components/worksheets/BundleDetail/MainContent.jsx
+++ frontend/src/components/worksheets/BundleDetail/MainContent.jsx
@@ -39,12 +39,6 @@ class MainContent extends React.Component<
 	render() {
 		const {
             classes, bundleInfo, stdout, stderr, fileContents } = this.props;
-        const isRunBundle = bundleInfo.bundle_type === 'run';
-
-        //Get the correct run time display
-        const bundleRunTime = bundleInfo.metadata.time
-            ? renderDuration(bundleInfo.metadata.time)
-            : "-- --";
 
 		return (
             <div className={ classes.outter }>
@@ -69,7 +63,7 @@ class MainContent extends React.Component<
                                 {this.state.showStdOut &&
                                     <Grid item xs={12}>
                                         <div className={ classNames({[classes.snippet]:true, [classes.greyBackground]:true })}>
-                                            { stdout }
+                                            {stdout}
                                         </div>
                                     </Grid>}
                             </Grid>
@@ -148,6 +142,7 @@ const styles = (theme) => ({
         flexWrap: 'wrap', 
         flexShrink: 1,
         overflow:'auto',
+        whiteSpace: 'pre-line',
     },
     greyBackground: {
         backgroundColor: theme.color.grey.lightest,
