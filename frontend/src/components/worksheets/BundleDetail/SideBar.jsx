03c337c2bae9233e495fe162ac6789cb91277574
yipenghe <yipenghe@stanford.edu>
2019-11-13 19:36:16 -0800

    Fix several small issues (#1636)
    * fix bundle permissions
    
    * fix rerun
    
    * newline for stdout & stderr in bundle detail
    
    * disable r, u, t shortcuts when bundles are selected
    
    * scope keyboard events for bulk bundle operations, rename status->state in bundle detail

--- frontend/src/components/worksheets/BundleDetail/SideBar.jsx
+++ frontend/src/components/worksheets/BundleDetail/SideBar.jsx
@@ -112,7 +112,7 @@ class SideBar extends React.Component<
                 </div>
                 {/** ----------------------------------------------------------------------------------------------- */}
                 <div>
-                    <ConfigLabel label="Status" inline={true}/>
+                    <ConfigLabel label="State" inline={true}/>
                     <span className={ `${ classes.stateBox } ${ classes[stateSpecClass] }`} style={{ display: 'inline' }}>
                         <Typography inline color='inherit'>{bundleState}</Typography>
                     </span>
@@ -220,7 +220,7 @@ class SideBar extends React.Component<
                                     permission_spec={bundleInfo.permission_spec}
                                     group_permissions={bundleInfo.group_permissions}
                                     onChange={this.props.onMetaDataChange}
-                                    wperm
+                                    perm
                                 />
                             </div>
                         :   null
