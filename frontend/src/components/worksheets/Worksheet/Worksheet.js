ce2419d9511c013ffc7fe655598395d1bd9f1218
yipenghe <yipenghe@stanford.edu>
2019-11-26 11:23:42 -0800

    Update shortcut help (#1718)
    * keyboard shortcuts help update

--- frontend/src/components/worksheets/Worksheet/Worksheet.js
+++ frontend/src/components/worksheets/Worksheet/Worksheet.js
@@ -609,7 +609,7 @@ class Worksheet extends React.Component {
 
             // Toggle edit mode
             Mousetrap.bind(
-                ['e'],
+                ['shift+e'],
                 function(e) {
                     this.toggleEditMode();
                     return false;
@@ -667,7 +667,7 @@ class Worksheet extends React.Component {
             if (!this.state.showBundleOperationButtons) {
                 // insert text after current cell
                 Mousetrap.bind(
-                    ['t'],
+                    ['a+t'],
                     function(e) {
                         // if no active focus, scroll to the bottom position
                         if (this.state.focusIndex < 0) {
@@ -680,7 +680,7 @@ class Worksheet extends React.Component {
 
                 // upload after current cell
                 Mousetrap.bind(
-                    ['u'],
+                    ['a+u'],
                     function(e) {
                         // if no active focus, scroll to the bottom position
                         if (this.state.focusIndex < 0) {
@@ -692,7 +692,7 @@ class Worksheet extends React.Component {
                 );
                 // run after current cell
                 Mousetrap.bind(
-                    ['a r'],
+                    ['a+r'],
                     function(e) {
                         // if no active focus, scroll to the bottom position
                         if (this.state.focusIndex < 0) {
@@ -723,13 +723,13 @@ class Worksheet extends React.Component {
                 }
                 this.togglePopupNoEvent('rm');
             });
-            Mousetrap.bind(['d'], () => {
+            Mousetrap.bind(['a d'], () => {
                 if (this.state.openDelete || this.state.openKill) {
                     return;
                 }
                 this.togglePopupNoEvent('detach');
             });
-            Mousetrap.bind(['v'], () => {
+            Mousetrap.bind(['a k'], () => {
                 if (this.state.openDetach || this.state.openDelete) {
                     return;
                 }
