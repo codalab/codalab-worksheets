e8013227acf0997ed80a6d15ce95c0dba970be29
Tony Lee <tonyh.lee@yahoo.com>
2019-12-10 01:21:43 -0800

    1347: fix MB to bytes conversion

--- stress_test.py
+++ stress_test.py
@@ -61,7 +61,7 @@ class TestFile:
 
     def _make_random_file(self):
         with open(self._file_name, 'wb') as file:
-            file.seek(self._size_mb * 1024)  # seek takes in file size in terms of bytes
+            file.seek(self._size_mb * 1024000)  # seek takes in file size in terms of bytes
             file.write(b'0')
         print('Created file {} of size {} MB.'.format(self._file_name, self._size_mb))
 
