3e85cf421bd617e7ebc7e3bd8732cf60db97aff4
yipenghe <yipenghe@stanford.edu>
2019-12-21 03:21:59 -0800

    inherit configs of the run

--- frontend/src/components/worksheets/BundleDetail/BundleActions.jsx
+++ frontend/src/components/worksheets/BundleDetail/BundleActions.jsx
@@ -27,6 +27,20 @@ class BundleActions extends React.Component<
 			});
 		});
 		run.dependencies = dependencies;
+		run.disk = bundleInfo.metadata.request_disk;
+		run.cpu = bundleInfo.metadata.request_cpus;
+		run.gpu = bundleInfo.metadata.request_gpus;
+		run.memory = bundleInfo.metadata.request_memory;
+		let docker_image = bundleInfo.metadata.docker_image;
+		if (docker_image && docker_image.startsWith('codalab/default-cpu')){
+			docker_image = 'codalab/default-cpu';
+		}
+		else if (docker_image && docker_image.startsWith('codalab/default-gpu')){
+			docker_image = 'codalab/default-gpu';
+		}
+		run.docker = docker_image;
+		run.networkAccess = bundleInfo.metadata.request_network;
+		run.failedDependencies = bundleInfo.metadata.allow_failed_dependencies;
 		this.props.rerunItem(run);
 	}
 
@@ -46,6 +60,7 @@ class BundleActions extends React.Component<
 
 	render() {
 		const { bundleInfo, editPermission } = this.props;
+		console.log(bundleInfo)
 		const bundleDownloadUrl = '/rest/bundles/' + bundleInfo.uuid + '/contents/blob/';
 		const isRunBundle = bundleInfo.bundle_type === 'run' && bundleInfo.metadata;
 		const isKillableBundle = (bundleInfo.state === 'running' 
