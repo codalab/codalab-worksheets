adab9fbd7b2eee5d779d10bfcf7071e98d31c0d4
yipenghe <yipenghe@stanford.edu>
2020-02-09 10:04:50 -0800

    remove unused style, update image bundle

--- frontend/src/components/worksheets/items/ImageItem.js
+++ frontend/src/components/worksheets/items/ImageItem.js
@@ -10,8 +10,16 @@ class ImageItem extends React.Component {
         super(props);
         this.state = {
             showDetail: false,
+            bundleInfoUpdates: {},
         };
     }
+
+    receiveBundleInfoUpdates = (update) => {
+        let { bundleInfoUpdates } = this.state;
+        // Use object spread to update.
+        bundleInfoUpdates = { ...bundleInfoUpdates, ...update };
+        this.setState({ bundleInfoUpdates });
+    };
 
     handleClick = () => {
         this.props.setFocus(this.props.focusIndex, 0);
@@ -47,14 +55,16 @@ class ImageItem extends React.Component {
             styles['width'] = this.props.item.width + 'px';
         }
         return (
-            <div className='ws-item' onClick={this.handleClick}>
-                <div className={className} ref={this.props.item.ref}>
+            <div className='ws-item'>
+                <div className={className} ref={this.props.item.ref} onClick={this.handleClick}>
                     <img style={styles} src={src} />
                 </div>
                 {this.state.showDetail &&
                     <BundleDetail
                         uuid={bundleInfo.uuid}
                         ref='bundleDetail'
+                        bundleMetadataChanged={this.props.reloadWorksheet}
+                        onUpdate={this.receiveBundleInfoUpdates}
                         onClose={() => {
                             this.setState({
                                 showDetail: false,
@@ -71,55 +81,4 @@ class ImageItem extends React.Component {
     }
 }
 
-const styles = (theme) => ({
-    tableBody: {
-        '&:hover $rightButtonStripe': {
-            display: 'flex',
-        },
-    },
-    rightButtonStripe: {
-        display: 'none',
-        flexDirection: 'row',
-        position: 'absolute',
-        justifyContent: 'center',
-        left: '100%',
-        transform: 'translateY(-100%) translateX(-100%)',
-    },
-    rootNoPad: {
-        verticalAlign: 'middle !important',
-        border: 'none !important',
-        padding: '0px !important',
-        wordWrap: 'break-word',
-        maxWidth: 200,
-        minWidth: 100,
-    },
-    bundleDetail: {
-        paddingLeft: `${theme.spacing.largest}px !important`,
-        paddingRight: `${theme.spacing.largest}px !important`,
-    },
-    contentRow: {
-        height: 26,
-        borderBottom: '2px solid #ddd',
-        borderLeft: '3px solid transparent',
-        padding: 0,
-        '&:hover': {
-            boxShadow:
-                'inset 1px 0 0 #dadce0, inset -1px 0 0 #dadce0, 0 1px 2px 0 rgba(60,64,67,.3), 0 1px 3px 1px rgba(60,64,67,.15)',
-            zIndex: 1,
-        },
-    },
-    checkBox: {
-        '&:hover': {
-            backgroundColor: '#ddd',
-        },
-    },
-    highlight: {
-        backgroundColor: `${theme.color.primary.lightest} !important`,
-        borderLeft: '3px solid #1d91c0',
-    },
-    lowlight: {
-        backgroundColor: `${theme.color.grey.light} !important`,
-    },
-});
-
-export default withStyles(styles)(ImageItem);
+export default ImageItem;
