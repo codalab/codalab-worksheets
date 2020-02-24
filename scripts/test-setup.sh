3f1f5686f04adf820057c1d204d8ee44ab2ecc7a
Tony Lee <tonyh.lee@yahoo.com>
2020-02-24 09:26:51 -0800

    debugging

--- scripts/test-setup.sh
+++ scripts/test-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # For testing CodaLab in Chrome
+echo "Installing ChromeDriver and placing it on PATH..."
 wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
@@ -8,6 +9,7 @@ sudo chown root:root /usr/bin/chromedriver
 sudo chmod +x /usr/bin/chromedriver
 
 # For testing CodaLab in Firefox
+echo "Installing GeckoDriver and placing it on PATH..."
 wget https://github.com/mozilla/geckodriver/releases/download/v0.21.0/geckodriver-v0.21.0-linux64.tar.gz
 mkdir geckodriver
 tar -xzf geckodriver-v0.21.0-linux64.tar.gz -C geckodriver
