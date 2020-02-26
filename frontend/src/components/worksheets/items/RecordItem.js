e3b205251b6f1688a68b99a3a65bedcfc17ba181
yipenghe <yipenghe@stanford.edu>
2020-02-26 13:32:46 -0800

    Click/'enter' to see record, graph mode bundle detail (#2008)
    * display record graph model bundle detail by click/"enter"
    Co-authored-by: Ashwin Ramaswami <aramaswamis@gmail.com>

--- frontend/src/components/worksheets/items/RecordItem.js
+++ frontend/src/components/worksheets/items/RecordItem.js
@@ -1,23 +1,51 @@
 import * as React from 'react';
-import Immutable from 'seamless-immutable';
+import * as Mousetrap from '../../../util/ws_mousetrap_fork';
+import BundleDetail from '../BundleDetail';
 import { worksheetItemPropsChanged } from '../../../util/worksheet_utils';
 
 class RecordItem extends React.Component {
     /** Constructor. */
     constructor(props) {
         super(props);
-        this.state = Immutable({});
+        this.state = {
+            showDetail: false,
+            bundleInfoUpdates: {},
+        };
     }
 
     handleClick = () => {
         this.props.setFocus(this.props.focusIndex, 0);
+        this.setState({ showDetail: !this.state.showDetail });
+    };
+
+    receiveBundleInfoUpdates = (update) => {
+        let { bundleInfoUpdates } = this.state;
+        // Use object spread to update.
+        bundleInfoUpdates = { ...bundleInfoUpdates, ...update };
+        this.setState({ bundleInfoUpdates: { ...bundleInfoUpdates, ...update } });
     };
 
     shouldComponentUpdate(nextProps, nextState) {
-        return worksheetItemPropsChanged(this.props, nextProps);
+        return (
+            worksheetItemPropsChanged(this.props, nextProps) ||
+            this.state.showDetail !== nextState.showDetail
+        );
     }
 
     render() {
+        if (this.props.focused) {
+            // Use e.preventDefault to avoid openning selected link
+            Mousetrap.bind(
+                ['enter'],
+                (e) => {
+                    e.preventDefault();
+                    if (!this.props.confirmBundleRowAction(e.code)) {
+                        this.setState({ showDetail: !this.state.showDetail });
+                    }
+                },
+                'keydown',
+            );
+        }
         var item = this.props.item;
         var className = 'table table-record' + (this.props.focused ? ' focused' : '');
         var bundleInfo = this.props.item.bundles_spec.bundle_infos[0];
@@ -37,12 +65,29 @@ class RecordItem extends React.Component {
         });
 
         return (
-            <div className='ws-item' onClick={this.handleClick}>
-                <div className='type-record'>
+            <div className='ws-item'>
+                <div className='type-record' onClick={this.handleClick}>
                     <table className={className}>
                         <tbody>{items}</tbody>
                     </table>
                 </div>
+                {this.state.showDetail && (
+                    <BundleDetail
+                        uuid={bundleInfo.uuid}
+                        ref='bundleDetail'
+                        bundleMetadataChanged={this.props.reloadWorksheet}
+                        onUpdate={this.receiveBundleInfoUpdates}
+                        onClose={() => {
+                            this.setState({
+                                showDetail: false,
+                            });
+                        }}
+                        isFocused={this.props.focused}
+                        focusIndex={this.props.focusIndex}
+                        showDetail={this.state.showDetail}
+                        editPermission={this.props.editPermission}
+                    />
+                )}
             </div>
         );
     }
