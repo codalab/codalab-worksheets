4ac1c5cb4c12d339ec3722501740c0c527cca68d
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-02-27 18:01:48 +0000

    throw obj data

--- codalab/client/json_api_client.py
+++ codalab/client/json_api_client.py
@@ -280,6 +280,7 @@ class JsonApiClient(RestClient):
         def unpack_object(obj_data):
             # Merge attributes, id, meta, and relationships into a single dict
             obj = {}
+            raise Exception(obj_data)
             obj['id'] = obj_data['id']
             if 'attributes' in obj_data:
                 obj.update(obj_data['attributes'])
