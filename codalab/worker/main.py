6563469461604a3e4c3b98139f3f2787c1d1edac
Nelson Liu <nelson-liu@users.noreply.github.com>
2020-02-28 20:22:41 -0800

    Fix worker default cpu count to reflect SLURM limits. (#2046)
    * Fix worker default cpu count to reflect SLURM limits.
    
    * Use physical core count as fallback.

--- codalab/worker/main.py
+++ codalab/worker/main.py
@@ -229,7 +229,14 @@ def parse_cpuset_args(arg):
     Arguments:
         arg: comma separated string of ints, or "ALL" representing all available cpus
     """
-    cpu_count = multiprocessing.cpu_count()
+    try:
+        # Get number of cores that the process can actually use.
+        cpu_count = len(os.sched_getaffinity(0))
+    except AttributeError:
+        # os.sched_getaffinity() isn't available on all platforms,
+        # so fallback to using the number of physical cores.
+        cpu_count = multiprocessing.cpu_count()
+
     if arg == 'ALL':
         cpuset = list(range(cpu_count))
     else:
