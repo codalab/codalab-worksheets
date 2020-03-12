50c01b9971ada279c9cfae4f124209743324a834
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-03-12 20:03:30 +0000

    add semaphore to limit concurrent requests

--- frontend/src/components/worksheets/items/PlaceholderItem.js
+++ frontend/src/components/worksheets/items/PlaceholderItem.js
@@ -1,31 +1,41 @@
 import React, { useState, useEffect, forwardRef } from 'react';
-import $ from "jquery";
-import queryString from "query-string";
+import $ from 'jquery';
+import queryString from 'query-string';
 import './PlaceholderItem.scss';
 import { BLOCK_TO_COMPONENT } from '../WorksheetItemList';
+import { Semaphore } from 'await-semaphore';
 
-const fetchData = async ({ worksheetUUID, directive }) => {
-    const queryParams = {
-        directive
-    };
-    const info = await $.ajax({
-        type: 'GET',
-        url: '/rest/interpret/worksheet/' + worksheetUUID + '?' + queryString.stringify(queryParams),
-        async: true,
-        dataType: 'json',
-        cache: false
+// Limit concurrent requests for resolving placeholder items
+const MAX_CONCURRENT_REQUESTS = 3;
+const semaphore = new Semaphore(MAX_CONCURRENT_REQUESTS);
+
+async function fetchData({ worksheetUUID, directive }) {
+    return semaphore.use(async () => {
+        const queryParams = {
+            directive,
+        };
+        const info = await $.ajax({
+            type: 'GET',
+            url:
+                '/rest/interpret/worksheet/' +
+                worksheetUUID +
+                '?' +
+                queryString.stringify(queryParams),
+            async: true,
+            dataType: 'json',
+            cache: false,
+        });
+        return info;
     });
-    return info;
-};
+}
 
 export default forwardRef((props, ref) => {
     const [item, setItem] = useState(null);
     const [error, setError] = useState(false);
+    const { worksheetUUID } = props;
+    const { directive } = props.item;
     useEffect(() => {
-        (async function () {
-            console.log("running effect for item ", props.item);
-            const { worksheetUUID } = props;
-            const { directive } = props.item;
+        (async function() {
             try {
                 const { items } = await fetchData({ directive, worksheetUUID });
                 setItem(items[0]);
@@ -34,7 +44,7 @@ export default forwardRef((props, ref) => {
                 setError(e);
             }
         })();
-    }, []);
+    }, [directive, worksheetUUID]);
     if (error) {
         return <div ref={ref}>Error loading item.</div>;
     }
