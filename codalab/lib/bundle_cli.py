1466372db169b675c25de74965a9ffe7e1ffae9f
Jing Ge <jingge2@illinois.edu>
2020-03-24 20:16:04 -0700

    Improve CLI documentation and add -m option for auto generating CLI docs (#2057)
    Co-authored-by: Jing Ge <stanford@Stanfords-MacBook-Pro.local>

--- codalab/lib/bundle_cli.py
+++ codalab/lib/bundle_cli.py
@@ -140,6 +140,9 @@ SERVER_COMMANDS = (
 )
 
 OTHER_COMMANDS = ('help', 'status', 'alias', 'config', 'logout')
+# Markdown headings
+HEADING_LEVEL_2 = '## '
+HEADING_LEVEL_3 = '### '
 
 
 class CodaLabArgumentParser(argparse.ArgumentParser):
@@ -258,7 +261,7 @@ class Commands(object):
         return register_command
 
     @classmethod
-    def help_text(cls, verbose):
+    def help_text(cls, verbose, markdown):
         def command_name(command):
             name = command
             aliases = cls.commands[command].aliases
@@ -307,6 +310,8 @@ class Commands(object):
                 )
 
             if verbose:
+                if markdown:
+                    name = HEADING_LEVEL_3 + name
                 return '%s%s:\n%s\n%s' % (
                     ' ' * indent,
                     name,
@@ -324,31 +329,39 @@ class Commands(object):
         def command_group_help_text(commands):
             return '\n'.join([command_help_text(command) for command in commands])
 
+        def doc_formatter():
+            return HEADING_LEVEL_2 if verbose and markdown else ''
+
+        def command_formatter():
+            return '`' if verbose and markdown else ''
+
         return (
             textwrap.dedent(
                 """
-        Usage: cl <command> <arguments>
+        Usage: {inline_code}cl <command> <arguments>{inline_code}
 
-        Commands for bundles:
+        {heading}Commands for bundles:
         {bundle_commands}
 
-        Commands for worksheets:
+        {heading}Commands for worksheets:
         {worksheet_commands}
 
-        Commands for groups and permissions:
+        {heading}Commands for groups and permissions:
         {group_and_permission_commands}
 
-        Commands for users:
+        {heading}Commands for users:
         {user_commands}
 
-        Commands for managing server:
+        {heading}Commands for managing server:
         {server_commands}
 
-        Other commands:
+        {heading}Other commands:
         {other_commands}
         """
             )
             .format(
+                heading=doc_formatter(),
+                inline_code=command_formatter(),
                 bundle_commands=command_group_help_text(BUNDLE_COMMANDS),
                 worksheet_commands=command_group_help_text(WORKSHEET_COMMANDS),
                 group_and_permission_commands=command_group_help_text(
@@ -857,12 +870,19 @@ class BundleCLI(object):
             'Show usage information for commands.',
             '  help           : Show brief description for all commands.',
             '  help -v        : Show full usage information for all commands.',
+            '  help -v -m     : Show full usage information for all commands in Markdown format.',
             '  help <command> : Show full usage information for <command>.',
         ],
         arguments=(
             Commands.Argument('command', help='name of command to look up', nargs='?'),
             Commands.Argument(
                 '-v', '--verbose', action='store_true', help='Display all options of all commands.'
+            ),
+            Commands.Argument(
+                '-m',
+                '--markdown',
+                action='store_true',
+                help='Auto-generate all options of all commands for CLI markdown in Markdown format.',
             ),
         ),
     )
@@ -871,7 +891,7 @@ class BundleCLI(object):
         if args.command:
             self.do_command([args.command, '--help'])
             return
-        print(Commands.help_text(args.verbose), file=self.stdout)
+        print(Commands.help_text(args.verbose, args.markdown), file=self.stdout)
 
     @Commands.command('status', aliases=('st',), help='Show current client status.')
     def do_status_command(self, args):
@@ -1630,8 +1650,7 @@ class BundleCLI(object):
             Commands.Argument('-d', '--description', help='New bundle description.'),
             Commands.Argument(
                 '--anonymous',
-                help='Set bundle to be anonymous (identity of the owner will NOT \n'
-                'be visible to users without \'all\' permission on the bundle).',
+                help='Set bundle to be anonymous (identity of the owner will NOT be visible to users without \'all\' permission on the bundle).',
                 dest='anonymous',
                 action='store_true',
                 default=None,
@@ -2930,8 +2949,7 @@ class BundleCLI(object):
             ),
             Commands.Argument(
                 '--anonymous',
-                help='Set worksheet to be anonymous (identity of the owner will NOT \n'
-                'be visible to users without \'all\' permission on the worksheet).',
+                help='Set worksheet to be anonymous (identity of the owner will NOT be visible to users without \'all\' permission on the worksheet).',
                 dest='anonymous',
                 action='store_true',
                 default=None,
--- scripts/gen-cli-docs.py
+++ scripts/gen-cli-docs.py
@@ -2,20 +2,17 @@
 Generate CLI docs.
 """
 import os
-import sys
 import argparse
-from codalab.bin import cl  # Important to put here to register the commands.
 from codalab.lib.bundle_cli import Commands
 
 INTRODUCTION = """# CLI Reference
 
-This file is auto-generated from the output of `cl help -v` and provides the list of all CLI commands.
+This file is auto-generated from the output of `cl help -v -m` and provides the list of all CLI commands.
 """
 
 
-def indent(s, padding='    '):
-    # Add `padding` in front of each non-empty line.
-    return '\n'.join(padding + line if line else '' for line in s.split('\n'))
+def indent(s):
+    return '\n'.join(line if line else '' for line in s.split('\n'))
 
 
 def main():
@@ -25,7 +22,7 @@ def main():
 
     with open(os.path.join(args.docs, 'CLI-Reference.md'), 'w') as f:
         print(INTRODUCTION, file=f)
-        print(indent(Commands.help_text(True)), file=f)
+        print(indent(Commands.help_text(verbose=True, markdown=True)), file=f)
 
 
 if __name__ == '__main__':
