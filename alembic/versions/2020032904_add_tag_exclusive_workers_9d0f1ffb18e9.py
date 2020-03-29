6622d2449ca136177c87e073a6d64597cb62ad50
Nelson Liu <nfliu@nelsonliu.me>
2020-03-29 00:31:38 -0700

    Add alembic migration

---
+++ alembic/versions/2020032904_add_tag_exclusive_workers_9d0f1ffb18e9.py
@@ -0,0 +1,22 @@
+"""Add tag_exclusive workers
+
+Revision ID: 9d0f1ffb18e9
+Revises: 75d4288ae265
+Create Date: 2020-03-29 04:57:59.567422
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '9d0f1ffb18e9'
+down_revision = '75d4288ae265'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('worker', sa.Column('tag_exclusive', sa.Boolean(), nullable=False, server_default='0'))
+
+
+def downgrade():
+    op.drop_column('worker', 'tag_exclusive')
