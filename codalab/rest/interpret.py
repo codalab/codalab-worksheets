3a9652b9023e56940d11a62cebc205742e089219
yipenghe <yipenghe@stanford.edu>
2020-04-18 23:07:40 -0700

    remove expand_search_items

--- codalab/rest/interpret.py
+++ codalab/rest/interpret.py
@@ -685,11 +685,6 @@ def perform_search_query(value_obj):
         # Not a search query
         return []
 
-
-def expand_search_items(raw_items):
-    return
-
-
 def expand_search_item(raw_item):
     """
     Raw items that include searches must be expanded into more raw items.
