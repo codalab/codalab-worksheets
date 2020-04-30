6dadbc3ad2aa847849660549d91eb0b2758974a0
yipenghe <yipenghe@stanford.edu>
2020-04-30 12:01:14 -0700

    set default to 65%

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -5,7 +5,7 @@ export const NAVBAR_HEIGHT = 60;
 
 // Worksheet width
 export const EXPANDED_WORKSHEET_WIDTH = '90%';
-export const DEFAULT_WORKSHEET_WIDTH = '50%';
+export const DEFAULT_WORKSHEET_WIDTH = '65%';
 export const FILE_SIZE_LIMIT_GB = 2;
 export const FILE_SIZE_LIMIT_B = FILE_SIZE_LIMIT_GB * 1024 * 1024 * 1024;
 export const LOCAL_STORAGE_WORKSHEET_WIDTH = 'worksheetWidth';
