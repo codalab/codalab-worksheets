606459697c7b825611770d4dfa2e804d258aff69
yipenghe <yipenghe@stanford.edu>
2020-05-13 16:36:35 -0700

    implement add, remove, move up/down for field rows

--- frontend/src/components/worksheets/items/SchemaItem/SchemaItem.js
+++ frontend/src/components/worksheets/items/SchemaItem/SchemaItem.js
@@ -6,12 +6,18 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from './SchemaCell';
 import TableRow from '@material-ui/core/TableRow';
-import { getAfterSortKey } from '../../../../util/worksheet_utils';
+import CheckIcon from '@material-ui/icons/Check';
 import TextField from '@material-ui/core/TextField';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import IconButton from '@material-ui/core/IconButton';
 import * as Mousetrap from '../../../../util/ws_mousetrap_fork';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import DeleteSweepIcon from '@material-ui/icons/DeleteSweep';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import EditIcon from '@material-ui/icons/Edit';
+import ClearIcon from '@material-ui/icons/Clear';
 
 class SchemaItem extends React.Component<{
     worksheetUUID: string,
@@ -28,27 +34,62 @@ class SchemaItem extends React.Component<{
             insertBefore: -1,
             hovered: false,
             showSchemaDetail: false,
-            rowContents: [],
+            rows: [...this.props.item.field_rows],
+            editing: false,
         };
     }
+
+    toggleEdit = (clear) => () => {
+        if (clear) {
+            this.setState({ rows: this.props.item.field_rows, editing: !this.state.editing });
+            return;
+        }
+        this.setState({ editing: !this.state.editing });
+    };
 
     updateRowIndex = (rowIndex) => {
         this.props.setFocus(this.props.focusIndex, rowIndex);
     };
 
-    render() {
-        const { classes, worksheetUUID, setFocus, prevItem, editPermission } = this.props;
-        var className = 'type-markup ' + (this.props.focused ? 'focused' : '');
-
-        let prevItemProcessed = null;
-        if (prevItem) {
-            const { maxKey } = getAfterSortKey(prevItem);
-            prevItemProcessed = { sort_key: maxKey };
-        }
-
+    addFieldRowAfter = (idx) => (e) => {
+        let newRow = {};
         const schemaItem = this.props.item;
         const schemaHeaders = schemaItem.header;
-        console.log(schemaItem);
+        schemaHeaders.forEach((header) => {
+            newRow[header] = '';
+        });
+        let curRow = this.state.rows;
+        curRow.splice(idx + 1, 0, newRow);
+        this.setState({ rows: curRow });
+    };
+
+    changeFieldValue = (idx, key) => (e) => {
+        this.state.rows[idx][key] = e.target.value;
+        this.setState({ rows: this.state.rows });
+    };
+
+    moveFieldRow = (idx, direction) => () => {
+        // -1 for moving up, 1 for moving down
+        let newIndex = idx + direction;
+        [this.state.rows[newIndex], this.state.rows[idx]] = [
+            this.state.rows[idx],
+            this.state.rows[newIndex],
+        ];
+        console.log(this.state.rows);
+        this.setState({ rows: this.state.rows });
+    };
+
+    removeFieldRow = (idx) => () => {
+        this.state.rows.splice(idx, 1);
+        this.setState({ rows: this.state.rows });
+    };
+
+    render() {
+        const { classes, worksheetUUID, setFocus, prevItem, editPermission } = this.props;
+        const { editing } = this.state;
+        var className = 'type-markup ' + (this.props.focused ? 'focused' : '');
+        const schemaItem = this.props.item;
+        const schemaHeaders = schemaItem.header;
         let headerHtml, bodyRowsHtml;
         headerHtml =
             this.state.showSchemaDetail &&
@@ -59,44 +100,101 @@ class SchemaItem extends React.Component<{
                         onMouseLeave={(e) => this.setState({ hovered: false })}
                         component='th'
                         key={index}
-                        style={{ paddingLeft: '30px' }}
+                        style={{ padding: '5', fontSize: '16px', maxWidth: '100' }}
                     >
                         {item}
                     </TableCell>
                 );
             });
-
+        if (headerHtml) {
+            headerHtml.push(
+                <TableCell
+                    key={headerHtml.length}
+                    onMouseEnter={(e) => this.setState({ hovered: true })}
+                    onMouseLeave={(e) => this.setState({ hovered: false })}
+                    style={{ padding: '5' }}
+                    component='th'
+                    scope='row'
+                >
+                    <IconButton disabled={!editing} onClick={this.addFieldRowAfter(-1)}>
+                        <AddCircleIcon />
+                    </IconButton>
+                    {!editing ? (
+                        <IconButton onClick={this.toggleEdit(false)}>
+                            <EditIcon />
+                        </IconButton>
+                    ) : (
+                        <IconButton onClick={this.toggleEdit(false)}>
+                            <CheckIcon />
+                        </IconButton>
+                    )}
+                    {editing && (
+                        <IconButton onClick={this.toggleEdit(true)}>
+                            <ClearIcon />
+                        </IconButton>
+                    )}
+                </TableCell>,
+            );
+        }
         bodyRowsHtml =
             this.state.showSchemaDetail &&
-            schemaItem.field_rows.map((rowItem, rowIndex) => {
+            this.state.rows.map((rowItem, ind) => {
                 let rowCells = schemaHeaders.map((headerKey, col) => {
-                    let rowContent = rowItem[headerKey];
-                    console.log(rowItem[headerKey]);
                     return (
                         <TableCell
                             key={col}
                             onMouseEnter={(e) => this.setState({ hovered: true })}
                             onMouseLeave={(e) => this.setState({ hovered: false })}
-                            style={{ paddingLeft: '30px', width: '300px' }}
+                            style={{ padding: '5' }}
                             component='th'
                             scope='row'
                         >
                             <TextField
                                 id='standard-multiline-static'
                                 multiline
-                                defaultValue={rowContent || '<none>'}
+                                placeholder={'<none>'}
+                                value={this.state.rows[ind][headerKey] || ''}
+                                disabled={!editing}
+                                onChange={this.changeFieldValue(ind, headerKey)}
                             />
                         </TableCell>
                     );
                 });
+                rowCells.push(
+                    <TableCell
+                        key={rowCells.length}
+                        onMouseEnter={(e) => this.setState({ hovered: true })}
+                        onMouseLeave={(e) => this.setState({ hovered: false })}
+                        style={{ padding: '5' }}
+                        component='th'
+                        scope='row'
+                    >
+                        <IconButton disabled={!editing} onClick={this.addFieldRowAfter(ind)}>
+                            <AddCircleIcon />
+                        </IconButton>
+                        <IconButton disabled={!editing} onClick={this.removeFieldRow(ind)}>
+                            <DeleteSweepIcon />
+                        </IconButton>
+                        <IconButton
+                            disabled={!editing || ind === 0}
+                            onClick={this.moveFieldRow(ind, -1)}
+                        >
+                            <ArrowDropUpIcon />
+                        </IconButton>
+                        <IconButton
+                            disabled={!editing || ind === this.state.rows.length - 1}
+                            onClick={this.moveFieldRow(ind, 1)}
+                        >
+                            <ArrowDropDownIcon />
+                        </IconButton>
+                    </TableCell>,
+                );
                 return (
                     <TableBody>
                         <TableRow>{rowCells}</TableRow>
                     </TableBody>
                 );
             });
-        // bodyRowsHtml = this.state.showSchemaDetail &&
-
         if (this.props.focused) {
             // Use e.preventDefault to avoid openning selected link
             Mousetrap.bind(
@@ -118,32 +216,27 @@ class SchemaItem extends React.Component<{
         );
 
         var className = 'type-markup ' + (this.props.focused ? 'focused' : '');
-
+        console.log('ROWS:', this.state.rows, this.props.item.field_rows);
         return (
-            <div
-                className='ws-item'
-                onClick={() => {
-                    this.props.setFocus(this.props.focusIndex, 0);
-                }}
-            >
-                <div className={`${className}`}>
-                    {showSchemasButton}
-                    {'Schema: ' + schemaItem.schema_name}
+            editPermission && (
+                <div
+                    className='ws-item'
+                    onClick={() => {
+                        this.props.setFocus(this.props.focusIndex, 0);
+                    }}
+                >
+                    <div className={`${className}`}>
+                        {showSchemasButton}
+                        {'Schema: ' + schemaItem.schema_name}
+                    </div>
+                    <Table>
+                        <TableHead>
+                            <TableRow>{headerHtml}</TableRow>
+                        </TableHead>
+                        {bodyRowsHtml}
+                    </Table>
                 </div>
-                <Table>
-                    <TableHead>
-                        <TableRow
-                            style={{
-                                height: 36,
-                                borderTop: '2px solid #DEE2E6',
-                            }}
-                        >
-                            {headerHtml}
-                        </TableRow>
-                    </TableHead>
-                    {bodyRowsHtml}
-                </Table>
-            </div>
+            )
         );
     }
 }
