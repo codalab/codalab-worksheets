cd2f9806a520775cab84ee7aad8419b326fda5fe
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-05-14 15:17:10 -0400

    fix force subprocess

--- test_cli.py
+++ test_cli.py
@@ -207,7 +207,8 @@ def _run_command(
 ):
     # We skip using the cli directly if force_subprocess is set to true (which forces
     # us to use subprocess even for cl commands).
-    force_subprocess = not force_subprocess and args[0] == cl
+    if args[0] == cl:
+        force_subprocess = force_subprocess if args[0] == cl else True
     return run_command(
         args, expected_exit_code, max_output_chars, env, include_stderr, binary, force_subprocess
     )
