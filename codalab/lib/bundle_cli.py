59c2dc64f01d0efb783f8415ee489d9c04f614ed
yshen-google <yiyangs@google.com>
2020-05-28 16:40:37 +0000

    address comment

--- codalab/lib/bundle_cli.py
+++ codalab/lib/bundle_cli.py
@@ -2268,8 +2268,7 @@ class BundleCLI(object):
     def print_permissions(self, info):
         print('permission: %s' % permission_str(info['permission']), file=self.stdout)
         print('group_permissions:', file=self.stdout)
-        if 'group_permissions' in info:
-            print('  %s' % group_permissions_str(info['group_permissions']), file=self.stdout)
+        print('  %s' % group_permissions_str(info.get('group_permissions', [])), file=self.stdout)
 
     def print_contents(self, client, info):
         def wrap(string):
