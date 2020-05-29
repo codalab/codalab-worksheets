a6243223f80e4223f088cff2f91cd0a9f54bd34c
Nelson Liu <nfliu@nelsonliu.me>
2020-05-29 10:33:41 -0700

    Blacken

--- codalab/worker_manager/aws_batch_worker_manager.py
+++ codalab/worker_manager/aws_batch_worker_manager.py
@@ -3,7 +3,8 @@ try:
 except ModuleNotFoundError:
     raise ModuleNotFoundError(
         "Running the worker manager requires the boto3 module.\n"
-        "Please run: pip install boto3==1.9.228")
+        "Please run: pip install boto3==1.9.228"
+    )
 import logging
 import os
 import uuid
