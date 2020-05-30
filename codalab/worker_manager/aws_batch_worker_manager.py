a7c4a04eee101bfe180ff4ee326bee2a8d8ea7c3
Nelson Liu <nelson-liu@users.noreply.github.com>
2020-05-29 17:04:56 -0700

    Add flag to cleanup worker work-dir upon exit (#2313)
    * Add flag to cleanup worker work-dir upon exit
    
    * Blacken, add flag to workermanager
    
    Co-authored-by: Jing Ge <jingge2@illinois.edu>

--- codalab/worker_manager/aws_batch_worker_manager.py
+++ codalab/worker_manager/aws_batch_worker_manager.py
@@ -90,6 +90,8 @@ class AWSBatchWorkerManager(WorkerManager):
             command.extend(['--tag', self.args.worker_tag])
         if self.args.worker_max_work_dir_size:
             command.extend(['--max-work-dir-size', self.args.worker_max_work_dir_size])
+        if self.args.worker_delete_work_dir_on_exit:
+            command.extend(['--worker-delete-work-dir-on-exit'])
 
         # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-batch-jobdefinition.html
         # Need to mount:
