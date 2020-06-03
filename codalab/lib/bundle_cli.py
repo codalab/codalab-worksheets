dd005d3ffc3c6b84ef010fa991666dd4a39c46f2
yipenghe <yipenghe@stanford.edu>
2020-06-03 12:43:45 -0700

    uncomment print

--- codalab/lib/bundle_cli.py
+++ codalab/lib/bundle_cli.py
@@ -3223,8 +3223,7 @@ class BundleCLI(object):
             elif mode == BlockModes.placeholder_block:
                 print('[Placeholder]', block['directive'], file=self.stdout)
             elif mode == BlockModes.schema_block:
-                # print('[SchemaBlock]', file=self.stdout)
-                pass
+                print('[SchemaBlock]', file=self.stdout)
             else:
                 raise UsageError('Invalid display mode: %s' % mode)
 
