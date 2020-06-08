c5b67fbcd20598556bdbfb095ed6e9ea21e84c89
Tony Lee <tonyh.lee@yahoo.com>
2020-06-07 17:30:26 -0700

    debug

--- scripts/create_sample_worksheet.py
+++ scripts/create_sample_worksheet.py
@@ -105,7 +105,7 @@ class SampleWorksheet:
                 )
 
         assert not has_error
-        print('Finished validating content of the sample worksheet...2')
+        print('Finished validating content of the sample worksheet...3')
         print('Success.')
 
     def _create_dependencies(self):
