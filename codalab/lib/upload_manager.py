9f54e4141ca07966193744bcdd26aa2d699dfa2e
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-06-10 16:34:13 -0400

    fix typo

--- codalab/lib/upload_manager.py
+++ codalab/lib/upload_manager.py
@@ -182,7 +182,7 @@ class UploadManager(object):
         return os.path.exists(self._bundle_store.get_bundle_location(bundle.uuid))
 
     def cleanup_existing_contents(self, bundle):
-        self._bundle_store.cleanup(bundle, dry_run=False)
+        self._bundle_store.cleanup(bundle.uuid, dry_run=False)
         bundle_update = {'data_hash': None, 'metadata': {'data_size': 0}}
         self._bundle_model.update_bundle(bundle, bundle_update)
         self._bundle_model.update_user_disk_used(bundle.owner_id)
