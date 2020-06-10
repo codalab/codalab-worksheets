315934e24d91463963a9768ce0ec832feeee3c02
Jing Ge <stanford@Stanfords-MacBook-Pro.local>
2020-06-09 17:18:03 -0700

    Update print information

--- codalab/lib/bundle_cli.py
+++ codalab/lib/bundle_cli.py
@@ -3377,7 +3377,7 @@ class BundleCLI(object):
                 )
 
         print(
-            'Copied %s worksheet items to %s.' % (len(source_items), dest_worksheet_uuid),
+            'Copied %s worksheet items to %s.' % (len(valid_source_items), dest_worksheet_uuid),
             file=self.stdout,
         )
 
