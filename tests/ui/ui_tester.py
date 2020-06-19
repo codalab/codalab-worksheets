258f59b4f1b81adc78ccdddc785512252b192fbb
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-06-19 14:30:35 -0400

    debug

--- tests/ui/ui_tester.py
+++ tests/ui/ui_tester.py
@@ -111,6 +111,7 @@ class UITester(ABC):
         # Edit bundle permission
         self.scroll_to_bottom('worksheet_container')
         self.browser.find_elements_by_tag_name('svg')[-1].click()
+        self.pause()
         select_boxes = self.browser.find_elements_by_tag_name('select')
         self.select_option(select_boxes[-1], permission)
         self.longer_pause()
