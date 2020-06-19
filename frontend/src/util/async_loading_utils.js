9ce75b09a05b8698db161afcdd8112c6b6d9aa9b
yipenghe <yipenghe@stanford.edu>
2020-06-19 10:48:11 -0700

    Fix showing file contents for record item  (#2455)
    fix #2446
    
    to test:
    create a schema with a field using files:
    % schema a
    % add hello /stdout
    
    display bundles in record mode
    % display record a

--- frontend/src/util/async_loading_utils.js
+++ frontend/src/util/async_loading_utils.js
@@ -1,5 +1,22 @@
 import { Semaphore } from 'await-semaphore';
+import $ from 'jquery';
 
 // Limit concurrent requests for async resolving items
 const MAX_CONCURRENT_REQUESTS = 3;
 export const semaphore = new Semaphore(MAX_CONCURRENT_REQUESTS);
+
+export async function fetchAsyncBundleContents({ contents }) {
+    // used in table and record items
+    return semaphore.use(async () => {
+        const response = await $.ajax({
+            type: 'POST',
+            contentType: 'application/json',
+            url: '/rest/interpret/genpath-table-contents',
+            async: true,
+            data: JSON.stringify({ contents }),
+            dataType: 'json',
+            cache: false,
+        });
+        return response;
+    });
+}
