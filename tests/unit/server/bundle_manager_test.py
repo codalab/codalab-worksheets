ca33d785c808599bf916d328aa39281a492bfcac
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-09-02 11:26:39 -0400

    more tests

--- tests/unit/server/bundle_manager_test.py
+++ tests/unit/server/bundle_manager_test.py
@@ -851,30 +851,53 @@ class BundleManagerScheduleRunBundlesTest(BaseBundleManagerTest):
         bundle = self.bundle_manager._model.get_bundle(bundle.uuid)
         self.assertEqual(bundle.state, State.STARTING)
 
-    def test_stage_single_bundle_request_gpu(self):
+    # TODO: switch to the newest version of freezegun with the patch in https://github.com/spulec/freezegun/pull/353,
+    # so that we can use as_kwarg and thus maintain the order of parameters as (self, frozen_time).
+    @freeze_time("2020-02-01", as_arg=True)
+    def test_cleanup_dead_workers(frozen_time, self):
+        # Workers should be removed after they don't check in for a long enough time period.
+        self.create_mock_worker(cpus=1)
+
+        self.assertEqual(len(self.bundle_manager._worker_model.get_workers()), 1)
+
+        frozen_time.move_to("2020-02-12")
+        self.bundle_manager._schedule_run_bundles()
+
+        self.assertEqual(len(self.bundle_manager._worker_model.get_workers()), 0)
+
+    def test_restage_stuck_starting_bundles(self):
+        # No workers are currently running this bundle, so it should be restaged.
         bundle = RunBundle.construct(
             targets=[],
             command='',
-            metadata=dict(
-                BASE_METADATA, request_memory="0", request_time="", request_cpus=0, request_gpus=1
-            ),
+            metadata=BASE_METADATA,
             owner_id=self.user_id,
             uuid=generate_uuid(),
-            state=State.STAGED,
+            state=State.STARTING,
         )
         self.bundle_manager._model.save_bundle(bundle)
 
-        self.create_mock_worker(cpus=1)
         self.bundle_manager._schedule_run_bundles()
 
         bundle = self.bundle_manager._model.get_bundle(bundle.uuid)
         self.assertEqual(bundle.state, State.STAGED)
 
-        self.create_mock_worker(gpus=1)
+    def test_bring_offline_stuck_running_bundles(self):
+        # No workers exist to claim this bundle, so it should go to the WORKER_OFFLINE state.
+        bundle = RunBundle.construct(
+            targets=[],
+            command='',
+            metadata=BASE_METADATA,
+            owner_id=self.user_id,
+            uuid=generate_uuid(),
+            state=State.RUNNING,
+        )
+        self.bundle_manager._model.save_bundle(bundle)
+
         self.bundle_manager._schedule_run_bundles()
 
         bundle = self.bundle_manager._model.get_bundle(bundle.uuid)
-        self.assertEqual(bundle.state, State.STARTING)
+        self.assertEqual(bundle.state, State.WORKER_OFFLINE)
 
 
 class BundleManagerFailUnresponsiveBundlesTest(BaseBundleManagerTest):
@@ -885,6 +908,7 @@ class BundleManagerFailUnresponsiveBundlesTest(BaseBundleManagerTest):
     # so that we can use as_kwarg and thus maintain the order of parameters as (self, frozen_time).
     @freeze_time("2012-01-14", as_arg=True)
     def test_fail_bundle(frozen_time, self):
+        # Bundles stuck in uploading state for too long should be failed.
         bundle = RunBundle.construct(
             targets=[],
             command='',
