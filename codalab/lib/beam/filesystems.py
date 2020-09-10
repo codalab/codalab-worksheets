deb699ba27eac1208eeecfa4822f03cd5d4c10ec
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-09-10 15:13:15 -0400

    add AZURITE_ACCOUNTS

--- codalab/lib/beam/filesystems.py
+++ codalab/lib/beam/filesystems.py
@@ -1,6 +1,6 @@
 import os
 from apache_beam.io.filesystems import FileSystems
-from codalab.lib.beam.blobstoragefilesystem import BlobStorageFileSystem
+from .blobstoragefilesystem import BlobStorageFileSystem
 
 """
 Modifies Beam to add support for Azure Blob Storage filesystems.
@@ -12,4 +12,4 @@ we will switch to using a Beam library instead.
 # Test key for Azurite (local development)
 os.environ[
     'AZURE_STORAGE_CONNECTION_STRING'
-] = 'DefaultEndpointsProtocol=https;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=https://127.0.0.1:10000/devstoreaccount1;'
+] = 'DefaultEndpointsProtocol=http;AccountName=storageclwsdev0;AccountKey=key;BlobEndpoint=http://azurite:10000/devstoreaccount1;'
