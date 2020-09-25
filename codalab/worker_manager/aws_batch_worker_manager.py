fc0313c54e6df8c941fcda7eb2155a351e5f3e0b
Nelson Liu <nfliu@nelsonliu.me>
2020-09-25 11:44:33 -0700

    simplify

--- codalab/worker_manager/aws_batch_worker_manager.py
+++ codalab/worker_manager/aws_batch_worker_manager.py
@@ -9,22 +9,6 @@ from codalab.lib.telemetry_util import CODALAB_SENTRY_INGEST, using_sentry
 logger = logging.getLogger(__name__)
 
 
-def get_boto3_pip_version():
-    server_requirements_path = os.path.abspath(
-        os.path.join(
-            os.path.dirname(os.path.realpath(__file__)),
-            os.pardir,
-            os.pardir,
-            "requirements-server.txt",
-        )
-    )
-    with open(server_requirements_path) as f:
-        for line in f:
-            if line.startswith("boto3"):
-                return line.rstrip("\n")
-    raise ValueError(f"Failed to fetch boto3 version from {server_requirements_path}")
-
-
 class AWSBatchWorkerManager(WorkerManager):
     NAME = 'aws-batch'
     DESCRIPTION = 'Worker manager for submitting jobs to AWS Batch'
@@ -78,7 +62,7 @@ class AWSBatchWorkerManager(WorkerManager):
         except ModuleNotFoundError:
             raise ModuleNotFoundError(
                 "Running the AWS worker manager requires the boto3 module.\n"
-                f"Please run: pip install {get_boto3_pip_version()}"
+                "Please run: pip install boto3"
             )
         self.batch_client = boto3.client('batch', region_name=self.args.region)
 
