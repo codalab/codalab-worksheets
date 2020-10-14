c1154d8c7b15aba8ce9fb79dfdc4e346561a20b2
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-10-13 21:09:45 -0400

    Bump version to 0.5.24

--- codalab/common.py
+++ codalab/common.py
@@ -8,7 +8,7 @@ import http.client
 
 # Increment this on master when ready to cut a release.
 # http://semver.org/
-CODALAB_VERSION = '0.5.23'
+CODALAB_VERSION = '0.5.24'
 BINARY_PLACEHOLDER = '<binary>'
 URLOPEN_TIMEOUT_SECONDS = 30
 
