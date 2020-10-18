7fc243f8b7283117e3c90320fb425fabf0052af7
Nelson Liu <nfliu@nelsonliu.me>
2020-10-18 11:38:28 -0700

    Use instance-action endpoint

--- scripts/detect-ec2-spot-preemption.sh
+++ scripts/detect-ec2-spot-preemption.sh
@@ -7,16 +7,17 @@ do
     # It's a special endpoint set up by AWS whereby AWS instances can view metadata about themselves.
     # One such piece of metadata is the termination time, which is only set when the spot instance is to be
     # pre-empted (you get a 404 otherwise).
-    # This script was partially taken from https://stackoverflow.com/q/32613600/14089059 .
-    if [ -z $(curl -Is http://169.254.169.254/latest/meta-data/spot/termination-time | head -1 | grep 404 | cut -d \  -f 2) ]
+    # This script was partially taken from:
+    # https://github.com/AmazonWebServices-Projects/ec2-spot-labs/blob/master/ec2-spot-interruption-handler/wait_x_seconds_before_interruption.sh
+    if [ -n "$(curl -s http://169.254.169.254/latest/meta-data/spot/instance-action | grep 404)" ];
     then
+        # Instance not yet marked for termination, so sleep and check again in 5 seconds.
+        sleep 5
+    else
         echo "EC2 spot instance scheduled for shutdown."
         echo "Sending SIGTERM to CodaLab workers"
         # Kill all cl-workers in the EC2 instance.
         pgrep -f "cl-worker" | xargs kill
-        sleep 5
-    else
-        # Instance not yet marked for termination, so sleep and check again in 5 seconds.
         sleep 5
     fi
 done
