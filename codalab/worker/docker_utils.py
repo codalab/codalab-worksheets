d5e1f032083eeb93dc47f86d010fe609630edb8f
Nelson Liu <nfliu@nelsonliu.me>
2020-11-05 00:13:52 -0800

    Fix typo

--- codalab/worker/docker_utils.py
+++ codalab/worker/docker_utils.py
@@ -356,7 +356,7 @@ def get_image_size_without_pulling(image_spec):
     requests_session = requests.Session()
     # Retry 5 times, sleeping for [0.1s, 0.2s, 0.4s, ...] between retries.
     retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[413, 429, 500, 502, 503, 504])
-    requests_session.mounft('https://', HTTPAdapter(max_retries=retries))
+    requests_session.mount('https://', HTTPAdapter(max_retries=retries))
 
     while True:
         response = requests_session.get(url=request + str(page_number))
