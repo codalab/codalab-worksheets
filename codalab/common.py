3ad705ad3ae3af12e6385719524ea47ac4f4c1b3
Varyiabol <lyb979428261@gmail.com>
2020-11-08 21:36:13 -0800

    Bump to version 0.5.27 (#3009)

--- codalab/common.py
+++ codalab/common.py
@@ -8,7 +8,7 @@ import http.client
 
 # Increment this on master when ready to cut a release.
 # http://semver.org/
-CODALAB_VERSION = '0.5.26'
+CODALAB_VERSION = '0.5.27'
 BINARY_PLACEHOLDER = '<binary>'
 URLOPEN_TIMEOUT_SECONDS = 5 * 60
 
