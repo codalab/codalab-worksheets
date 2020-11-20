1bc92962b9dced31cc9298e1b8545e1400b4281b
yuqijin <yuqijin@stanford.edu>
2020-11-20 15:21:39 -0800

    Update alembic file format

--- alembic/versions/2020112023_make_date_created_nullable_ddd9989327c7.py
+++ alembic/versions/2020112023_make_date_created_nullable_ddd9989327c7.py
@@ -6,21 +6,17 @@ Create Date: 2020-11-20 23:12:26.393224
 
 """
 
+from alembic import op
+from sqlalchemy.dialects import mysql
+
 # revision identifiers, used by Alembic.
 revision = 'ddd9989327c7'
 down_revision = '0602bcb323f4'
 
-from alembic import op
-import sqlalchemy as sa
-from sqlalchemy.dialects import mysql
 
 def upgrade():
-    op.alter_column('worksheet', 'date_created',
-               existing_type=mysql.DATETIME(),
-               nullable=True)
+    op.alter_column('worksheet', 'date_created', existing_type=mysql.DATETIME(), nullable=True)
 
 
 def downgrade():
-    op.alter_column('worksheet', 'date_created',
-               existing_type=mysql.DATETIME(),
-               nullable=False)
+    op.alter_column('worksheet', 'date_created', existing_type=mysql.DATETIME(), nullable=False)
