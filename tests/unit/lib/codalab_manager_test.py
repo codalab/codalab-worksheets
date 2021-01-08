43425153f64d990ce5d39b90ff97bcdc5f36be68
Tony Lee <tonyh.lee@yahoo.com>
2021-01-08 00:29:23 -0800

    Use a temp CodaLabManager for WorkerManagers

---
+++ tests/unit/lib/codalab_manager_test.py
@@ -0,0 +1,30 @@
+import os
+import unittest
+from pathlib import Path
+
+from codalab.lib.codalab_manager import CodaLabManager
+
+
+class CodalabManagerTest(unittest.TestCase):
+    def setUp(self):
+        os.environ['CODALAB_HOME'] = str(Path.home())
+
+    def tearDown(self):
+        def remove_file_if_exists(file):
+            file_path = os.path.join(str(Path.home()), file)
+            if os.path.exists(file_path):
+                os.remove(file_path)
+
+        # Remove the state and config json files created by the CodaLabManagers
+        remove_file_if_exists('state.json')
+        remove_file_if_exists('config.json')
+        del os.environ['CODALAB_HOME']
+
+    def test_get_state_for_temp_codalab_manager(self):
+        manager: CodaLabManager = CodaLabManager(temporary=True)
+        self.assertEqual(manager.state, {'auth': {}, 'sessions': {}})
+        manager.save_state()
+        self.assertFalse(
+            os.path.exists(manager.state_path),
+            msg='Assert that the current state is not written out to state_path for a temporary CodaLabManager',
+        )
