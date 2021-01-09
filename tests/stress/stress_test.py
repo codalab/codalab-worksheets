bce755599942bf071e31693d5267984d211514c7
yiboliu <lyb979428261@gmail.com>
2021-01-09 09:40:24 -0800

    Replace a bad image tag

--- tests/stress/stress_test.py
+++ tests/stress/stress_test.py
@@ -85,7 +85,7 @@ class StressTestRunner:
         'couchbase:latest',
         'golang:latest',
         'iwane/numpy-matplotlib',
-        'jenkins:latest',
+        'jenkins:2.60.3',
         'larger/rdp:dev',
         'maven:latest',
         'mysql:latest',
