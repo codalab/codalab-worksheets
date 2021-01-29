dd81f5f0f5668ba4009a79418dd6c45e10a7d24e
Tony Lee <tonyh.lee@yahoo.com>
2021-01-28 21:19:54 -0800

    test

--- codalab/worker/worker_run_state.py
+++ codalab/worker/worker_run_state.py
@@ -408,13 +408,7 @@ class RunStateMachine(StateTransitioner):
                 finished=finished, exitcode=exitcode, failure_message=failure_msg
             )
 
-        def check_resource_utilization(run_state: RunState):
-            cpu_usage, memory_limit = docker_utils.get_container_stats_with_docker_stats(
-                run_state.container
-            )
-            run_state = run_state._replace(cpu_usage=cpu_usage)
-            run_state = run_state._replace(memory_limit=memory_limit)
-
+        def check_resource_utilization(run_state):
             kill_messages = []
 
             run_stats = docker_utils.get_container_stats(run_state.container)
