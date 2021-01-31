7c8deba69253685929a052f456f6f7cb5bed1c7a
Ashwin Ramaswami <aramaswamis@gmail.com>
2021-01-31 09:48:59 -0500

    remove empty string

--- codalab/lib/worksheet_util.py
+++ codalab/lib/worksheet_util.py
@@ -412,7 +412,7 @@ def interpret_genpath(bundle_info, genpath, db_model=None, owner_cache=None):
     elif genpath == 'summary':
 
         def friendly_render_dep(dep):
-            key = dep['child_path'] or dep['parent_name'] or ''
+            key = dep['child_path'] or dep['parent_name']
             friendly_parent_name = formatting.verbose_contents_str(dep['parent_name'])
             value = "%s{%s%s}" % (
                 key,
