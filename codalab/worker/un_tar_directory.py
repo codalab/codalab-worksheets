fbcbc4b19c479e2e2c059b796cad50d604695e8e
Ashwin Ramaswami <aramaswamis@gmail.com>
2021-02-14 22:03:00 -0500

    Speed up CLI by lazy loading some imports (#3212)
    * lazy import docker, apache_beam
    
    * lazy load sqlalchemy import
    
    * black
    
    * fix typo
    
    * fix typo
    
    * fixes, add a test
    
    * improve comment
    
    * fixes
    
    * remove test, it's too flaky
    
    * Undo sqlalchemy changes
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ codalab/worker/un_tar_directory.py
@@ -0,0 +1,30 @@
+import os
+import tarfile
+
+
+def un_tar_directory(fileobj, directory_path, compression='', force=False):
+    """
+    Extracts the given file-like object containing a tar archive into the given
+    directory, which will be created and should not already exist. If it already exists,
+    and `force` is `False`, an error is raised. If it already exists, and `force` is `True`,
+    the directory is removed and recreated.
+
+    compression specifies the compression scheme and can be one of '', 'gz' or
+    'bz2'.
+
+    Raises tarfile.TarError if the archive is not valid.
+    """
+    directory_path = os.path.realpath(directory_path)
+    if force:
+        from codalab.worker.file_util import remove_path
+
+        remove_path(directory_path)
+    os.mkdir(directory_path)
+    with tarfile.open(fileobj=fileobj, mode='r|' + compression) as tar:
+        for member in tar:
+            # Make sure that there is no trickery going on (see note in
+            # TarFile.extractall() documentation.
+            member_path = os.path.realpath(os.path.join(directory_path, member.name))
+            if not member_path.startswith(directory_path):
+                raise tarfile.TarError('Archive member extracts outside the directory.')
+            tar.extract(member, directory_path)
