45814c2d0b0f1abf34d29fdd80bbe367b0962bb9
Yuqi Jin <34461466+yuqijin@users.noreply.github.com>
2021-03-08 12:52:24 -0800

    Update frontend/src/util/worksheet_utils.js
    Co-authored-by: Ashwin Ramaswami <aramaswamis@gmail.com>

--- frontend/src/util/worksheet_utils.js
+++ frontend/src/util/worksheet_utils.js
@@ -301,7 +301,7 @@ export function createHandleRedirectFn(worksheetUuid) {
 // Return the sort key at index subFocusIndex, if subFocusIndex is defined.
 // Otherwise, return the largest sort_key.
 export function getAfterSortKey(item, subFocusIndex) {
-    // the default after_sort_key is -1 when inserting an item on top of the worksheet
+    // The default after_sort_key is -1 when inserting an item on top of the worksheet,
     // so the items' sort_key should always >= 0 (sort_key > after_sort_key)
     if (!item) return -1;
     if (item.mode === 'image_block') {
