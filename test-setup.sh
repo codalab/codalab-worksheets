5717608d984c618d9a14565946d1e3b6340e71c4
Jizhen Wang <jzwang43@gmail.com>
2021-04-02 01:53:14 -0700

    change action

---
+++ test-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Run this script before start service to running link tests.
+
+mkdir -p /tmp/codalab/link-mounts/test
+echo 'hello world!' > /tmp/codalab/link-mounts/test.txt
+echo 'hello world!' > /tmp/codalab/link-mounts/test/test.txt
