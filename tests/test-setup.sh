82a730f3bb08ce20c6d5552b0b50eafbbbd81bfc
Jizhen Wang <jzwang43@gmail.com>
2021-04-02 14:10:55 -0700

    Update tests/test-setup.sh
    Co-authored-by: Ashwin Ramaswami <aramaswamis@gmail.com>

--- tests/test-setup.sh
+++ tests/test-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # When running tests, run this script before starting codalab with codalab_service.py.
+# This script is required for setup of the link tests.
 
 mkdir -p /tmp/codalab/link-mounts/test
 echo 'hello world!' > /tmp/codalab/link-mounts/test.txt
