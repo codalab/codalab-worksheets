5dfa6d66a431e941a62d6558acba1143032c9bc2
Tony Lee <tonyh.lee@yahoo.com>
2021-04-08 02:58:47 -0700

    test

--- codalab/lib/interactive_session.py
+++ codalab/lib/interactive_session.py
@@ -31,7 +31,7 @@ class InteractiveSession:
         9. Stop and remove the interactive session container.
     """
 
-    _BASH_HISTORY_CONTAINER_PATH = "/root/.bash_history"
+    _BASH_HISTORY_CONTAINER_PATH = "/usr/sbin/.bash_history"
     _CHOOSE_COMMAND_INSTRUCTIONS = (
         "\n\n#\n"
         "# Choose the commands to use for cl run:\n"
@@ -143,14 +143,13 @@ class InteractiveSession:
                 volumes[get_docker_path(key)] = dependency_local_path
 
         name = self._get_container_name()
-        # Start a container as a root user. Root (id = 0) is the default user within a container.
+        # Start a container as a non-root user.
         command = [
             'docker run',
             '-it',
             f'--name {name}',
             f'-w {os.path.sep}{self._session_uuid}',
-            # TODO: figure out a way to run as a non-root user and be able to save the bash_history on any machine
-            # '-u 1',
+            '-u 1',
         ]
         command.extend(
             [
