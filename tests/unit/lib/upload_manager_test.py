e0cf5c727f80edee5042cdd78fac6ea7f0c6727e
Ashwin Ramaswami <aramaswamis@gmail.com>
2021-04-17 21:40:53 -0400

    comment

--- tests/unit/lib/upload_manager_test.py
+++ tests/unit/lib/upload_manager_test.py
@@ -70,6 +70,9 @@ class UploadManagerTest(unittest.TestCase):
         self.check_file_contains_string(os.path.join(self.bundle_location, 'filename'), 'testing')
 
     def test_fileobj_single_tar_gz_with_dsstore_should_not_simplify_archive(self):
+        """If the user included two files, filename and .DS_Store, in the archive,
+        the archive should not be simplified because we have more than one file in the archive.
+        """
         source = os.path.join(self.temp_dir, 'source_dir')
         os.mkdir(source)
         self.write_string_to_file('testing', os.path.join(source, 'filename'))
