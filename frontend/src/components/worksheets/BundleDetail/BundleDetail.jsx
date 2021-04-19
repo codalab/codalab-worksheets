2fcd82462d141a99a412cad1bafd2e9348c0ddbf
yuqijin <yuqijin@stanford.edu>
2021-04-19 05:01:52 -0700

    Refactor BundleDetail to function component

--- frontend/src/components/worksheets/BundleDetail/BundleDetail.jsx
+++ frontend/src/components/worksheets/BundleDetail/BundleDetail.jsx
@@ -1,5 +1,6 @@
 // @flow
 import * as React from 'react';
+import {useEffect, useState} from 'react';
 import * as $ from 'jquery';
 // import Drawer from '@material-ui/core/Drawer';
 import { JsonApiDataStore } from 'jsonapi-datastore';
@@ -10,79 +11,57 @@ import BundleDetailSideBar from './BundleDetailSideBar';
 import BundleActions from './BundleActions';
 import {findDOMNode} from "react-dom";
 
-class BundleDetail extends React.Component<
-    {
-        uuid: string,
-        // Callback on metadata change.
-        bundleMetadataChanged: () => void,
-        onClose: () => void,
-        onOpen: () => void,
-    },
-    {
-        errorMessages: string[],
-        bundleInfo: {},
-        fileContents: string,
-        stdout: string,
-        stderr: string,
-    }
-> {
+const BundleDetail = ({ uuid,
+                          // Callback on metadata change.
+                          bundleMetadataChanged,
+    onClose,
+    onOpen,
+                          onUpdate,
+                          rerunItem, showNewRerun,
+                          showDetail, handleDetailClick,
+                          editPermission}) =>{
+    const [errorMessages, setErrorMessages] = useState([]);
+    const [bundleInfo, setBundleInfo] = useState(null);
+    const [fileContents, setFileContents] = useState(null);
+    const [stdout, setStdout] = useState(null);
+    const [stderr, setStderr] = useState(null);
+    const [prevUuid, setPrevUuid] = useState(uuid);
+    const [open, setOpen] = useState(true);
+    const [fetch, setFetch] = useState(true);
 
-    static getDerivedStateFromProps(props, state) {
-        // Any time the current bundle uuid changes,
-        // clear the error messages and not the actual contents, so that in
-        // the side panel, the page doesn't flicker.
-        if (props.uuid !== state.prevUuid) {
-            return {
-                prevUuid: props.uuid,
-                errorMessages: [],
-            };
+    useEffect(() => {
+        if (uuid !== prevUuid) {
+           setPrevUuid(uuid);
+           setErrorMessages([]);
         }
-        return null;
-    }
+    }, [ uuid ])
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            errorMessages: [],
-            bundleInfo: null,
-            fileContents: null,
-            stdout: null,
-            stderr: null,
-            prevUuid: props.uuid,
-            open: true,
-            fetch: true,
-        };
-    }
 
-    componentDidMount() {
-        this.fetchBundleMetaData();
-        this.fetchBundleContents();
-        this.timer = setInterval(()=>{ 
-            if (!this.state.bundleInfo){
+    useEffect(() => {
+        fetchBundleMetaData();
+        fetchBundleContents();
+        const timer = setInterval(() => {
+            if (!bundleInfo){
                 // If info is not available yet, fetch
-                this.fetchBundleMetaData();
-                this.fetchBundleContents();
+                fetchBundleMetaData();
+                fetchBundleContents();
                 return;
             }
-            if (this.state.bundleInfo.state.match("uploading|created|staged|making|starting|preparing|running|finalizing|worker_offline")){
+            if (bundleInfo.state.match("uploading|created|staged|making|starting|preparing|running|finalizing|worker_offline")){
                 // If bundle is in a state that is possible to transition to a different state, fetch data
                 // we have ignored ready|failed|killed states here
-                this.fetchBundleMetaData();
-                this.fetchBundleContents();
+                fetchBundleMetaData();
+                fetchBundleContents();
             } else{
                 // otherwise just clear the timer
-                clearInterval(this.timer);
+                clearInterval(timer);
+            }
+            if(onOpen){
+                onOpen();
             }
         }, 4000);
-        if(this.props.onOpen){
-            this.props.onOpen();
-        }
-    }
-
-    componentWillUnmount() {
-        // Clear the timer
-        clearInterval(this.timer);
-    }
+        return () => clearInterval(timer);
+    }, []);
 
     /**
      * Return a Promise to fetch the summary of the given file.
@@ -90,7 +69,7 @@ class BundleDetail extends React.Component<
      * @param path  path within the bundle
      * @return  jQuery Deferred object
      */
-    fetchFileSummary(uuid, path) {
+    const fetchFileSummary = (uuid, path)=> {
         return $.ajax({
             type: 'GET',
             url: '/rest/bundles/' + uuid + '/contents/blob' + path,
@@ -105,9 +84,8 @@ class BundleDetail extends React.Component<
         });
     }
 
-    fetchBundleMetaData = () => {
-        const { uuid } = this.props;
 
+    const fetchBundleMetaData = () => {
         $.ajax({
             type: 'GET',
             url: '/rest/bundles/' + uuid,
@@ -123,22 +101,18 @@ class BundleDetail extends React.Component<
             const bundleInfo = new JsonApiDataStore().sync(response);
             bundleInfo.editableMetadataFields = response.data.meta.editable_metadata_keys;
             bundleInfo.metadataType = response.data.meta.metadata_type;
-            this.setState({ bundleInfo });
+            setBundleInfo( bundleInfo );
         }).fail(function(xhr, status, err) {
-            this.setState({
-                bundleInfo: null,
-                fileContents: null,
-                stdout: null,
-                stderr: null,
-                errorMessages: this.state.errorMessages.concat([xhr.responseText]),
-            });
+           setBundleInfo(null);
+           setFileContents(null);
+           setStderr(null);
+           setStdout(null);
+           setErrorMessages(errorMessages=>errorMessages.concat([xhr.responseText]))
         });
     }
 
     // Fetch bundle contents
-    fetchBundleContents = () => {
-        const { uuid } = this.props;
-
+    const fetchBundleContents = () => {
         $.ajax({
             type: 'GET',
             url: '/rest/bundles/' + uuid + '/contents/info/',
@@ -152,8 +126,10 @@ class BundleDetail extends React.Component<
             const info = response.data;
             if (!info) return;
             if (info.type === 'file' || info.type === 'link') {
-                return this.fetchFileSummary(uuid, '/').then(function(blob) {
-                    this.setState({ fileContents: blob, stdout: null, stderr: null });
+                return fetchFileSummary(uuid, '/').then(function(blob) {
+                   setFileContents(blob);
+                    setStderr(null);
+                    setStdout(null);
                 });
             } else if (info.type === 'directory') {
                 // Get stdout/stderr (important to set things to null).
@@ -165,7 +141,7 @@ class BundleDetail extends React.Component<
                     function(name) {
                         if (info.contents.some((entry) => entry.name === name)) {
                             fetchRequests.push(
-                                this.fetchFileSummary(uuid, '/' + name).then(
+                                fetchFileSummary(uuid, '/' + name).then(
                                     function(blob) {
                                         stateUpdate[name] = blob;
                                     },
@@ -174,77 +150,71 @@ class BundleDetail extends React.Component<
                         } else {
                             stateUpdate[name] = null;
                         }
-                    }.bind(this),
+                    },
                 );
                 await Promise.all(fetchRequests);
-                this.setState(stateUpdate);
+                setFileContents(stateUpdate['fileContents']);
+                if('stdout' in stateUpdate){setStdout(stateUpdate['stdout'])}
+                if('stderr' in stateUpdate){setStderr(stateUpdate['stderr'])}
             }
         }).fail(function(xhr, status, err) {
             // 404 Not Found errors are normal if contents aren't available yet, so ignore them
             if (xhr.status !== 404) {
-                this.setState({
-                    bundleInfo: null,
-                    fileContents: null,
-                    stdout: null,
-                    stderr: null,
-                    errorMessages: this.state.errorMessages.concat([xhr.responseText]),
-                });
+                setBundleInfo(null);
+                setFileContents(null);
+                setStderr(null);
+                setStdout(null);
+                setErrorMessages(errorMessages=>errorMessages.concat([xhr.responseText]));
             } else {
                 // If contents aren't available yet, then also clear stdout and stderr.
-                this.setState({ fileContents: null, stdout: null, stderr: null });
+                setFileContents(null);
+                setStderr(null);
+                setStdout(null);
             }
         });
     }
-  
-    render(): React.Node {
-        const { bundleMetadataChanged,
-            onUpdate,
-            rerunItem, showNewRerun,
-            showDetail, handleDetailClick,
-            editPermission } = this.props;
-        const {
-            bundleInfo,
-            stdout,
-            stderr,
-            fileContents } = this.state;
-        if (!bundleInfo){
-            return <div></div>
-        }
-        if (bundleInfo.bundle_type === 'private') {
-            return <div>Detail not available for this bundle</div>
-        }
 
-        return (
-            <ConfigurationPanel
-                //  The ref is created only once, and that this is the only way to properly create the ref before componentDidMount().
-                ref={(node) => this.scrollToNewlyOpenedDetail(node)}
-                buttons={ <BundleActions
-                    showNewRerun={showNewRerun}
-                    showDetail={showDetail}
-                    handleDetailClick={handleDetailClick}
-                    bundleInfo={ bundleInfo }
-                    rerunItem={ rerunItem }
-                    onComplete={ bundleMetadataChanged }
-                    editPermission={editPermission} /> }
-                sidebar={ <BundleDetailSideBar bundleInfo={ bundleInfo } onUpdate={ onUpdate } onMetaDataChange={ this.fetchBundleMetaData } /> }
-            >
-                <MainContent
-                    bundleInfo={ bundleInfo }
-                    stdout={ stdout }
-                    stderr={ stderr }
-                    fileContents={ fileContents }
-                />
-            </ConfigurationPanel>
-        );
-  }
-    scrollToNewlyOpenedDetail(node) {
+   const  scrollToNewlyOpenedDetail=(node)=>{
         // Only scroll to the bundle detail when it is opened
-        if (node && this.state.open) {
+        if (node && open) {
             findDOMNode(node).scrollIntoView({block:'center'});
             // Avoid undesirable scroll
-            this.setState({open:false})
+            setOpen(false);
         }
     }
+
+    if (!bundleInfo){
+        return <div></div>
+    }
+    if (bundleInfo.bundle_type === 'private') {
+        return <div>Detail not available for this bundle</div>
+    }
+
+    return (
+        <ConfigurationPanel
+            //  The ref is created only once, and that this is the only way to properly create the ref before componentDidMount().
+            ref={(node) => scrollToNewlyOpenedDetail(node)}
+            buttons={ <BundleActions
+                showNewRerun={showNewRerun}
+                showDetail={showDetail}
+                handleDetailClick={handleDetailClick}
+                bundleInfo={ bundleInfo }
+                rerunItem={ rerunItem }
+                onComplete={ bundleMetadataChanged }
+                editPermission={editPermission} /> }
+            sidebar={ <BundleDetailSideBar bundleInfo={ bundleInfo } onUpdate={ onUpdate } onMetaDataChange={ fetchBundleMetaData } /> }
+        >
+            <MainContent
+                bundleInfo={ bundleInfo }
+                stdout={ stdout }
+                stderr={ stderr }
+                fileContents={ fileContents }
+            />
+        </ConfigurationPanel>
+    );
+
+
 }
+
 
 export default BundleDetail;
