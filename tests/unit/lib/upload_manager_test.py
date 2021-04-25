1c1c0f165c1e4b931c32989cf08c5690ff01d7af
Ashwin Ramaswami <aramaswamis@gmail.com>
2021-04-25 17:33:09 -0400

    fix test

--- tests/unit/lib/upload_manager_test.py
+++ tests/unit/lib/upload_manager_test.py
@@ -78,7 +78,7 @@ class UploadManagerTest(unittest.TestCase):
         self.write_string_to_file('testing', os.path.join(source, 'README'))
         self.write_string_to_file('testing', os.path.join(source, '.DS_Store'))
         self.do_upload([('source.tar.gz', tar_gzip_directory(source))])
-        self.assertEqual(['.DS_Store', 'README'], os.listdir(self.bundle_location))
+        self.assertEqual(['.DS_Store', 'README'], sorted(os.listdir(self.bundle_location)))
 
     def test_fileobj_single_tar_gz_with_dsstore_should_not_simplify_archive_2(self):
         """If the user included three files, README, README2, and .DS_Store, in the archive,
@@ -90,7 +90,7 @@ class UploadManagerTest(unittest.TestCase):
         self.write_string_to_file('testing', os.path.join(source, 'README2'))
         self.write_string_to_file('testing', os.path.join(source, '.DS_Store'))
         self.do_upload([('source.tar.gz', tar_gzip_directory(source))])
-        self.assertEqual(['.DS_Store', 'README2', 'README'], os.listdir(self.bundle_location))
+        self.assertEqual(['.DS_Store', 'README', 'README2'], sorted(os.listdir(self.bundle_location)))
 
     def mock_url_sources(self, fileobj, ext=""):
         """Returns a URL that is mocked to return the contents of fileobj.
