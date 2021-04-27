455a83f82f72be926c9d5cc13021f6eec78d2773
Ashwin Ramaswami <aramaswamis@gmail.com>
2021-04-27 08:15:12 -0400

    Remove support for uploads of multiple URLs (#3432)
    * Simplify uploads by removing local path support, exclude patterns
    
    * format
    
    * update
    
    * update tests, shallow clone
    
    * revert git
    
    * comment
    
    * add test
    
    * comment
    
    * fix tests
    
    * more tests
    
    * simplify
    
    * delete
    
    * typing
    
    * add some typing
    
    * add more types
    
    * Update upload_manager.py
    
    * type annotation
    
    * fix flake8
    
    * fix typing
    
    * Remove support for uploads of multiple URLs
    
    * Add test
    
    * fix test
    
    * update
    
    * fix test
    
    * fix test
    
    * black
    
    * black
    
    * fix test
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- tests/unit/lib/upload_manager_test.py
+++ tests/unit/lib/upload_manager_test.py
@@ -35,7 +35,7 @@ class UploadManagerTest(unittest.TestCase):
         remove_path(self.temp_dir)
 
     def do_upload(
-        self, sources, git=False, unpack=True, simplify_archives=True, use_azure_blob_beta=False,
+        self, source, git=False, unpack=True, simplify_archives=True, use_azure_blob_beta=False,
     ):
         class FakeBundle(object):
             def __init__(self):
@@ -43,33 +43,33 @@ class UploadManagerTest(unittest.TestCase):
                 self.metadata = object()
 
         self.manager.upload_to_bundle_store(
-            FakeBundle(), sources, git, unpack, simplify_archives, use_azure_blob_beta,
+            FakeBundle(), source, git, unpack, simplify_archives, use_azure_blob_beta,
         )
 
-    def test_fileobj_single(self):
-        self.do_upload([('source', BytesIO(b'testing'))])
+    def test_fileobj(self):
+        self.do_upload(('source', BytesIO(b'testing')))
         self.check_file_contains_string(self.bundle_location, 'testing')
 
-    def test_fileobj_single_gz(self):
-        self.do_upload([('source.gz', BytesIO(gzip_bytestring(b'testing')))])
+    def test_fileobj_gz(self):
+        self.do_upload(('source.gz', BytesIO(gzip_bytestring(b'testing'))))
         self.check_file_contains_string(self.bundle_location, 'testing')
 
-    def test_fileobj_single_tar_gz_simplify_archives(self):
+    def test_fileobj_tar_gz_simplify_archives(self):
         source = os.path.join(self.temp_dir, 'source_dir')
         os.mkdir(source)
         self.write_string_to_file('testing', os.path.join(source, 'filename'))
-        self.do_upload([('source.tar.gz', tar_gzip_directory(source))])
+        self.do_upload(('source.tar.gz', tar_gzip_directory(source)))
         self.check_file_contains_string(self.bundle_location, 'testing')
 
-    def test_fileobj_single_tar_gz_no_simplify_archives(self):
+    def test_fileobj_tar_gz_no_simplify_archives(self):
         source = os.path.join(self.temp_dir, 'source_dir')
         os.mkdir(source)
         self.write_string_to_file('testing', os.path.join(source, 'filename'))
-        self.do_upload([('source.tar.gz', tar_gzip_directory(source))], simplify_archives=False)
+        self.do_upload(('source.tar.gz', tar_gzip_directory(source)), simplify_archives=False)
         self.assertEqual(['filename'], os.listdir(self.bundle_location))
         self.check_file_contains_string(os.path.join(self.bundle_location, 'filename'), 'testing')
 
-    def test_fileobj_single_tar_gz_with_dsstore_should_not_simplify_archive(self):
+    def test_fileobj_tar_gz_with_dsstore_should_not_simplify_archive(self):
         """If the user included two files, README and .DS_Store, in the archive,
         the archive should not be simplified because we have more than one file in the archive.
         """
@@ -77,10 +77,10 @@ class UploadManagerTest(unittest.TestCase):
         os.mkdir(source)
         self.write_string_to_file('testing', os.path.join(source, 'README'))
         self.write_string_to_file('testing', os.path.join(source, '.DS_Store'))
-        self.do_upload([('source.tar.gz', tar_gzip_directory(source))])
+        self.do_upload(('source.tar.gz', tar_gzip_directory(source)))
         self.assertEqual(['.DS_Store', 'README'], sorted(os.listdir(self.bundle_location)))
 
-    def test_fileobj_single_tar_gz_with_dsstore_should_not_simplify_archive_2(self):
+    def test_fileobj_tar_gz_with_dsstore_should_not_simplify_archive_2(self):
         """If the user included three files, README, README2, and .DS_Store, in the archive,
         the archive should not be simplified because we have more than one file in the archive.
         """
@@ -89,12 +89,12 @@ class UploadManagerTest(unittest.TestCase):
         self.write_string_to_file('testing', os.path.join(source, 'README'))
         self.write_string_to_file('testing', os.path.join(source, 'README2'))
         self.write_string_to_file('testing', os.path.join(source, '.DS_Store'))
-        self.do_upload([('source.tar.gz', tar_gzip_directory(source))])
+        self.do_upload(('source.tar.gz', tar_gzip_directory(source)))
         self.assertEqual(
             ['.DS_Store', 'README', 'README2'], sorted(os.listdir(self.bundle_location))
         )
 
-    def mock_url_sources(self, fileobj, ext=""):
+    def mock_url_source(self, fileobj, ext=""):
         """Returns a URL that is mocked to return the contents of fileobj.
         The URL will end in the extension "ext", if given.
         """
@@ -103,10 +103,10 @@ class UploadManagerTest(unittest.TestCase):
         fileobj.seek(0)
         urllib.request.urlopen = MagicMock()
         urllib.request.urlopen.return_value = addinfourl(fileobj, {"content-length": size}, url)
-        return [url]
+        return url
 
-    def test_url_single(self):
-        self.do_upload(self.mock_url_sources(BytesIO(b'hello world')))
+    def test_url(self):
+        self.do_upload(self.mock_url_source(BytesIO(b'hello world')))
         self.check_file_contains_string(self.bundle_location, 'hello world')
 
     def test_url_tar_gz(self):
@@ -115,27 +115,12 @@ class UploadManagerTest(unittest.TestCase):
         self.write_string_to_file('testing', os.path.join(source, 'file1'))
         self.write_string_to_file('testing', os.path.join(source, 'file2'))
         self.do_upload(
-            self.mock_url_sources(BytesIO(tar_gzip_directory(source).read()), ext=".tar.gz")
+            self.mock_url_source(BytesIO(tar_gzip_directory(source).read()), ext=".tar.gz")
         )
         self.assertIn('file2', os.listdir(self.bundle_location))
 
-    def test_multiple_sources(self):
-        self.do_upload(
-            [
-                ('source1', BytesIO(b'testing1')),
-                ('source2', BytesIO(b'testing2')),
-                'http://alpha.gnu.org/gnu/bc/bc-1.06.95.tar.bz2',
-            ]
-        )
-        self.assertEqual(
-            ['bc-1.06.95', 'source1', 'source2'], sorted(os.listdir(self.bundle_location))
-        )
-        self.check_file_contains_string(os.path.join(self.bundle_location, 'source1'), 'testing1')
-        self.check_file_contains_string(os.path.join(self.bundle_location, 'source2'), 'testing2')
-        self.assertIn('README', os.listdir(os.path.join(self.bundle_location, 'bc-1.06.95')))
-
     def test_url_git(self):
-        self.do_upload(['https://github.com/codalab/test'], git=True)
+        self.do_upload('https://github.com/codalab/test', git=True)
 
     def write_string_to_file(self, string, file_path):
         with open(file_path, 'w') as f:
