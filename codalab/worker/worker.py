27972278f645ff4f72e514effb5143c9a2081d18
Aditya Prerepa <adiprerepa@gmail.com>
2021-05-06 15:04:45 -0700

    update comment with regard to @epicfaace's comment

--- codalab/worker/worker.py
+++ codalab/worker/worker.py
@@ -476,11 +476,9 @@ class Worker:
         for uuid in self.runs:
             prev_state = self.runs[uuid]
             self.runs[uuid] = self.run_state_manager.transition(prev_state)
-            # it's possible that a bundle can return to the same stage, so we don't want to reset that initial stage.
-            # to prevent this, ensure we are actually going to a new stage as opposed to reiterating on an old one.
+            # Only start saving stats for a new stage when the run has actually transitioned to that stage.
             if prev_state.stage != self.runs[uuid].stage:
                 self.end_stage_stats(uuid, prev_state.stage)
-                # only start stages that are not endpoints
                 if self.runs[uuid].stage != RunStage.FINISHED:
                     self.start_stage_stats(uuid, self.runs[uuid].stage)
 
