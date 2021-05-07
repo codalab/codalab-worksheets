4f0c77bbdd411eb22ea7e12e9bf22beda08059d8
Ashwin Ramaswami <aramaswamis@gmail.com>
2021-05-07 11:26:11 -0400

    more performant version

--- codalab/lib/beam/blobstorageuploader.py
+++ codalab/lib/beam/blobstorageuploader.py
@@ -8,41 +8,41 @@ from apache_beam.io.azure.blobstorageio import parse_azfs_path
 import tempfile
 import io
 import base64
+from codalab.worker.un_gzip_stream import BytesBuffer
 
 class BlobStorageUploader(Uploader):
+  """An improved version of apache_beam.io.azure.blobstorageio.BlobStorageUploader
+  that handles multipart streaming (block-by-block) uploads.
+  TODO (Ashwin): contribute this back upstream to Apache Beam.
+  """
   def __init__(self, client, path, mime_type='application/octet-stream'):
     self._client = client
     self._path = path
     self._container, self._blob = parse_azfs_path(path)
     self._content_settings = ContentSettings(mime_type)
-    
 
     self._blob_to_upload = self._client.get_blob_client(
         self._container, self._blob)
 
     self.block_number = 1
-    self.buffer = b''
+    self.buffer = BytesBuffer()
     self.block_list = []
 
   def put(self, data):
-
-    # Note that Blob Storage currently can hold a maximum of 100,000 uncommitted blobs.
+    # Note that Blob Storage currently can hold a maximum of 100,000 uncommitted blocks.
     # This means that with this current implementation, we can upload a file with a maximum
     # size of 10 TiB to Blob Storage. To exceed that limit, we must either increase MIN_WRITE_SIZE
     # or modify the implementation of this class to call commit_block_list more often (and not
-    # just at the end of the upload).
+    # just at the end of the upload). 
     MIN_WRITE_SIZE = 100 * 1024 * 1024
     MAX_WRITE_SIZE = 2 * 1024 * 1024 * 1024
 
-    # TODO: Byte strings might not be the most performant way to handle this
-    self.buffer += data.tobytes()
+    self.buffer.write(data.tobytes())
 
     while len(self.buffer) >= MIN_WRITE_SIZE:
-      # Take the first chunk off the buffer and write it to S3
-      chunk = self.buffer[:MAX_WRITE_SIZE]
+      # Take the first chunk off the buffer and write it to Blob Storage
+      chunk = self.buffer.read(MAX_WRITE_SIZE)
       self._write_to_blob(chunk)
-      # Remove the written chunk from the buffer
-      self.buffer = self.buffer[MAX_WRITE_SIZE:]
 
   def _write_to_blob(self, data):
     # block_id's have to be base-64 strings normalized to have the same length.
@@ -78,6 +78,7 @@ if __name__ == '__main__':
         "BlobEndpoint=http://localhost:10000/devstoreaccount1;"
     )
     from codalab.lib.beam.filesystems import client
+    from apache_beam.io.filesystems import FileSystems
     try:
         client.create_container("bundles")
         print("Created initial Azure Blob Storage container \"bundles\".")
@@ -86,7 +87,9 @@ if __name__ == '__main__':
     LOCAL_PATH = '/Users/epicfaace/Downloads/googlechrome.dmg'
     BLOB_PATH = "azfs://devstoreaccount1/bundles/test.txt"
     uploader = BlobStorageUploader(client, BLOB_PATH)
-    with io.BufferedWriter(UploaderStream(uploader, mode="wb"), buffer_size=128 * 1024) as f, open(LOCAL_PATH, 'rb') as inp_f:
+    with FileSystems.create(
+        BLOB_PATH
+    ) as f, open(LOCAL_PATH, 'rb') as inp_f:
         shutil.copyfileobj(inp_f, f)
     with BlobStorageIO(client).open(BLOB_PATH) as f, tempfile.NamedTemporaryFile() as tf:
         shutil.copyfileobj(f, tf)
