821e81c58092ced460ffc165843cbb7c61015ce4
Ashwin Ramaswami <aramaswamis@gmail.com>
2021-05-06 21:36:54 -0400

    implement multipart upload for blobs

--- codalab/lib/beam/blobstorageuploader.py
+++ codalab/lib/beam/blobstorageuploader.py
@@ -2,10 +2,12 @@ from apache_beam.io.filesystemio import Uploader, UploaderStream
 from azure.storage.blob import (
     BlobServiceClient,
     ContentSettings,
+    BlobBlock,
 )
 from apache_beam.io.azure.blobstorageio import parse_azfs_path
 import tempfile
 import io
+import base64
 
 class BlobStorageUploader(Uploader):
   def __init__(self, client, path, mime_type='application/octet-stream'):
@@ -13,24 +15,42 @@ class BlobStorageUploader(Uploader):
     self._path = path
     self._container, self._blob = parse_azfs_path(path)
     self._content_settings = ContentSettings(mime_type)
+    
 
     self._blob_to_upload = self._client.get_blob_client(
         self._container, self._blob)
 
-    # Temporary file.
-    self._temporary_file = tempfile.NamedTemporaryFile()
+    self.block_number = 1
+    self.buffer = b''
+    self.block_list = []
 
   def put(self, data):
-    print("put", len(data), "bytes")
-    self._temporary_file.write(data.tobytes())
+
+    MIN_WRITE_SIZE = 100 * 1024 * 1024
+    MAX_WRITE_SIZE = 2 * 1024 * 1024 * 1024
+    print("put", len(data))
+
+    # TODO: Byte strings might not be the most performant way to handle this
+    self.buffer += data.tobytes()
+
+    while len(self.buffer) >= MIN_WRITE_SIZE:
+      # Take the first chunk off the buffer and write it to S3
+      chunk = self.buffer[:MAX_WRITE_SIZE]
+      self._write_to_blob(chunk)
+      # Remove the written chunk from the buffer
+      self.buffer = self.buffer[MAX_WRITE_SIZE:]
+
+  def _write_to_blob(self, data):
+    # block_id's have to be base-64 strings normalized to have the same length.
+    block_id = base64.b64encode('{0:-32d}'.format(self.block_number).encode()).decode()
+    
+    self._blob_to_upload.stage_block(block_id, data)
+    self.block_list.append(BlobBlock(block_id))
+    self.block_number = self.block_number + 1
 
   def finish(self):
-    print("finally uploading blob")
-    self._temporary_file.seek(0)
-    # The temporary file is deleted immediately after the operation.
-    with open(self._temporary_file.name, "rb") as f:
-      self._blob_to_upload.upload_blob(
-          f.read(), overwrite=True, content_settings=self._content_settings)
+    self._write_to_blob(self.buffer)
+    self._blob_to_upload.commit_block_list(self.block_list, content_settings=self._content_settings)
 
 # To run test, run:
 # python codalab/lib/beam/blobstorageuploader.py
@@ -55,4 +75,5 @@ if __name__ == '__main__':
     with BlobStorageIO(client).open(BLOB_PATH) as f, tempfile.NamedTemporaryFile() as tf:
         shutil.copyfileobj(f, tf)
         result = filecmp.cmp(PATH, tf.name)
-        print(result)
+        assert result == True
+        print("done")
