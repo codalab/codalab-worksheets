2020444df173446c975b09e6108753a1c79b3e47
Ashwin Ramaswami <aramaswamis@gmail.com>
2021-05-09 21:32:54 -0400

    add tell function to BytesBuffer

--- codalab/worker/un_gzip_stream.py
+++ codalab/worker/un_gzip_stream.py
@@ -241,6 +241,7 @@ class BytesBuffer(BytesIO):
     def __init__(self):
         self.__buf = deque()
         self.__size = 0
+        self.__pos = 0
 
     def __len__(self):
         return self.__size
@@ -262,6 +263,7 @@ class BytesBuffer(BytesIO):
             self.__buf.appendleft(remainder)
         ret = b''.join(ret_list)
         self.__size -= len(ret)
+        self.__pos += len(ret)
         return ret
 
     def flush(self):
@@ -269,6 +271,9 @@ class BytesBuffer(BytesIO):
 
     def close(self):
         pass
+
+    def tell(self):
+        return self.__pos
 
     def __bool__(self):
         return True
