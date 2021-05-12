d913f1c4d831505134e7650aac35e94204fc9936
Aditya Prerepa <adiprerepa@gmail.com>
2021-05-12 10:18:23 -0700

    Update codalab/worker/worker_run_state.py
    Co-authored-by: Ashwin Ramaswami <aramaswamis@gmail.com>

--- codalab/worker/worker_run_state.py
+++ codalab/worker/worker_run_state.py
@@ -467,7 +467,7 @@ class RunStateMachine(StateTransitioner):
             cpu_usage, memory_usage = docker_utils.get_container_stats_with_docker_stats(
                 run_state.container
             )
-            run_state = run_state._replace(cpu_usage=cpu_usage)
+            run_state = run_state._replace(cpu_usage=cpu_usage, memory_usage=memory_usage)
             run_state = run_state._replace(memory_usage=memory_usage)
 
             kill_messages = []
