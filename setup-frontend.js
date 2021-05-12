d99f8eff40f1600a1e194338fb90ab7fe1e5bd35
Jizhen Wang <jzwang43@gmail.com>
2021-05-11 17:41:51 -0700

    optimize env reading

--- setup-frontend.js
+++ setup-frontend.js
@@ -1,8 +1,8 @@
-const FRONTEND_ARGS = ["CODALAB_RECAPTCHA_SITE_KEY"];
+const FRONTEND_ARGS = ["REACT_APP_CODALAB_RECAPTCHA_SITE_KEY"];
 
 fs = require("fs");
 vars = FRONTEND_ARGS.map(
-  (arg) => `window.env.REACT_APP_${arg}="${process.env[arg]}"`
+  (arg) => `window.env.${arg}="${process.env[arg]}"`
 );
 
 fs.writeFileSync("build/js/env.js", "window.env = {}\n" + vars.join("\n"));
