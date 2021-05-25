f8502fda0bda889a0e3b0e237a0358ac396564eb
yuqijin <yuqijin@stanford.edu>
2021-05-25 07:57:46 -0700

    fix frontend bundle detail bug

--- frontend/src/components/FileBrowser/FileBrowser.js
+++ frontend/src/components/FileBrowser/FileBrowser.js
@@ -487,15 +487,9 @@ export const FileBrowserLite = ({ uuid, startCollapsed, isRunningBundle, bundle_
     );
 
     useEffect(() => {
-        if (!startCollapsed) {
-            updateFileBrowser('');
-        }
-    }, [startCollapsed, updateFileBrowser]);
-
-    useEffect(() => {
         setFileBrowserData({});
         updateFileBrowser('');
-    }, [updateFileBrowser, uuid]);
+    }, [uuid]);
 
     useEffect(() => {
         if (cache.has(url)) {
