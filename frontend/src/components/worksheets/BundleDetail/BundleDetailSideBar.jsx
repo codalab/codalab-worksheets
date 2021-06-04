7414b73e61dbbf84169c848328a13ad506915487
Yuqi Jin <34461466+yuqijin@users.noreply.github.com>
2021-06-04 14:30:31 -0700

    multiline command rendering (#3574)
    Co-authored-by: Ashwin Ramaswami <aramaswamis@gmail.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- frontend/src/components/worksheets/BundleDetail/BundleDetailSideBar.jsx
+++ frontend/src/components/worksheets/BundleDetail/BundleDetailSideBar.jsx
@@ -160,7 +160,7 @@ class BundleDetailSideBar extends React.Component<
                                 </div>        
                         </CopyToClipboard>
                         <div style={{  flexWrap: 'wrap', flexShrink: 1}}>
-                            {bundleInfo.command}
+                             <pre>{bundleInfo.command} </pre>
                         </div>
                     </Grid>
                 }
