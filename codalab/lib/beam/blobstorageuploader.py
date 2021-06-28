846decd4fa721c4efd630e35ea894e0df5f24023
Ashwin Ramaswami <aramaswamis@gmail.com>
2021-06-27 22:12:07 -0500

    Update blobstorageuploader.py

--- codalab/lib/beam/blobstorageuploader.py
+++ codalab/lib/beam/blobstorageuploader.py
@@ -1,12 +1,9 @@
-from apache_beam.io.filesystemio import Uploader, UploaderStream
+from apache_beam.io.filesystemio import Uploader
 from azure.storage.blob import (
-    BlobServiceClient,
     ContentSettings,
     BlobBlock,
 )
 from apache_beam.io.azure.blobstorageio import parse_azfs_path
-import tempfile
-import io
 import base64
 from codalab.worker.un_gzip_stream import BytesBuffer
 
@@ -56,44 +53,3 @@ class BlobStorageUploader(Uploader):
   def finish(self):
     self._write_to_blob(self.buffer)
     self._blob_to_upload.commit_block_list(self.block_list, content_settings=self._content_settings)
-
-"""
-Below is a quick manual integration test / sanity check for BlobStorageUploader
-that can be run from the command line. To run it, first set the LOCAL_PATH variable
-to a local path to a > 100 MB file, then run:
-    npm i -g azurite
-    azurite
-    python codalab/lib/beam/blobstorageuploader.py
-"""
-
-if __name__ == '__main__':
-    from azure.core.exceptions import ResourceExistsError
-    from apache_beam.io.azure.blobstorageio import BlobStorageIO
-    import filecmp
-    import os
-    import shutil
-    
-    os.environ["CODALAB_AZURE_BLOB_CONNECTION_STRING"] = (
-        "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;"
-        "AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;"
-        "BlobEndpoint=http://localhost:10000/devstoreaccount1;"
-    )
-    from codalab.lib.beam.filesystems import client
-    from apache_beam.io.filesystems import FileSystems
-    try:
-        client.create_container("bundles")
-        print("Created initial Azure Blob Storage container \"bundles\".")
-    except ResourceExistsError:
-        pass
-    LOCAL_PATH = '/Users/epicfaace/Downloads/googlechrome.dmg'
-    BLOB_PATH = "azfs://devstoreaccount1/bundles/test.txt"
-    uploader = BlobStorageUploader(client, BLOB_PATH)
-    with FileSystems.create(
-        BLOB_PATH
-    ) as f, open(LOCAL_PATH, 'rb') as inp_f:
-        shutil.copyfileobj(inp_f, f)
-    with BlobStorageIO(client).open(BLOB_PATH) as f, tempfile.NamedTemporaryFile() as tf:
-        shutil.copyfileobj(f, tf)
-        result = filecmp.cmp(LOCAL_PATH, tf.name)
-        assert result == True
-        print("done")
