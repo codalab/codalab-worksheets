3850ef7ca11405861f6d83859a29191eba988b34
Ashwin Ramaswami <aramaswamis@gmail.com>
2021-08-21 21:57:20 +0000

    fix

--- codalab/rest/bundles.py
+++ codalab/rest/bundles.py
@@ -611,7 +611,7 @@ def _fetch_bundle_contents_blob(uuid, path=''):
     - `support_redirect`: Set to 1 if the client supports bypassing the server
       and redirecting to another URL (such as Blob Storage). If so, the Target-Type and
       X-CodaLab-Target-Size headers will not be present in the response.
-      
+
       If this endpoint is called from a web browser (`Referer` header is set), this parameter
       defaults to 1. Otherwise, it defaults to 0, meant for compatibility
       with older clients / CLI versions that depend on the Target-Type and
