3d0d0c21b2b4a91258a734b8c77b9f9d35bc1c8a
Aditya Prerepa <adiprerepa@gmail.com>
2021-08-25 22:56:04 -0700

    docstrings

--- codalab/worker/bundle_container.py
+++ codalab/worker/bundle_container.py
@@ -9,18 +9,35 @@ class BundleContainer:
         pass
 
     def check_finished(self):
+        """
+        check_finished will check if a container has finished running.
+        """
         raise NotImplementedError
 
     def get_container_stats_with_stats_api(self):
+        """
+        get_container_stats_with_stats_api will get a container's run statistics
+        from the container statistics API, if there is one.
+        """
         raise NotImplementedError
 
     def get_container_stats_native(self):
+        """
+        get_container_stats_native gets the statistics of a container's run through means other
+        than the container API. An instance of this is cgroups.
+        """
         raise NotImplementedError
 
     def container_exists(self):
+        """
+        container_exists checks whether a given container exists in the interface's container store.
+        """
         raise NotImplementedError
 
     def get_container_running_time(self):
+        """
+        get_container_running_time gets the time it took for a container to run.
+        """
         raise NotImplementedError
 
 class BundleRunner:
@@ -38,4 +55,7 @@ class BundleRunner:
             detach=True,
             tty=False,
             runtime=DEFAULT_RUNTIME):
+        """
+        run will run a bundle given these run parameters.
+        """
         raise NotImplementedError
