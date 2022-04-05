92a0b4373dbce26ad0342afc3c41056253fddb4a
Jiani Wang <wjn9424@163.com>
2022-04-05 16:31:16 -0700

    modify .value

--- codalab/lib/bundle_cli.py
+++ codalab/lib/bundle_cli.py
@@ -1057,17 +1057,17 @@ class BundleCLI(object):
                 "authentication": args.authentication,
             }
             if args.url is not None:
-                if args.url.startswith(StorageURLScheme.AZURE_BLOB_STORAGE):
+                if args.url.startswith(StorageURLScheme.AZURE_BLOB_STORAGE.value):
                     if args.storage_type is None:
-                        bundle_store_info["storage_type"] = StorageType.AZURE_BLOB_STORAGE
-                    elif args.storage_type != StorageType.AZURE_BLOB_STORAGE:
+                        bundle_store_info["storage_type"] = StorageType.AZURE_BLOB_STORAGE.value
+                    elif args.storage_type != StorageType.AZURE_BLOB_STORAGE.value:
                         raise UsageError(
                             f"cl store storage type '{args.storage_format}' conflicts with storage format."
                         )
-                elif args.url.startswith(StorageURLScheme.GCS_STORAGE):
+                elif args.url.startswith(StorageURLScheme.GCS_STORAGE.value):
                     if args.storage_type is None:
-                        bundle_store_info["storage_type"] = StorageType.GCS_STORAGE
-                    elif args.storage_type != StorageType.GCS_STORAGE:
+                        bundle_store_info["storage_type"] = StorageType.GCS_STORAGE.value
+                    elif args.storage_type != StorageType.GCS_STORAGE.value:
                         raise UsageError(
                             f"cl store storage type '{args.storage_format}' conflicts with storage format."
                         )
