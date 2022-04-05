05f78a733c0d82f038bbc81a4e456cd575a6372b
Jiani Wang <wjn9424@163.com>
2022-04-05 12:13:36 -0700

    checkout bundles.py

--- codalab/rest/bundles.py
+++ codalab/rest/bundles.py
@@ -511,7 +511,7 @@ def _add_bundle_store():
     new_bundle_store = BundleStoreSchema(strict=True, many=True).load(request.json).data[0]
     storage_type = new_bundle_store.get('storage_type')
     storage_format = new_bundle_store.get('storage_format')
-    if storage_format is None: 
+    if storage_format is None:
         if storage_type in (StorageType.AZURE_BLOB_STORAGE.value, StorageType.GCS_STORAGE.value):
             storage_format = StorageFormat.COMPRESSED_V1.value
         elif storage_type == StorageType.DISK_STORAGE.value:
