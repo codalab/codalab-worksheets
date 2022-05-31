aed2aa0b9340a58f5204788c7ff1e6ef11e9d79f
Ashwin Ramaswami <aramaswamis@gmail.com>
2022-05-31 19:08:04 +0000

    Remove defaults for --worker-tag for k8s worker managers, Make sure --worker-tag-exclusive is only applied when --worker-tag is set

--- codalab/worker/main.py
+++ codalab/worker/main.py
@@ -156,7 +156,7 @@ def parse_args():
     parser.add_argument(
         '--tag-exclusive',
         action='store_true',
-        help='To be used when the worker should only run bundles that match the worker\'s tag.',
+        help='To be used when the worker should only run bundles that match the worker\'s tag. Only has an effect when --tag is set.',
     )
     parser.add_argument(
         '--pass-down-termination',
@@ -323,7 +323,7 @@ def main():
         args.checkin_frequency_seconds,
         bundle_service,
         args.shared_file_system,
-        args.tag_exclusive,
+        args.tag_exclusive if args.tag else False,
         args.group,
         docker_runtime=docker_runtime,
         docker_network_prefix=args.network_prefix,
