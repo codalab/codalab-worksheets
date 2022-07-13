156ea6753bad6dbde2f87422c7eba98c9c93c693
leilenah <leilenah.mamea@gmail.com>
2022-07-13 14:06:05 -0700

    Fix GetBundleStateDetailsTest

--- tests/unit/lib/bundle_util_test.py
+++ tests/unit/lib/bundle_util_test.py
@@ -3,22 +3,6 @@ from codalab.lib.bundle_util import get_bundle_state_details
 
 
 class GetBundleStateDetailsTest(unittest.TestCase):
-    def test_missing_bundle_state(self):
-        """
-        Returns an empty string if `state` is missing from the bundle.
-        """
-        bundle = {'bundle_type': 'run'}
-        state_details = get_bundle_state_details(bundle)
-        self.assertEqual(state_details, '')
-
-    def test_missing_bundle_type(self):
-        """
-        Returns an empty string if `bundle_type` is missing from the bundle.
-        """
-        bundle = {'state': 'running'}
-        state_details = get_bundle_state_details(bundle)
-        self.assertEqual(state_details, '')
-
     def test_running_bundle(self):
         """
         Returns `run_status` if state is `running`.
@@ -31,19 +15,6 @@ class GetBundleStateDetailsTest(unittest.TestCase):
         }
         state_details = get_bundle_state_details(running_bundle)
         self.assertEqual(state_details, run_status)
-
-    def test_staged_bundle(self):
-        """
-        Returns `staged_status` if state is `staged`.
-        """
-        staged_status = 'Staged.'
-        staged_bundle = {
-            'bundle_type': 'run',
-            'state': 'staged',
-            'metadata': {'staged_status': staged_status},
-        }
-        state_details = get_bundle_state_details(staged_bundle)
-        self.assertEqual(state_details, staged_status)
 
     def test_created_bundles(self):
         """
@@ -76,7 +47,7 @@ class GetBundleStateDetailsTest(unittest.TestCase):
         }
         make_bundle_details = get_bundle_state_details(make_bundle)
         make_bundle_details_expected = (
-            'Bundle has been created but its contents have not been populated yet.'
+            'Bundle has been created but its contents have not yet been populated.'
         )
 
         # assertions
