ee68952f03e38250d31338e3f417593a25334e92
leilenah <leilenah.mamea@gmail.com>
2022-07-19 14:19:57 -0700

    undo version bump

--- codalab/common.py
+++ codalab/common.py
@@ -27,7 +27,7 @@ from codalab.lib.beam.filesystems import (
 
 # Increment this on master when ready to cut a release.
 # http://semver.org/
-CODALAB_VERSION = 'development-v1'
+CODALAB_VERSION = '1.5.5'
 BINARY_PLACEHOLDER = '<binary>'
 URLOPEN_TIMEOUT_SECONDS = int(os.environ.get('CODALAB_URLOPEN_TIMEOUT_SECONDS', 5 * 60))
 
