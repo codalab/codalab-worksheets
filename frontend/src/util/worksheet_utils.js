d81f5b6ddeddaad4c677d3cac881857a452271c6
leilenah <leilenah.mamea@gmail.com>
2022-09-14 11:35:22 -0700

    Revert metadata changes.

--- frontend/src/util/worksheet_utils.js
+++ frontend/src/util/worksheet_utils.js
@@ -70,9 +70,6 @@ export function renderSize(size, includeBytes = false) {
     // size: number of bytes
     // includeBytes: whether or not to include 'bytes' string in the return value
     // Return a human-readable string.
-    if (!size) {
-        return '';
-    }
     var units = ['', 'k', 'm', 'g', 't'];
     for (var i = 0; i < units.length; i++) {
         const unit = includeBytes && units[i] === '' ? ' bytes' : units[i];
