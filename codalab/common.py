f806303f3db2aa657911fb7dfad70a4e2d26624d
AndrewJGaut <ajg@umail.ucsb.edu>
2022-11-29 16:01:58 -0800

    Update version

--- codalab/common.py
+++ codalab/common.py
@@ -29,7 +29,7 @@ from codalab.lib.beam.filesystems import (
 
 # Increment this on master when ready to cut a release.
 # http://semver.org/
-CODALAB_VERSION = '1.5.12'
+CODALAB_VERSION = '1.5.13'
 BINARY_PLACEHOLDER = '<binary>'
 URLOPEN_TIMEOUT_SECONDS = int(os.environ.get('CODALAB_URLOPEN_TIMEOUT_SECONDS', 5 * 60))
 
