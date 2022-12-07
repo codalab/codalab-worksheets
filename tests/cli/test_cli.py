d4f3cd651ee373a9354ab764c9970a3e5516c51d
Ashwin Ramaswami <aramaswamis@gmail.com>
2022-12-07 16:33:03 +0000

    fix

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -1727,7 +1727,7 @@ def test_run(ctx):
     _run_command([cl, 'uedit', 'codalab', '--time-quota', str(time_used + 2)])
     uuid = _run_command([cl, 'run', 'sleep 100000'])
     wait_until_state(uuid, State.RUNNING)
-    wait_until_state(uuid, State.KILLED, timeout_seconds=60)
+    wait_until_state(uuid, State.KILLED, timeout_seconds=120)
     check_equals(
         'Kill requested: User time quota exceeded. To apply for more quota,'
         ' please visit the following link: '
