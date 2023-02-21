87ba6353802d9aefc8921c2ebf270119e41582b3
AndrewJGaut <ajg@umail.ucsb.edu>
2023-02-20 21:18:27 -0800

    Adding extra line to craete the partitions directory since it sometimes wasn't present before riming tests started and this caused an error. still not sure why that was happening

--- timing_test_for_profilng.sh
+++ timing_test_for_profilng.sh
@@ -5,6 +5,7 @@ do
     export CODALAB_SENTRY_TRANSACTION_RATE=$rate
     echo "transaction rate $rate"
     python3 codalab_service.py start -s rest-server worker
+    docker exec codalab_rest-server_1 /bin/bash -c "mkdir -p /home/azureuser/codalab-worksheets/var/codalab/home/partitions"
     docker exec -it codalab_rest-server_1 /bin/bash -c "python3 tests/timing/timing_test.py"
 done
 
