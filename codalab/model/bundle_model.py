7868afa5b2f53bad9ba20b25867b5bba279d4e22
Jiani Wang <wjn9424@163.com>
2023-04-07 14:14:14 -0400

    add regex to recongnize emails

--- codalab/model/bundle_model.py
+++ codalab/model/bundle_model.py
@@ -68,6 +68,7 @@ logger = logging.getLogger(__name__)
 
 SEARCH_KEYWORD_REGEX = re.compile('^([\.\w/]*)=(.*)$')
 SEARCH_RESULTS_LIMIT = 10
+EDU_USER_REGEXS = re.compile('@[\w\.-]+\.(edu|edu\.[a-z]{2})$')
 
 
 def str_key_dict(row):
@@ -2475,18 +2476,27 @@ class BundleModel(object):
         :param affiliation:
         :return: (new integer user ID, verification key to send)
         """
+
+        def is_academic_email(email):
+            """
+            This is a basic function that can be used to compare the email domain suffix with a list of academic email domains. 
+            Academic emails typically have domains such as "yy.edu" or "xyz.edu.xx" (where "edu" is followed by a country code).
+            """
+            email_suffix = EDU_USER_REGEXS.findall(email.lower())
+            return len(email_suffix) > 0
+
         with self.engine.begin() as connection:
             now = datetime.datetime.utcnow()
             user_id = user_id or '0x%s' % uuid4().hex
 
             time_quota = (
                 self.default_user_info['edu_time_quota']
-                if spec_util.is_academic_email(email)
+                if is_academic_email(email)
                 else self.default_user_info['time_quota']
             )
             disk_quota = (
                 self.default_user_info['edu_disk_quota']
-                if spec_util.is_academic_email(email)
+                if is_academic_email(email)
                 else self.default_user_info['disk_quota']
             )
 
