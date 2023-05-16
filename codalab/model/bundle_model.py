f99c799fc6f2f7e0a5765ba30fb4be759c2769ac
AndrewJGaut <stanford@DNa1c4533.SUNet>
2023-05-16 15:15:20 -0700

    Added more debug logging and took out all tests that were only kill or only run

--- codalab/model/bundle_model.py
+++ codalab/model/bundle_model.py
@@ -1127,11 +1127,16 @@ class BundleModel(object):
             was recorded during finalization of the bundle.
         """
         logger.error("-"*80)
+        logger.error("In transition_bundle_finished")
         logger.error(bundle.to_dict())
         metadata = bundle.metadata.to_dict()
+        logger.error(metadata)
         failure_message = metadata.get('failure_message', None)
         exitcode = metadata.get('exitcode', 0)
+        logger.error(failure_message)
+        logger.error(exitcode)
         state = State.FAILED if failure_message or exitcode else State.READY
+        logger.error(state)
         if failure_message and 'Kill requested' in failure_message:
             state = State.KILLED
 
