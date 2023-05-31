f50810ea59a8cd4c19794ea0f42b10c3dc0942b3
Ashwin Ramaswami <aramaswamis@gmail.com>
2023-05-31 18:13:49 +0000

    rm return

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -481,7 +481,6 @@ class ModuleContext(object):
 
         # Clean up and restore original worksheet
         print("[*][*] CLEANING UP")
-        return
 
         switch_user('codalab')  # root user
         _run_command([cl, 'work', self.original_worksheet])
