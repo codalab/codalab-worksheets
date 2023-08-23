4474e0a3b30b9a9a3fa42d7b9747cf0e7a507edc
AndrewJGaut <ajg@umail.ucsb.edu>
2023-08-22 21:45:00 -0700

    minor change for testing purpsose

--- codalab/worker/docker_utils.py
+++ codalab/worker/docker_utils.py
@@ -215,70 +215,74 @@ class DockerRuntime(Runtime):
         runtime=DEFAULT_RUNTIME,
         shared_memory_size_gb=1,
     ) -> str:
-        if not command.endswith(';'):
-            command = '{};'.format(command)
-        # Explicitly specifying "/bin/bash" instead of "bash" for bash shell to avoid the situation when
-        # the program can't find the symbolic link (default is "/bin/bash") of bash in the environment
-        docker_command = ['/bin/bash', '-c', '( %s ) >stdout 2>stderr' % command]
-        docker_bundle_path = '/' + uuid
-        volumes = self.get_bundle_container_volume_binds(
-            bundle_path, docker_bundle_path, dependencies
-        )
-        environment = {'HOME': docker_bundle_path, 'CODALAB': 'true'}
-        working_dir = docker_bundle_path
-        # Unset entrypoint regardless of image
-        entrypoint = ''
-        cpuset_str = ','.join(cpuset) if cpuset else ''
-        # Get user/group that owns the bundle directory
-        # Then we can ensure that any created files are owned by the user/group
-        # that owns the bundle directory, not root.
-        bundle_stat = os.stat(bundle_path)
-        uid = bundle_stat.st_uid
-        gid = bundle_stat.st_gid
-        # TODO: Fix potential permissions issues arising from this setting
-        # This can cause problems if users expect to run as a specific user
-        user = '%s:%s' % (uid, gid)
-
-        if runtime == NVIDIA_RUNTIME:
-            # nvidia-docker runtime uses this env variable to allocate GPUs
-            environment['NVIDIA_VISIBLE_DEVICES'] = ','.join(gpuset) if gpuset else ''
-
-        # Name the container with the UUID for readability
-        container_name = 'codalab_run_%s' % uuid
         try:
-            container = self.client.containers.run(
-                image=docker_image,
-                command=docker_command,
-                name=container_name,
-                network=network,
-                mem_limit=memory_bytes,
-                shm_size=f"{shared_memory_size_gb}G",
-                cpuset_cpus=cpuset_str,
-                environment=environment,
-                working_dir=working_dir,
-                entrypoint=entrypoint,
-                volumes=volumes,
-                user=user,
-                detach=detach,
-                runtime=runtime,
-                tty=tty,
-                stdin_open=tty,
+            if not command.endswith(';'):
+                command = '{};'.format(command)
+            # Explicitly specifying "/bin/bash" instead of "bash" for bash shell to avoid the situation when
+            # the program can't find the symbolic link (default is "/bin/bash") of bash in the environment
+            docker_command = ['/bin/bash', '-c', '( %s ) >stdout 2>stderr' % command]
+            docker_bundle_path = '/' + uuid
+            volumes = self.get_bundle_container_volume_binds(
+                bundle_path, docker_bundle_path, dependencies
             )
-            logger.debug(
-                'Started Docker container for UUID %s, container ID %s,', uuid, container.id
-            )
-        except docker.errors.APIError:
-            # The container failed to start, so it's in the CREATED state
-            # If we try to re-run the container again, we'll get a 409 CONFLICT
-            # because a container with the same name already exists. So, we try to remove
-            # the container here.
+            environment = {'HOME': docker_bundle_path, 'CODALAB': 'true'}
+            working_dir = docker_bundle_path
+            # Unset entrypoint regardless of image
+            entrypoint = ''
+            cpuset_str = ','.join(cpuset) if cpuset else ''
+            # Get user/group that owns the bundle directory
+            # Then we can ensure that any created files are owned by the user/group
+            # that owns the bundle directory, not root.
+            bundle_stat = os.stat(bundle_path)
+            uid = bundle_stat.st_uid
+            gid = bundle_stat.st_gid
+            # TODO: Fix potential permissions issues arising from this setting
+            # This can cause problems if users expect to run as a specific user
+            user = '%s:%s' % (uid, gid)
+
+            if runtime == NVIDIA_RUNTIME:
+                # nvidia-docker runtime uses this env variable to allocate GPUs
+                environment['NVIDIA_VISIBLE_DEVICES'] = ','.join(gpuset) if gpuset else ''
+
+            # Name the container with the UUID for readability
+            container_name = 'codalab_run_%s' % uuid
             try:
-                self.client.api.remove_container(container_name, force=True)
-            except Exception:
-                logger.warning("Failed to clean up Docker container after failed launch.")
-                traceback.print_exc()
-            raise
-        return container.id
+                container = self.client.containers.run(
+                    image=docker_image,
+                    command=docker_command,
+                    name=container_name,
+                    network=network,
+                    mem_limit=memory_bytes,
+                    shm_size=f"{shared_memory_size_gb}G",
+                    cpuset_cpus=cpuset_str,
+                    environment=environment,
+                    working_dir=working_dir,
+                    entrypoint=entrypoint,
+                    volumes=volumes,
+                    user=user,
+                    detach=detach,
+                    runtime=runtime,
+                    tty=tty,
+                    stdin_open=tty,
+                )
+                logger.debug(
+                    'Started Docker container for UUID %s, container ID %s,', uuid, container.id
+                )
+            except docker.errors.APIError:
+                # The container failed to start, so it's in the CREATED state
+                # If we try to re-run the container again, we'll get a 409 CONFLICT
+                # because a container with the same name already exists. So, we try to remove
+                # the container here.
+                try:
+                    self.client.api.remove_container(container_name, force=True)
+                except Exception:
+                    logger.warning("Failed to clean up Docker container after failed launch.")
+                    traceback.print_exc()
+                raise
+            return container.id
+        except:
+            logger.error("ERROR STARTING DOCKER CONTAINER")
+            traceback.print_exc()  # Print the whole traceback.
 
     def get_bundle_container_volume_binds(self, bundle_path, docker_bundle_path, dependencies):
         """
