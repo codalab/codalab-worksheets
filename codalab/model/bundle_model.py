43ae9e83b1f47190d76ef47f1a0a2157d70dceeb
Ashwin Ramaswami <aramaswamis@gmail.com>
2023-10-18 18:57:40 -0400

    Revert "Fix bugs in migration script (#4501)"
    This reverts commit 73b4d7efccff38f7e9c610662fd856400fa398f4.

--- codalab/model/bundle_model.py
+++ codalab/model/bundle_model.py
@@ -744,8 +744,10 @@ class BundleModel(object):
 
         return self._execute_query(query)
 
-    def get_all_bundle_uuids(self, max_results=100):
-        query = select([cl_bundle.c.uuid]).limit(max_results)
+    def get_all_bundle_uuids(self, max_results):
+        clause = and_(true(), cl_bundle.c.uuid == cl_bundle_metadata.c.bundle_uuid)  # Join
+        query = select([cl_bundle.c.uuid]).where(clause)
+        query = query.order_by(cl_bundle.c.id.desc()).limit(max_results)
         return self._execute_query(query)
 
     def get_memoized_bundles(self, user_id, command, dependencies):
