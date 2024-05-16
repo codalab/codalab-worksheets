9490a012681ee691ff7c382b69d7eadecbcd6885
Daniel <ziyuema@gmail.com>
2024-05-16 00:09:38 -0700

    Fixed race condition

--- codalab/lib/beam/MultiReaderFileStream.py
+++ codalab/lib/beam/MultiReaderFileStream.py
@@ -25,10 +25,8 @@ class MinMaxHeap:
             raise IndexError("pop from an empty heap")
     
     def update(self, index, new_item):
-        old_item = self.heap[index]
         self.heap[index] = new_item
         self.item_index[new_item] = index
-        del self.item_index[old_item]
         heapq._siftup(self.heap, index)
         heapq._siftdown(self.heap, 0, index)
     
@@ -71,13 +69,14 @@ class MultiReaderFileStream(BytesIO):
         self._buffer = bytes()
         self._buffer_pos = 0 # start position of buffer in the fileobj (min reader position - LOOKBACK LENGTH)
         self._size = 0 # size of bytes (for convenience)
-        self._pos = MinMaxHeap() # position of each reader
+        # self._pos = MinMaxHeap() # position of each reader
+        self._pos = [0 for _ in range(self.NUM_READERS)] # position of each reader
         self._fileobj = fileobj
         self._lock = Lock()  # lock to ensure one does not concurrently read self._fileobj / write to the buffer.
 
-        for i in range(0, self.NUM_READERS):
-            self._pos.push(0)
-            assert self._pos.get_at_index(i) == 0
+        # for i in range(0, self.NUM_READERS):
+        #     self._pos.push(0)
+        #     assert self._pos.get_at_index(i) == 0
         class FileStreamReader(BytesIO):
             def __init__(s, index):
                 s._index = index
@@ -88,7 +87,7 @@ class MultiReaderFileStream(BytesIO):
             def peek(s, num_bytes):
                 return self.peek(s._index, num_bytes)
             
-            def seek(s, offset, whence):
+            def seek(s, offset, whence=SEEK_SET):
                 return self.seek(s._index, offset, whence)
 
         self.readers = [FileStreamReader(i) for i in range(0, self.NUM_READERS)]
@@ -105,39 +104,51 @@ class MultiReaderFileStream(BytesIO):
         """Read the specified number of bytes from the associated file.
         index: index that specifies which reader is reading.
         """
-        self._fill_buf_bytes(index, num_bytes)
+        # Calculate how many new bytes need to be read
+        print("READING HERE with num_bytes, index, self._buffer_pos, self._size: ", num_bytes, index, self._buffer_pos, self._size)
+        num_bytes -= max(self._pos) - self._pos[index]
+        if num_bytes > 0:
+            print("new num bytes: ", num_bytes)
+            self._fill_buf_bytes(num_bytes)
         # if num_bytes is None:
         #     num_bytes = len(self._bufs[index])
-        while (self._pos.get_at_index(index) + num_bytes) - self._buffer_pos > self.MAX_THRESHOLD:
+        while (self._pos[index] + num_bytes) - self._buffer_pos > self.MAX_THRESHOLD:
+            print("SLEEPING")
             time.sleep(.1) # 100 ms
 
-        old_position = self._pos.get_at_index(index)
-        s = self._buffer[old_position:old_position + num_bytes]
+        with self._lock:
+            old_position = self._pos[index]
+            s = self._buffer[old_position:old_position + num_bytes]
 
-        # Modify position
-        new_position = old_position + len(s)
-        self._pos.update(index, new_position) 
+            # Modify position
+            self._pos[index] += len(s)
 
-        # Update buffer if this reader is the minimum reader
-        diff = (self._pos.min() - self.LOOKBACK_LENGTH) - self._buffer_pos # calculated min position of buffer minus current min position of buffer
-        # NOTE: it's possible for diff < 0 if seek backwards occur
-        if diff > 0:
-            self._buffer = self._buffer[diff:]
-            self._buffer_pos += diff
-            self._size -= diff
+            # Update buffer if this reader is the minimum reader
+            diff = (min(self._pos) - self.LOOKBACK_LENGTH) - self._buffer_pos # calculated min position of buffer minus current min position of buffer
+            # NOTE: it's possible for diff < 0 if seek backwards occur
+            print("DIFF: ", diff)
+            if diff > 0:
+                self._buffer = self._buffer[diff:]
+                self._buffer_pos += diff
+                self._size -= diff
 
+            print("Length of return: ", len(s))
         return s
 
     def peek(self, index: int, num_bytes):   # type: ignore
-        self._fill_buf_bytes(index, num_bytes)
-        s = self._bufs[index].peek(num_bytes)
-        return s
+        pass
+        # self._fill_buf_bytes(index, num_bytes)
+        # s = self._bufs[index].peek(num_bytes)
+        # return s
 
     def seek(self, index: int, offset: int, whence=SEEK_SET):
+        print("SEEKING SEEKING SEEKING")
+        print(index, offset)
         if whence == SEEK_END:
             super().seek(offset, whence)
         else:
-            pass
+            assert offset >= self._buffer_pos
+            self._pos[index] = offset
             
     def close(self):
         self.__input.close()
